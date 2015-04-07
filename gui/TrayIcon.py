19cefcb7860fdae3c353e382bfd34c59ac07ad5c
Jan Binder <jan-git@nlogn.org>
2015-04-07 21:39:25 +0000

    le Tray Icon has arrived

---
+++ gui/TrayIcon.py
@@ -0,0 +1,39 @@
+# a tray Icon for QuDi
+
+from core.Base import Base
+from pyqtgraph.Qt import QtCore, QtGui
+
+class TrayIcon(Base):
+    def __init__(self, manager, name, config = {}, **kwargs):
+        Base.__init__(self, manager, name, configuation=config, callback_dict = {})
+
+        self.__tray = SystemTrayIcon()
+        self.__tray.show()
+
+class RightClickMenu(QtGui.QMenu):
+    def __init__(self, parent=None):
+        QtGui.QMenu.__init__(self, "Edit", parent)
+        self.icon = QtGui.QIcon.fromTheme("edit-cut")
+        self.addAction(QtGui.QAction(self.icon, "&Cut", self))
+
+class LeftClickMenu(QtGui.QMenu):
+    def __init__(self, parent=None):
+        QtGui.QMenu.__init__(self, "Edit", parent)
+        self.icon = QtGui.QIcon.fromTheme("document-new")
+        self.addAction(QtGui.QAction(self.icon, "&New", self))
+
+class SystemTrayIcon(QtGui.QSystemTrayIcon):
+    def __init__(self, parent=None):
+        QtGui.QSystemTrayIcon.__init__(self, parent)
+        self.setIcon(QtGui.QIcon.fromTheme("document-save"))
+        self.right_menu = RightClickMenu()
+        self.left_menu = LeftClickMenu()
+        self.setContextMenu(self.right_menu)
+        self.activated.connect(self.click_trap)
+
+    def click_trap(self, value):
+        if value == self.Trigger:
+            self.left_menu.exec_(QtGui.QCursor.pos())
+
+    def welcome(self):
+        self.showMessage("Hello World")
