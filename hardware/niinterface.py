0b18c459cd09b5a951171c2228b1c0633bf689fa
Kay Jahnke <kay.jahnke@alumni.uni-ulm.de>
2015-04-10 08:17:58 +0000

    cleaning up the counter code and giving it some docstrings also included some error handling to make it a bit more robust

--- hardware/niinterface.py
+++ hardware/niinterface.py
@@ -22,30 +22,72 @@ class niinterface(Base,SlowCounterInterface):
                     
         self._MaxCounts = 1e7
         self._RWTimeout = 5
+        self._counter_daq_task = None
+        self._clock_daq_task = None
         
+        if 'clock_channel' in config.keys():
+            self._clock_channel=config['clock_channel']
+        else:
+            self.logMsg('No clock_channel configured.', messageType='error')
+            
+        if 'counter_channel' in config.keys():
+            self._counter_channel=config['counter_channel']
+        else:
+            self.logMsg('No counter_channel configured.', messageType='error')
+            
+        if 'photon_source' in config.keys():
+            self._photon_source=config['photon_source']
+        else:
+            self.logMsg('No photon_source configured.', messageType='error')
+            
+        if 'clock_frequency' in config.keys():
+            self._clock_frequency=config['clock_frequency']
+        else:
+            self._clock_frequency=100
+            self.logMsg('No clock_frequency configured tanking 100 Hz instead.', \
+            messageType='warning')
+            
+        if 'samples_number' in config.keys():
+            self._samples_number=config['samples_number']
+        else:
+            self._samples_number=10
+            self.logMsg('No samples_number configured tanking 10 instead.', \
+            messageType='warning')
+            
         # checking for the right configuration
         for key in config.keys():
             self.logMsg('{}: {}'.format(key,config[key]), 
                         messageType='status')
                         
-#        self.testing()
+        self.testing()
         
     def testing(self):
-        self.set_up_clock(clock_frequency = 100, clock_channel = '/Dev1/Ctr0')
+        self.set_up_clock(clock_frequency = self._clock_frequency, clock_channel = '/Dev1/Ctr0')
         self.set_up_counter(counter_channel = '/Dev1/Ctr1', photon_source= '/Dev1/PFI8')
         for i in range(5):
-            print(self.get_counter(samples=100))
+            print(self.get_counter(samples=self._samples_number))
         self.close_counter()
         self.close_clock()
         
-    def set_up_clock(self, clock_frequency = 1000., clock_channel = 'dummy'):
+    def set_up_clock(self, clock_frequency = None, clock_channel = None):
         """ Configures the hardware clock of the NiDAQ card to give the timing. 
+        <blank line>
+        @param float clock_frequency: if defined, this sets the frequency of the clock
+        @param string clock_channel: if defined, this is the physical channel of the clock
         <blank line>
         @return int: error code (0:OK, -1:error)
         """ 
+        
+        if self._clock_daq_task != None:            
+            self.logMsg('Another clock is already running, close this one first.', \
+            messageType='error')
+            return -1
+        
         self._clock_daq_task = daq.TaskHandle()  # create handle for task, this task will generate pulse signal for photon counting
-        self._clock_frequency = float(clock_frequency)
-        self._clock_channel = clock_channel
+        if clock_frequency != None:
+            self._clock_frequency = float(clock_frequency)
+        if clock_channel != None:
+            self._clock_channel = clock_channel
         
         daq.DAQmxCreateTask('', daq.byref(self._clock_daq_task))    # create task for pulse_out, here the parameter self.pulse_out_task has to be passed by reference (passing a pointer) 
         daq.DAQmxCreateCOPulseChanFreq( self._clock_daq_task,      # the task to which to add the channels that this function creates
@@ -67,15 +109,35 @@ class niinterface(Base,SlowCounterInterface):
                            
         return 0
     
-    def set_up_counter(self, counter_channel = 'dummy', photon_source = 'dummy'):
+    def set_up_counter(self, counter_channel = None, photon_source = None, clock_channel = None):
         """ Configures the actual counter with a given clock. 
+        <blank line>
+        @param string counter_channel: if defined, this is the physical channel of the counter
+        @param string photon_source: if defined, this is the physical channel where the photons are to count from
+        @param string clock_channel: if defined, this specifies the clock for the counter
         <blank line>
         @return int: error code (0:OK, -1:error)
         """
         
+        if self._clock_daq_task == None and clock_channel == None:            
+            self.logMsg('No clock running, call set_up_clock before starting the counter.', \
+            messageType='error')
+            return -1
+        if self._counter_daq_task != None:            
+            self.logMsg('Another counter is already running, close this one first.', \
+            messageType='error')
+            return -1
+            
         self._counter_daq_task = daq.TaskHandle()  # this task will count photons with binning defined by pulse_out_task
-        self._counter_channel = counter_channel
-        self._photon_source = photon_source
+        if counter_channel != None:
+            self._counter_channel = counter_channel
+        if photon_source != None:
+            self._photon_source = photon_source
+        
+        if clock_channel != None:
+            my_clock_channel = clock_channel
+        else: 
+            my_clock_channel = self._clock_channel
         
         daq.DAQmxCreateTask('', daq.byref(self._counter_daq_task))  # create task for counter_in, here the parameter self.counter_in_task has to be passed by reference (passing a pointer) 
         
@@ -93,14 +155,8 @@ class niinterface(Base,SlowCounterInterface):
                                         daq.DAQmx_Val_Ticks, #units of width measurement, here photon ticks
                                         '')   
         
-        hook=self._clock_channel+'InternalOutput'
-#        import ctypes
-#        hook=ctypes.c_char_p(hook.encode('ascii'))
-#        hook=daq.CtypesString().from_param(hook)
-#        hook="Dev1/Ctr0InternalOutput"
-        print (hook)
         # set the pulses to counter self._trace_counter_in
-        daq.DAQmxSetCISemiPeriodTerm( self._counter_daq_task, self._counter_channel, hook)
+        daq.DAQmxSetCISemiPeriodTerm( self._counter_daq_task, self._counter_channel, my_clock_channel+'InternalOutput')
         # set the timebase for width measurement as self._photon_source
         daq.DAQmxSetCICtrTimebaseSrc( self._counter_daq_task, self._counter_channel, self._photon_source )  
         
@@ -118,11 +174,23 @@ class niinterface(Base,SlowCounterInterface):
         
         return -1
         
-    def get_counter(self, samples=1):
+    def get_counter(self, samples=None):
         """ Returns the current counts per second of the counter. 
+        <blank line>
+        @param int samples: if defined, number of samples to read in one go
         <blank line>
         @return float: the photon counts per second
         """
+        
+        if self._counter_daq_task == None:            
+            self.logMsg('No counter running, call set_up_counter before reading it.', \
+            messageType='error')
+            return np.ones((samples,), dtype=np.uint32) * -1.
+            
+        if samples == None:
+            samples = int(self._samples_number)
+        else:
+            samples = int(samples)
         
         count_data = np.empty((samples,), dtype=np.uint32) # count data will be written here in the NumPy array
         
@@ -135,7 +203,7 @@ class niinterface(Base,SlowCounterInterface):
                                 samples,         # length of array to write into
                                 daq.byref(n_read_samples),     # number of samples which were read (should be 1 obviously)
                                 None)                           # Reserved for future use. Pass NULL(here None) to this parameter
-        return count_data    #normalize to counts per second
+        return count_data * self._clock_frequency   #normalize to counts per second
     
     def close_counter(self):
         """ Closes the counter and cleans up afterwards. 
@@ -145,6 +213,7 @@ class niinterface(Base,SlowCounterInterface):
         
         daq.DAQmxStopTask(self._counter_daq_task)
         daq.DAQmxClearTask(self._counter_daq_task)
+        self._counter_daq_task = None
         
         return 0
         
@@ -156,5 +225,6 @@ class niinterface(Base,SlowCounterInterface):
         
         daq.DAQmxStopTask(self._clock_daq_task)
         daq.DAQmxClearTask(self._clock_daq_task)
+        self._clock_daq_task = None
         
         return 0
--- hardware/slowcounterinterface.py
+++ hardware/slowcounterinterface.py
@@ -11,28 +11,39 @@ class SlowCounterInterface():
     microwave hardware.
     """
     
-    def set_up_clock(self):
+    def set_up_clock(self, clock_frequency = None, clock_channel = None):
         """ Configures the hardware clock of the NiDAQ card to give the timing. 
+        <blank line>
+        @param float clock_frequency: if defined, this sets the frequency of the clock
+        @param string clock_channel: if defined, this is the physical channel of the clock
         <blank line>
         @return int: error code (0:OK, -1:error)
         """ 
+        
         raise InterfaceImplementationError('SlowCounterInterface>set_up_clock')
         return -1
     
-    def set_up_counter(self):
+    def set_up_counter(self, counter_channel = None, photon_source = None, clock_channel = None):
         """ Configures the actual counter with a given clock. 
+        <blank line>
+        @param string counter_channel: if defined, this is the physical channel of the counter
+        @param string photon_source: if defined, this is the physical channel where the photons are to count from
+        @param string clock_channel: if defined, this specifies the clock for the counter
         <blank line>
         @return int: error code (0:OK, -1:error)
         """
+        
         raise InterfaceImplementationError('SlowCounterInterface>set_up_counter')
         return -1
         
-    def get_counter(self):
+    def get_counter(self, samples=None):
         """ Returns the current counts per second of the counter. 
+        <blank line>
+        @param int samples: if defined, number of samples to read in one go
         <blank line>
         @return float: the photon counts per second
         """
-        # This is not a good way to implement it!
+        
         raise InterfaceImplementationError('SlowCounterInterface>get_counter')
         return 0.0
     
@@ -41,6 +52,7 @@ class SlowCounterInterface():
         <blank line>
         @return int: error code (0:OK, -1:error)
         """
+        
         raise InterfaceImplementationError('SlowCounterInterface>close_counter')
         return -1
         
@@ -49,5 +61,6 @@ class SlowCounterInterface():
         <blank line>
         @return int: error code (0:OK, -1:error)
         """
+        
         raise InterfaceImplementationError('SlowCounterInterface>close_clock')
         return -1
