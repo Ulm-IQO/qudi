ca51f388848f77727261c3a22242cd3f198b0c36
Florian Frank <florian.frank@uni-ulm.de>
2015-04-17 22:53:06 +0000

    added some functions (Some descriptions are missing)

--- hardware/mwsourceinterface.py
+++ hardware/mwsourceinterface.py
@@ -72,3 +72,71 @@ class MWInterface():
         """
         raise InterfaceImplementationError('MWInterface>set_cw')
         return -1
+        
+    def set_list(self,frequency=None, power=None):
+        """ Sets the microwave output to list.
+        
+        @return int: Returns the number of items of the list.
+        """
+        raise InterfaceImplementationError('MWInterface>set_list')
+        return -1
+        
+    def reset_listpos(self):
+        """
+        
+        @return int: error code (0:OK, -1:error)
+        """
+        raise InterfaceImplementationError('MWInterface>reset_listpos')
+        return -1
+                
+    def list_on(self):
+        """ Switches on the list mode. 
+        
+        @return int: error code (0:OK, -1:error)
+        """
+        raise InterfaceImplementationError('MWInterface>list_on')
+        return -1
+        
+    def set_sweep(self,frequency_start=None,frequency_stop=None,frequency_delta=None):
+        """
+        """
+        raise InterfaceImplementationError('MWInterface>set_sweep')
+        return -1
+        
+    def sweep_pos(self,frequency=None):
+        """
+        """
+        raise InterfaceImplementationError('MWInterface>sweep_pos')
+        return -1
+        
+    def trigger(self,source,pol):
+        """
+        
+        @return int: error code (0:OK, -1:error)
+        """
+        raise InterfaceImplementationError('MWInterface>trigger')
+        return -1
+        
+    def set_modulation(self,flag=None):
+        """
+        """
+        raise InterfaceImplementationError('MWInterface>set_modulation')
+        return -1
+        
+    def output(self):
+        """
+        """
+        raise InterfaceImplementationError('MWInterface>output')
+        return -1
+        
+    def am(self,depth=None):
+        """
+        
+        @return float:
+        """
+        raise InterfaceImplementationError('MWInterface>am')
+        return -1
+        
+    
+        
+    
