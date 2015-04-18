a1e61a0e44cb4393278568beaf4c1e261189bab1
Alexander Stark <alexander.stark@uni-ulm.de>
2015-04-18 19:40:43 +0000

    - Add some additional comments for better understanding. - Replace the formatted output with the python3 definitions like print('This is a variable: {0}'.format(a) ) - Correct the proper usage of a QApplication object in the __main__.py. Here it should be deleted, otherwise you would have two QApplication objects. As a consequence of this change, the core module has to be imported to be able to access to the QApplication.

--- core/__main__.py
+++ core/__main__.py
@@ -5,7 +5,7 @@ Copyright 2010  Luke Campagnola
 Distributed under MIT/X11 license. See license.txt for more infomation.
 """
 
-print("Loading QuDi...")
+print('Loading QuDi...')
 if __package__ is None:
     import core
     __package__ = 'core'
@@ -16,44 +16,54 @@ import numpy as np
 import pyqtgraph as pg
 import sys
 
-# Pull some args out
+import core
+
+# Possibility to start the program with additional parameters. In the normal 
+# command line or the console, you can start the program as
+#
+#   python start.py --profile --callgraph
+#
+# where "--profile" enables the usage of a profiler and "--callgraph" gives the
+# possibility to display the dependencies between the methods/modules.
+
 if "--profile" in sys.argv:
     profile = True
-    sys.argv.pop(sys.argv.index('--profile'))
+    sys.argv.pop(sys.argv.index('--profile'))   # remove parameter from argv
+                                                # since it is used now.
 else:
     profile = False
 if "--callgraph" in sys.argv:
     callgraph = True
-    sys.argv.pop(sys.argv.index('--callgraph'))
+    sys.argv.pop(sys.argv.index('--callgraph')) # remove parameter from argv
+                                                # since it is used now.
 else:
     callgraph = False
 
 
-## Enable stack trace output when a crash is detected
+# Enable stack trace output when a crash is detected
 import faulthandler
 faulthandler.disable()
 faulthandler.enable(all_threads=True)
 
-## Initialize Qt
-app = pg.mkQApp()
 
-## Disable garbage collector to improve stability. 
-## (see pyqtgraph.util.garbage_collector for more information)
+# Disable garbage collector to improve stability. 
+# (see pyqtgraph.util.garbage_collector in the doc for more information)
 from pyqtgraph.util.garbage_collector import GarbageCollector
 gc = GarbageCollector(interval=1.0, debug=False)
 
-## Create Manager. This configures devices and creates the main manager window.
+# Create Manager. This configures devices and creates the main manager window.
+# All additional arguments in sys.argv, which were not used and executed here
+# are passed to the main device handler, the Manager.
 man = Manager(argv=sys.argv[1:])
 
 ## for debugging with pdb
 #QtCore.pyqtRemoveInputHook()
 
-## Start Qt event loop unless running in interactive mode.
-import pyqtgraph as pg
+# Start Qt event loop unless running in interactive mode and not using PySide.
 interactive = (sys.flags.interactive == 1) and not pg.Qt.USE_PYSIDE
 
-## Run python code periodically to allow interactive debuggers to interrupt
-## the qt event loop
+# Run python code periodically to allow interactive debuggers to interrupt
+# the qt event loop
 timer = QtCore.QTimer()
 def donothing(*args):
     #print "-- beat --"
@@ -66,47 +76,47 @@ timer.start(1000)
 if interactive:
     print("Interactive mode; not starting event loop.")
     
-    ## import some things useful on the command line
+    # import some modules which might be useful on the command line
     import numpy as np
 
-    ### Use CLI history and tab completion
+    # Use CLI history and tab completion
     import atexit
     import os
     historyPath = os.path.expanduser("~/.pyhistory")
     try:
         import readline
     except ImportError:
-        print("Module readline not available.")
+        print("Import Error in __main__: Module readline not available.")
     else:
         import rlcompleter
         readline.parse_and_bind("tab: complete")
         if os.path.exists(historyPath):
             readline.read_history_file(historyPath)
+
     def save_history(historyPath=historyPath):
         try:
             import readline
         except ImportError:
-            print("Module readline not available.")
+            print("Import Error in __main__: Module readline not available.")
         else:
             readline.write_history_file(historyPath)
     atexit.register(save_history)
 else:
     if profile:
         import cProfile
-        cProfile.run('app.exec_()', sort='cumulative')  
+        cProfile.run('core.app.exec_()', sort='cumulative')  
         # pg.exit() causes python to exit before Qt has
         # a chance to clean up.
-        # this avoids otherwise irritating exit crashes.
+        # This avoids otherwise irritating exit crashes.
         pg.exit()
 
     elif callgraph:
         from pycallgraph import PyCallGraph
         from pycallgraph.output import GraphvizOutput
         with PyCallGraph(output=GraphvizOutput()):
-            app.exec_()
+            core.app.exec_()
     else:
-        app.exec_()
+        core.app.exec_()
         # pg.exit() causes python to exit before Qt has a chance to clean up. 
-        # this avoids otherwise irritating exit crashes.
+        # This avoids otherwise irritating exit crashes.
         pg.exit()
-
