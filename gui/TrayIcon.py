c15b84ef78c751e250bb9070b6a9958e406ec4a2
Jan Binder <jan-git@nlogn.org>
2015-05-17 15:29:17 +0000

    clean up and document tray icon module

--- gui/TrayIcon.py
+++ gui/TrayIcon.py
@@ -1,11 +1,23 @@
 # -*- coding: utf-8 -*-
-# a tray Icon for QuDi
+""" This module contains a tray icon implementation for QuDi.
+When this module is loaded into QuDi, it will show the QuDi icon in the system tray.
+Left-clicking this icon will show an action menu that lets you bring the Manager window to the front.
+Right-clicking this icon will bring up a Quit button that will colse the whole application.
+"""
 
 from core.Base import Base
 from pyqtgraph.Qt import QtCore, QtGui
 
 class TrayIcon(Base):
+    """ The QuDi modlue class for the tray icon.
+    """
     def __init__(self, manager, name, config = {}, **kwargs):
+        """ Constructor for QuDi tray icon module.
+          @param object manager: the manager object that this tray icon belongs to
+          @param string name: the unique name of the module
+          @param dict config: the configuration dict for the module
+          @param dict kwargs: further named arguments
+        """
         callback = {'onactivate': self.initUI}
         Base.__init__(self,
                     manager,
@@ -15,22 +27,25 @@ class TrayIcon(Base):
                     )
 
     def initUI(self, e=None):
+        """ Set up tray icon UI .
+
+          @param e: Fysom state change
+
+            This method is automatically called by changing the Base state through activate().
+        """
         self._tray = SystemTrayIcon()
         self._tray.show()
         self._tray.quitAction.triggered.connect(self._manager.quit)
         self._tray.managerAction.triggered.connect(lambda: self._manager.sigShowManager.emit())
 
-class RightClickMenu(QtGui.QMenu):
-    def __init__(self, parent=None):
-        QtGui.QMenu.__init__(self, "quit", parent)
-
-class LeftClickMenu(QtGui.QMenu):
-    def __init__(self, parent=None):
-        QtGui.QMenu.__init__(self, "Manager", parent)
-
 class SystemTrayIcon(QtGui.QSystemTrayIcon):
-    def __init__(self, parent=None):
-        QtGui.QSystemTrayIcon.__init__(self, parent)
+    """Tray icon class subclassing QSystemTrayIcon for custom functionality.
+    """
+    def __init__(self):
+        """Tray icon constructor.
+        Adds all the appropriate menus and actions.
+        """
+        QtGui.QSystemTrayIcon.__init__(self)
         self.logo=QtGui.QIcon('artwork/qudi_trayicon.png')
         self.setIcon(self.logo)
         self.right_menu = QtGui.QMenu('Quit')
@@ -45,8 +60,13 @@ class SystemTrayIcon(QtGui.QSystemTrayIcon):
         self.activated.connect(self.click_trap)
 
     def click_trap(self, value):
+        """ Click handler.
+            
+          @param value: action that caused the activation
+
+            This method is called when the tray icon is left-clicked and
+            it opens a menu at the position of the left click.
+        """
         if value == self.Trigger:
             self.left_menu.exec_(QtGui.QCursor.pos())
 
-    def welcome(self):
-        self.showMessage("Hello World")
