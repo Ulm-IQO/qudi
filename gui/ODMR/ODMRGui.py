5a50162856aa53bbf165b94c159e34203f6f4fcc
Florian Frank <florian.frank@uni-ulm.de>
2015-05-28 16:14:56 +0000

    started with a extra z-scan tab in Confocal added some license headers

--- gui/ODMR/ODMRGui.py
+++ gui/ODMR/ODMRGui.py
@@ -1,8 +1,21 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri May  8 11:28:26 2015
+This file contains the QuDi GUI module base class.
 
-@author: user-admin
+QuDi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+QuDi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with QuDi. If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (C) 2015 Florian S. Frank florian.frank@uni-ulm.de
 """
 
 #from PyQt4 import QtCore, QtGui
