2c9385c8e145e305642fd48d20d628e13fd6be9d
Thomas Unden <thomas.unden@uni-ulm.de>
2015-05-28 11:08:57 +0000

    changes meeting

---
+++ logic/PulseExtractionLogic.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 28 12:24:25 2015
+
+@author: quantenoptik
+"""
+'input: rawdata from fastconter
+'       is gated?
+
+'output: 2D array with laserpulses in each row start is start of pulse and
+' in entry the hole pulse is captured
+' and sum of pulses with delay to see rising and falling
