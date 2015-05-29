e9783dcad2ebf4dc6f740e70cd2fd4b342118c7b
Lachlan Rogers <lachlan.j.rogers@quantum.diamonds>
2015-05-29 13:23:08 +0000

    added some License headers

--- logic/PoiManagerLogic.py
+++ logic/PoiManagerLogic.py
@@ -1,5 +1,25 @@
 # -*- coding: utf-8 -*-
-# unstable: Kay Jahnke
+"""
+This module contains a POI Manager core class which gives capability to mark 
+points of interest, re-optimise their position, and keep track of sample drift 
+over time.
+
+QuDi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+QuDi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with QuDi. If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (C) 2015 Kay D. Jahnke  kay.jahnke@alumni.uni-ulm.de
+"""
+
 
 from logic.GenericLogic import GenericLogic
 from pyqtgraph.Qt import QtCore
