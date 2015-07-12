81c60918c63242718c6c2089090285575b4f2a38
Jan Binder <jan-git@nlogn.org>
2015-07-12 18:11:29 +0000

    laser scanner now has an ui file and works when starting after stop

--- gui/laserscanner/laserscangui.py
+++ gui/laserscanner/laserscangui.py
@@ -1,17 +1,30 @@
 # -*- coding: utf-8 -*-
-# Test gui (test)
 
 from gui.guibase import GUIBase
-from pyqtgraph.Qt import QtCore, QtGui
+from pyqtgraph.Qt import QtCore, QtGui, uic
 from collections import OrderedDict
 import numpy as np
 import pyqtgraph as pg
 import pyqtgraph.exporters
 import time
+import os
 
+class LaserScanWindow(QtGui.QMainWindow):
+    def __init__(self):
+        """ Create the laser scanner window.
+        """
+        # Get the path to the *.ui file
+        this_dir = os.path.dirname(__file__)
+        ui_file = os.path.join(this_dir, 'ui_scanwindow.ui')
+
+        # Load it
+        super().__init__()
+        uic.loadUi(ui_file, self)
+        self.show()
+    
 
 class LaserScanningGui(GUIBase):
-    _modclass = 'countergui'
+    _modclass = 'LaserScanningGUi'
     _modtype = 'gui'
 
     ## declare connectors
@@ -24,7 +37,7 @@ class LaserScanningGui(GUIBase):
 
     def __init__(self, manager, name, config, **kwargs):
         ## declare actions for state transitions
-        c_dict = {'onactivate': self.initUI}
+        c_dict = {'onactivate': self.initUI, 'ondeactivate': self.deactivation}
         super().__init__(manager, name, config, c_dict)
 
         self.logMsg('The following configuration was found.', msgType='status')
@@ -40,19 +53,13 @@ class LaserScanningGui(GUIBase):
         """
 
         self._scanning_logic = self.connector['in']['laserscanninglogic1']['object']
-#        print("Counting logic is", self._counting_logic)
         self._save_logic = self.connector['in']['savelogic']['object']
-                
-        # setting up the window
-        self._mw = QtGui.QMainWindow()
-        self._mw.setWindowTitle('qudi: Laser Scanning')
-        self._mw.setGeometry(1000,400,800,600)
-        self._cw = QtGui.QWidget()
-        self._mw.setCentralWidget(self._cw)
         
-        # creating a plot in pyqtgraph and configuring it
+        # setting up the window
+        self._mw = LaserScanWindow()
+
         ## giving the plots names allows us to link their axes together
-        self._pw = pg.PlotWidget(name='Counter1')  
+        self._pw = self._mw.plotWidget # pg.PlotWidget(name='Counter1')
         self._plot_item = self._pw.plotItem
         
         ## create a new ViewBox, link the right axis to its coordinate system
@@ -79,117 +86,21 @@ class LaserScanningGui(GUIBase):
         self._pw.setLabel('bottom', 'Wavelength', units='nm')
         self._pw.setLabel('top', 'Relative Frequency', units='Hz')
                 
-        # defining buttons
-        self._stop_resume_button = QtGui.QPushButton('Stop')
-        self._stop_resume_button.setFixedWidth(70)
-        self._stop_resume_button.clicked.connect(self.stop_resume_clicked)
-        self._stop_resume_button.setEnabled(False)
-        self._save_button = QtGui.QPushButton('Save Histogram')
-        self._save_button.setFixedWidth(120)
-        self._save_button.clicked.connect(self.save_clicked)
-        self._start_button = QtGui.QPushButton('Start new')
-        self._start_button.setFixedWidth(100)
-        self._start_button.clicked.connect(self.start_clicked)
-        self._start_button.setEnabled(True)
+        self._mw.actionStop_resume_scan.triggered.connect(self.stop_resume_clicked)
+        self._mw.actionSave_histogram.triggered.connect(self.save_clicked)
+        self._mw.actionStart_scan.triggered.connect(self.start_clicked)
+        self._mw.actionAuto_range.triggered.connect(self.set_auto_range)
         
         # defining the parameters to edit
-        bins_tooltip = 'Number of bins to split the wavelength range up into.\nHigh bin number gives noisy but detailed data.'
-        self._bins_label = QtGui.QLabel('Bins (#):')
-        self._bins_label.setToolTip(bins_tooltip)
-        self._bins_display = QtGui.QSpinBox()
-        self._bins_display.setToolTip(bins_tooltip)
-        self._bins_display.setRange(1,1e4)
-        self._bins_display.setValue(self._scanning_logic.get_bins())
-        self._bins_display.editingFinished.connect(self.recalculate_histogram)
+        self._mw.binSpinBox.setValue(self._scanning_logic.get_bins())
+        self._mw.binSpinBox.editingFinished.connect(self.recalculate_histogram)
         
-        self._min_wavelength_label = QtGui.QLabel('Min (nm):')
-        self._min_wavelength_display = QtGui.QDoubleSpinBox()
-        self._min_wavelength_display.setDecimals(6)
-        self._min_wavelength_display.setRange(1,1e6)
-        self._min_wavelength_display.setValue(self._scanning_logic.get_min_wavelength())
-        self._min_wavelength_display.editingFinished.connect(self.recalculate_histogram)
+        self._mw.minDoubleSpinBox.setValue(self._scanning_logic.get_min_wavelength())
+        self._mw.minDoubleSpinBox.editingFinished.connect(self.recalculate_histogram)
         
-        self._max_wavelength_label = QtGui.QLabel('Max (nm):')
-        self._max_wavelength_display = QtGui.QDoubleSpinBox()
-        self._max_wavelength_display.setDecimals(6)
-        self._max_wavelength_display.setRange(1,1e4)
-        self._max_wavelength_display.setValue(self._scanning_logic.get_max_wavelength())
-        self._max_wavelength_display.editingFinished.connect(self.recalculate_histogram)
+        self._mw.maxDoubleSpinBox.setValue(self._scanning_logic.get_max_wavelength())
+        self._mw.maxDoubleSpinBox.editingFinished.connect(self.recalculate_histogram)
         
-        # creating a layout for the parameters to live in and aranging it nicely
-        self._hbox_layout = QtGui.QHBoxLayout()
-        self._hbox_layout.addWidget(self._bins_label)
-        self._hbox_layout.addWidget(self._bins_display)
-        self._hbox_layout.addStretch(1)
-        self._hbox_layout.addWidget(self._min_wavelength_label)
-        self._hbox_layout.addWidget(self._min_wavelength_display)
-        self._hbox_layout.addStretch(1)
-        self._hbox_layout.addWidget(self._max_wavelength_label)
-        self._hbox_layout.addWidget(self._max_wavelength_display)
-        self._hbox_layout.addStretch(1)
-        self._hbox_layout.addWidget(self._start_button)
-        self._hbox_layout.addStretch(1)
-        self._hbox_layout.addWidget(self._save_button)
-        self._hbox_layout.addWidget(self._stop_resume_button)
-        self._control_widget = QtGui.QWidget()
-        self._control_widget.setLayout(self._hbox_layout)
-        
-        # creating the label for the current counts and right alignment
-        self._wavelength_label = QtGui.QLabel('xxx')
-        self._wavelength_label.setFont(QtGui.QFont('Arial', 40, QtGui.QFont.Bold))
-        self._hbox_wavelength = QtGui.QHBoxLayout()
-        self._hbox_wavelength.addStretch(1)
-        self._hbox_wavelength.addWidget(self._wavelength_label)
-        self._wavelength_widget = QtGui.QWidget()
-        self._wavelength_widget.setLayout(self._hbox_wavelength)
-        
-        # creating the labels for the auto ranges
-        self._auto_min_label = QtGui.QLabel('Minimum: xxx.xxxxxx (nm)   ')
-        self._auto_max_label = QtGui.QLabel('Maximum: xxx.xxxxxx (nm)   ')
-        self._set_auto_range = QtGui.QPushButton('Set Auto Range')
-        self._set_auto_range.setFixedWidth(150)
-        self._set_auto_range.clicked.connect(self.set_auto_range)
-        
-        self._hbox_auto_range = QtGui.QHBoxLayout()
-        self._hbox_auto_range.addWidget(self._auto_min_label)
-        self._hbox_auto_range.addWidget(self._auto_max_label)
-        self._hbox_auto_range.addWidget(self._set_auto_range)
-        self._hbox_auto_range.addStretch(1)
-        
-        # combining the auto range with the plot
-        self._vbox_layout = QtGui.QVBoxLayout()
-        self._vbox_layout.addLayout(self._hbox_auto_range)
-        self._vbox_layout.addWidget(self._pw)
-        self._plot_widget = QtGui.QWidget()
-        self._plot_widget.setLayout(self._vbox_layout)
-        
-        # set up GUI with dock widgets
-        self._wavelength_dock_widget = QtGui.QDockWidget()
-        self._wavelength_dock_widget.setWidget(self._wavelength_widget)
-        self._wavelength_dock_widget.setWindowTitle("Laserscanning Wavelength")
-#        self._wavelength_dock_widget.setSizePolicy(QtGui.QSizePolicy.Minimum,QtGui.QSizePolicy.MinimumExpanding)
-        self._wavelength_dock_widget.setAllowedAreas(QtCore.Qt.AllDockWidgetAreas)
-        self._wavelength_dock_widget.setFeatures(QtGui.QDockWidget.AllDockWidgetFeatures)
-        self._mw.addDockWidget(QtCore.Qt.LeftDockWidgetArea, self._wavelength_dock_widget)
-        
-        self._plot_dock_widget = QtGui.QDockWidget()
-        self._plot_dock_widget.setWidget(self._plot_widget)
-        self._plot_dock_widget.setWindowTitle("Laserscanning Plot")
-        self._plot_dock_widget.setAllowedAreas(QtCore.Qt.AllDockWidgetAreas)
-        self._plot_dock_widget.setFeatures(QtGui.QDockWidget.AllDockWidgetFeatures)
-        self._mw.addDockWidget(QtCore.Qt.LeftDockWidgetArea, self._plot_dock_widget)
-        
-        self._control_dock_widget = QtGui.QDockWidget()
-        self._control_dock_widget.setWidget(self._control_widget)
-        self._control_dock_widget.setWindowTitle("Laserscanning Control")
-        self._control_dock_widget.setAllowedAreas(QtCore.Qt.AllDockWidgetAreas)
-        self._control_dock_widget.setFeatures(QtGui.QDockWidget.AllDockWidgetFeatures)
-        self._mw.addDockWidget(QtCore.Qt.LeftDockWidgetArea, self._control_dock_widget)
-
-       
-        # showing all the GUI elements to the window
-        self._mw.setDockNestingEnabled(True)
-        self._cw.hide()
         self._mw.show()
         
         ## Create an empty plot curve to be filled later, set its pen
@@ -208,6 +119,9 @@ class LaserScanningGui(GUIBase):
         
         self._scanning_logic.sig_data_updated.connect(self.updateData)
         
+    def deactivation(self, e):
+        self._mw.close()
+        
     def show(self):
         """Make window visible and put it above all other windows.
         """
@@ -218,37 +132,37 @@ class LaserScanningGui(GUIBase):
     def updateData(self):
         """ The function that grabs the data and sends it to the plot.
         """
-        
-        self._wavelength_label.setText('{0:,.6f} nm'.format(self._scanning_logic.current_wavelength))
-        self._auto_min_label.setText('Minimum: {0:3.6f} (nm)   '.format(self._scanning_logic.intern_xmin))
-        self._auto_max_label.setText('Maximum: {0:3.6f} (nm)   '.format(self._scanning_logic.intern_xmax))
+        self._mw.wavelengthLabel.setText('{0:,.6f} nm '.format(self._scanning_logic.current_wavelength))
+        self._mw.autoMinLabel.setText('Minimum: {0:3.6f} (nm)   '.format(self._scanning_logic.intern_xmin))
+        self._mw.autoMaxLabel.setText('Maximum: {0:3.6f} (nm)   '.format(self._scanning_logic.intern_xmax))
         
         x_axis = self._scanning_logic.histogram_axis
-        x_axis_hz = 3.0e17/(x_axis) \
-                - 6.0e17/(self._scanning_logic.get_max_wavelength() + self._scanning_logic.get_min_wavelength())
+        x_axis_hz = 3.0e17 / (x_axis) - 6.0e17 / (self._scanning_logic.get_max_wavelength() + self._scanning_logic.get_min_wavelength())
         
         self._curve1.setData(y=self._scanning_logic.histogram, x=x_axis)
         self._curve2.setData(y=self._scanning_logic.sumhisto, x=x_axis)
-        self._curve3.setData(y=self._scanning_logic.histogram, x=x_axis_hz)        
+        self._curve3.setData(y=self._scanning_logic.histogram, x=x_axis_hz)
         
         if self._scanning_logic.getState() == 'running':
-            self._stop_resume_button.setText('Stop')
-            self._start_button.setEnabled(False)
+            self._mw.actionStop_resume_scan.setText('Stop')
+            self._mw.actionStart_scan.setEnabled(False)
         else:
-            self._stop_resume_button.setText('Resume')
-            self._start_button.setEnabled(True)
+            self._mw.actionStop_resume_scan.setText('Resume')
+            self._mw.actionStart_scan.setEnabled(True)
 
     def stop_resume_clicked(self):
         """ Handling the Start button to stop and restart the counter.
         """
         if self._scanning_logic.getState() == 'running':
-            self._stop_resume_button.setText('Resume')
+            self._mw.actionStop_resume_scan.setText('Resume')
             self._scanning_logic.stop_scanning()
-            self._stop_resume_button.setEnabled(True)
+            self._mw.actionStop_resume_scan.setEnabled(True)
+            self._mw.binSpinBox.setEnabled(True)
         else:
-            self._stop_resume_button.setText('Stop')
+            self._mw.actionStop_resume_scan.setText('Stop')
             self._scanning_logic.start_scanning(resume=True)
-            self._start_button.setEnabled(False)
+            self._mw.actionStart_scan.setEnabled(False)
+            self._mw.binSpinBox.setEnabled(False)
             
     def start_clicked(self):
         """ Handling resume of the scanning without resetting the data.
@@ -257,9 +171,11 @@ class LaserScanningGui(GUIBase):
             self._scanning_logic.start_scanning()
 
             # Enable the stop button once a scan starts.
-            self._stop_resume_button.setEnabled(True)
+            self._mw.actionStop_resume_scan.setEnabled(True)
+            self._mw.binSpinBox.setEnabled(False)
+            self.recalculate_histogram()
         else:
-            print('Can not scan, since a scan is alredy running')
+            self.logMsg('Can not scan, since a scan is alredy running', msgType='error')
 
     def save_clicked(self):
         """ Handling the save button to save the data into a file.
@@ -278,14 +194,15 @@ class LaserScanningGui(GUIBase):
         self._scanning_logic.save_data()
             
     def recalculate_histogram(self):
-        self._scanning_logic.recalculate_histogram(\
-        bins=self._bins_display.value(),\
-        xmin=self._min_wavelength_display.value(),\
-        xmax=self._max_wavelength_display.value())
+        self._scanning_logic.recalculate_histogram(
+            bins = self._mw.binSpinBox.value(),
+            xmin = self._mw.minDoubleSpinBox.value(),
+            xmax = self._mw.maxDoubleSpinBox.value()
+        )
         
     def set_auto_range(self):
-        self._min_wavelength_display.setValue(self._scanning_logic.intern_xmin)
-        self._max_wavelength_display.setValue(self._scanning_logic.intern_xmax)
+        self._mw.minDoubleSpinBox.setValue(self._scanning_logic.intern_xmin)
+        self._mw.maxDoubleSpinBox.setValue(self._scanning_logic.intern_xmax)
         self.recalculate_histogram()
         
     ## Handle view resizing 
@@ -299,3 +216,4 @@ class LaserScanningGui(GUIBase):
         ## (probably this should be handled in ViewBox.resizeEvent)
         self._right_axis.linkedViewChanged(self._plot_item.vb, self._right_axis.XAxis)
         self._top_axis.linkedViewChanged(self._plot_item.vb, self._top_axis.YAxis)
+
