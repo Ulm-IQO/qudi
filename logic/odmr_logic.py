53fc82a5e37601a1220eb6f948e7410a01d9dcb1
Florian Frank <florian.frank@uni-ulm.de>
2015-07-28 13:53:05 +0000

    odmr has now dock widgets and a toolbar to start/stop the measurement

--- logic/odmr_logic.py
+++ logic/odmr_logic.py
@@ -1,6 +1,26 @@
 # -*- coding: utf-8 -*-
 # unstable: Christoph Müller
 
+"""
+This file contains the QuDi Logic module base class.
+
+QuDi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+QuDi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with QuDi. If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (C) 2015 Christoph Müller christoph-2.mueller@uni-ulm.de
+Copyright (C) 2015 Florian S. Frank florian.frank@uni-ulm.de
+"""
+
 from logic.generic_logic import GenericLogic
 from pyqtgraph.Qt import QtCore
 from core.util.mutex import Mutex
