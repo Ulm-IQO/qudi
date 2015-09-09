f48d8a298f709efce38036306cac7f26fa3605e0
Jan Binder <jan-git@nlogn.org>
2015-09-09 16:17:12 +0000

    work on task concept

---
+++ gui/taskrunner/taskgui.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+This file contains the QuDi logic module base class.
+
+QuDi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+QuDi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with QuDi. If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (C) 2015 Jan M. Binder jan.binder@uni-ulm.de
+"""
+
+import os
+import numpy as np
+from collections import OrderedDict
+from gui.guibase import GUIBase
+from pyqtgraph.Qt import QtCore, QtGui, uic
+
+class TaskGui(GUIBase):
+    """ A grephical interface to mofe switches by hand and change their calibration.
+    """
+    _modclass = 'TaskGui'
+    _modtype = 'gui'
+    ## declare connectors
+    _in = {'tasklogic': 'TaskRunner'}
+
+    def __init__(self, manager, name, config, **kwargs):
+        """ Create the switch control GUI.
+
+          @param object manager: Manager object that this module was loaded from
+          @param str name: Unique module name
+          @param dict config: Module configuration
+          @param dict kwargs: Optional arguments as a dict
+        """
+        c_dict = {'onactivate': self.initUI, 'ondeactivate': self.deactivation}
+        super().__init__(manager, name, config, c_dict)
+
+    def initUI(self, e=None):
+        """Create all UI objects and show the window.
+
+          @param object e: Fysom state change notice
+        """
+        self._mw = TaskMainWindow()
+        self.restoreWindowPos(self._mw)
+        self.show()
+       
+    def show(self):
+        """Make sure that the window is visible and at the top.
+        """
+        self._mw.show()
+ 
+    def deactivation(self, e=None):
+        """ Hide window and stop ipython console.
+          @param object e: Fysom state change notice
+        """
+        self.saveWindowPos(self._mw)
+        self._mw.close()
+
+
+class TaskMainWindow(QtGui.QMainWindow):
+    """ Helper class for window loaded from UI file.
+    """
+    def __init__(self):
+        """ Create the switch GUI window.
+        """
+        # Get the path to the *.ui file
+        this_dir = os.path.dirname(__file__)
+        ui_file = os.path.join(this_dir, 'ui_taskgui.ui')
+
+        # Load it
+        super().__init__()
+        uic.loadUi(ui_file, self)
+        self.show()
+
---
+++ logic/generic_task.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+"""
+This file contains the QuDi logic module base class.
+
+QuDi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+QuDi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with QuDi. If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (C) 2015 Jan M. Binder jan.binder@uni-ulm.de
+"""
+
+class TaskResult(QtCore.QObject):
+    def __init__(self):
+        super().__init__()
+        self.data = None
+        self.success = None
+
+    def updata(self, data, success=None):
+        self.data = data
+        self.success = success
+
+class InterruptableTask(QtCore.QObject, Fysom):
+    """ This class represents a task in a module that can be safely executed by checking preconditions
+        and pausing other tasks that are being executed as well.
+    """
+    sigPaused = QtCore.Signal()
+    sigResumed = QtCore.Signal()
+    sigExecutionStarted = QtCore.Signal()
+    sigExecutionFinished = QtCore.Signal()
+
+    def __init__(self, name, function, args=[], kwargs={}):
+        QtCore.QObject.__init__(self)
+        _default_callbacks = {'run': self._run, 'pause': self._pause, 'resume': self._resume, 'finish': self._finish}
+        _stateList = {
+            'initial': 'stopped',
+            'events': [
+                {'name': 'run', 'src': 'stopped', 'dst': 'running'},
+                {'name': 'pause', 'src': 'running', 'dst': 'paused'},
+                {'name': 'finish', 'src': 'running', 'dst': 'stopped'},
+                {'name': 'resume', 'src': 'paused', 'dst': 'running'}
+            ],
+            'callbacks': _default_callbacks
+        }
+        Fysom.__init__(self)
+        self.lock = Mutex()
+        self.name = name
+        self.func = function
+        self.args = args
+        self.interruptable = False
+        self.success = False
+
+    def makeInterruptable(self, pausefunction, resumefunction):
+        if callable(self.pausefunction) and callable(resumefunction):
+            self.pausefunction = pausefunction
+            self.resumefuncion = resumefunction
+            self.interruptable = True
+
+    def _run(self, e):
+        self.result = TaskResult()
+        try:
+            if callable(self.function):
+                 self.function(*self.args, **self.kwargs)
+            else:
+               self.result.update(None, False)
+        except Exception as e:
+            self.logMsg('Exception during task {}. {}'.format(self.name, e), msgType='error')
+            self.result.update(None, False)
+            
+                
+    def _pause(self, e):
+        try:
+            if callable(self.function):
+                 self.function(*self.args, **self.kwargs)
+            else:
+               self.result.update(None, False)
+        except Exception as e:
+            self.logMsg('Exception during task {}. {}'.format(self.name, e), msgType='error')
+            self.result.update(None, False)
+ 
+        self.sigPaused.emit()
+
+    def _resume(self, e):
+        try:
+            if callable(self.function):
+                 self.function(*self.args, **self.kwargs)
+            else:
+               self.result.update(None, False)
+        except Exception as e:
+            self.logMsg('Exception during task {}. {}'.format(self.name, e), msgType='error')
+            self.result.update(None, False)
+ 
+        self.sigResumed.eimit()
+
+    def _finish(self, e):
+        result.update(self.result, self.success)
+        self.sigFinished.emit()
+
+    def canPause(self):
+        return self.interruptable and self.can('pause')
+
+class PrePostTask(QtCore.QObject, Fysom):
+
+    sigPreExecStart = QtCore.Signal()
+    sigPreExecFinish = QtCore.Signal()
+    sigPostExecStart = QtCore.Signal()
+    sigPostExecFinish = QtCore.Signal()
+
+    def __init__(self, name, function, args=[]):
+        QtCore.QObject.__init__()
+        _default_callbacks = {'prerun': self.preExecute, 'postrun': self.postExecute}
+        _stateList = {
+            'initial': 'stopped',
+            'events': [
+                {'name': 'prerun', 'src': 'stopped', 'dst': 'paused'},
+                {'name': 'postrun', 'src': 'paused', 'dst': 'stopped'}
+            ],
+            'callbacks': _default_callbacks
+        }
+        Fysom.__init__()
+        self.lock = Mutex()
+        self.name = name
+        self.func = function
+        self.args = args
+
+    def preExecute(self):
+        self.sigPreExecStart.emit()
+
+        self.sigPreExecFinish.emit()
+
+    def postExecute(self):
+        self.sigPostExecStart.emit()
+
+        self.sigPostExecFinish.emit()
+
+    def canPause():
+        return False
