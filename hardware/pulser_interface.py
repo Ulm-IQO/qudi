63a4b6767b632bbdac6d2938e57e5c045f2f2dd1
Alexander Stark <alexander.stark@uni-ulm.de>
2015-11-22 18:49:29 +0000

    Some minor fixes and small doc strings adjustments in the files.

--- hardware/pulser_interface.py
+++ hardware/pulser_interface.py
@@ -41,8 +41,8 @@ class PulserInterface():
         hardware to the caller.
         Each constraint is a tuple of the form
             (min_value, max_value, stepsize)
-        and the key 'channel_map' indicates all possible combinations in usage
-        for this device.
+        and the key 'channel_map' indicates all possible combinations
+        (analogue, digital) in usage for this device.
 
         The possible keys in the constraint are defined here in the interface
         file. If the hardware does not support the values for the constraints,
@@ -64,16 +64,16 @@ class PulserInterface():
         return constraints
 
     def pulser_on(self):
-        """ Switches the pulsing device on. 
-        
+        """ Switches the pulsing device on.
+
         @return int: error code (0:OK, -1:error)
-        """ 
+        """
         raise InterfaceImplementationError('PulserInterface>pulser_on')
         return -1
-    
+
     def pulser_off(self):
-        """ Switches the pulsing device off. 
-        
+        """ Switches the pulsing device off.
+
         @return int: error code (0:OK, -1:error)
         """
         raise InterfaceImplementationError('PulserInterface>pulser_off')
@@ -81,13 +81,13 @@ class PulserInterface():
 
     def download_sequence(self, waveform, write_to_file = True):
         """ Convert the pre-sampled numpy array to a specific hardware file.
-        
+
         @param Waveform() waveform: The raw sampled pulse sequence.
         @param bool write_to_file: Flag to indicate if the samples should be
                                    written to a file (= True) or uploaded
                                    directly to the pulse generator channels
                                    (= False).
-        
+
         @return int: error code (0:OK, -1:error)
 
         Brings the numpy arrays containing the samples in the Waveform() object
@@ -96,13 +96,13 @@ class PulserInterface():
         """
         raise InterfaceImplementationError('PulserInterface>download_sequence')
         return -1
-        
+
     def send_file(self, filepath):
         """ Sends an already hardware specific waveform file to the pulse
             generators waveform directory.
-        
+
         @param string filepath: The file path of the source file
-        
+
         @return int: error code (0:OK, -1:error)
 
         Unused for digital pulse generators without sequence storage capability
@@ -113,11 +113,11 @@ class PulserInterface():
 
     def load_sequence(self, seq_name, channel=None):
         """ Loads a sequence to the specified channel of the pulsing device.
-        
+
         @param str seq_name: The name of the sequence to be loaded
         @param int channel: The channel for the sequence to be loaded into if
                             not already specified in the sequence itself
-        
+
         @return int: error code (0:OK, -1:error)
 
         Unused for digital pulse generators without sequence storage capability
@@ -125,13 +125,13 @@ class PulserInterface():
         """
         raise InterfaceImplementationError('PulserInterface>load_sequence')
         return -1
-        
+
     def clear_channel(self, channel=None):
         """ Clears the loaded waveform from the specified channel.
-        
+
         @param int channel: The channel to be cleared. If no channel is passed
                             all the channels will be cleared.
-        
+
         @return int: error code (0:OK, -1:error)
 
         Unused for digital pulse generators without sequence storage capability
@@ -157,42 +157,42 @@ class PulserInterface():
 
         raise InterfaceImplementationError('PulserInterface>get_status')
         return (-1, status_dic)
-    
+
     def set_sample_rate(self, sample_rate):
         """ Set the sample rate of the pulse generator hardware
-        
+
         @param float sample_rate: The sampling rate to be set (in Hz)
-        
+
         @return foat: the sample rate returned from the device (-1:error)
         """
         raise InterfaceImplementationError('PulserInterface>set_sampling_rate')
         return -1.
-        
+
     def get_sample_rate(self):
         """ Get the sample rate of the pulse generator hardware
-        
+
         @return float: The current sample rate of the device (in Hz)
         """
         raise InterfaceImplementationError('PulserInterface>get_sampling_rate')
         return -1
-        
+
     def set_amplitude(self, channel, amplitude):
         """ Set the output amplitude of the pulse generator hardware.
         Unused for purely digital hardware without logic level setting capability (DTG, FPGA, etc.).
-        
+
         @param int channel: The channel to be reconfigured
         @param float amplitude: The peak-to-peak amplitude the channel should be set to (in V)
-        
+
         @return int: error code (0:OK, -1:error)
         """
         raise InterfaceImplementationError('PulserInterface>set_amplitude')
         return -1
-        
+
     def get_amplitude(self, channel):
         """ Get the output amplitude of the pulse generator hardware.
-        
+
         @param int channel: The channel to be checked
-        
+
         @return float: The peak-to-peak amplitude the channel is set to (in V)
 
         Unused for purely digital hardware without logic level setting
@@ -200,21 +200,21 @@ class PulserInterface():
         """
         raise InterfaceImplementationError('PulserInterface>get_amplitude')
         return -1
-        
+
     def set_active_channels(self, d_ch=0, a_ch=0):
         """ Set the active channels for the pulse generator hardware.
-        
+
         @param int d_ch: number of active digital channels
         @param int a_ch: number of active analogue channels
-        
+
         @return int: error code (0:OK, -1:error)
         """
         raise InterfaceImplementationError('PulserInterface>set_active_channels')
         return -1
-        
+
     def get_active_channels(self):
         """ Get the active channels of the pulse generator hardware.
-        
+
         @return (int, int): number of active channels (analogue, digital)
         """
         raise InterfaceImplementationError('PulserInterface>get_active_channels')
@@ -257,7 +257,7 @@ class PulserInterface():
         """
         raise InterfaceImplementationError('PulserInterface>set_sequence_directory')
         return -1
-        
+
     def get_sequence_directory(self):
         """ Ask for the directory where the sequences are stored on the device.
 
@@ -268,12 +268,12 @@ class PulserInterface():
         """
         raise InterfaceImplementationError('PulserInterface>get_sequence_directory')
         return ''
-        
+
     def set_interleave(self, state=False):
         """ Turns the interleave of an AWG on or off.
-        
+
         @param bool state: The state the interleave should be set to (True: ON, False: OFF)
-        
+
         @return int: error code (0:OK, -1:error)
 
         Unused for pulse generator hardware other than an AWG.
@@ -295,27 +295,27 @@ class PulserInterface():
 
     def tell(self, command):
         """ Sends a command string to the device.
-        
+
         @param string question: string containing the command
-        
+
         @return int: error code (0:OK, -1:error)
         """
         raise InterfaceImplementationError('PulserInterface>tell')
         return -1
-    
+
     def ask(self, question):
         """ Asks the device a 'question' and receive and return an answer from it.
-        
+
         @param string question: string containing the command
-        
+
         @return string: the answer of the device to the 'question' in a string
         """
         raise InterfaceImplementationError('PulserInterface>ask')
         return ''
-        
+
     def reset(self):
         """Reset the device.
-        
+
         @return int: error code (0:OK, -1:error)
         """
         raise InterfaceImplementationError('PulserInterface>reset')
