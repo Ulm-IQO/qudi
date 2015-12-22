529d811c3d4d2e748274220acbcec66d744d3636
Jan Binder <jan-git@nlogn.org>
2015-12-22 15:57:41 +0000

    lots of preparation around startup and shutdown, which helps especially with ipython integration

--- start.py
+++ start.py
@@ -23,11 +23,30 @@ import subprocess
 import sys
 import os
 
-myenv = os.environ.copy()
+myenv = os.environ.copy() 
+
+if sys.platform == 'win32':
+    from .core.util.win_interrupt import create_interrupt_event
+    # Create a Win32 event for interrupting the kernel and store it in an environment variable.
+    interrupt_event = create_interrupt_event()
+    myenv["QUDI_INTERRUPT_EVENT"] = str(interrupt_event)
+    try:
+        from _winapi import DuplicateHandle, GetCurrentProcess, DUPLICATE_SAME_ACCESS, CREATE_NEW_PROCESS_GROUP
+    except:
+        from _subprocess import DuplicateHandle, GetCurrentProcess, DUPLICATE_SAME_ACCESS, CREATE_NEW_PROCESS_GROUP
+    pid = GetCurrentProcess()
+    handle = DuplicateHandle(pid, pid, pid, 0, True, DUPLICATE_SAME_ACCESS)
+    myenv['QUDI_PARENT_PID'] = str(int(handle))
+else:
+    myenv['QUDI_PARENT_PID'] = str(os.getpid())
+
 argv = [sys.executable, '-m', 'core'] + sys.argv[1:]
 
 while True:
     process = subprocess.Popen(argv, close_fds=False, env=myenv, stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr, shell=False)
+    if sys.platform == 'win32':
+        # Attach the interrupt event to the Popen objet so it can be used later.
+        process.win32_interrupt_event = interrupt_event
     try:
         retval = process.wait()
         if retval == 0:
