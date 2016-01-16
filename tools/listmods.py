a916e5621498dcb4ff16f032aebcbccc147f1bba
Jan Binder <jan-git@nlogn.org>
2016-01-16 15:56:14 +0000

    improve listmods

--- tools/listmods.py
+++ tools/listmods.py
@@ -4,8 +4,26 @@ import sys
 import fnmatch
 import importlib
 
-
 sys.path.append(os.getcwd())
+
+from core.base import Base
+from gui.guibase import GUIBase
+from logic.generic_logic import GenericLogic
+from logic.generic_task import InterruptableTask, PrePostTask
+
+
+def print_connectors(module):
+    if len(thing._in) > 0:
+        print('  IN:')
+        for conn in thing._in:
+            print('    {}: {}'.format(conn, thing._in[conn]))
+    if len(thing._out) > 0:
+        print('  OUT:')
+        for conn in thing._out:
+            print('    {}: {}'.format(conn, thing._out[conn]))
+    print('')
+    
+
 path = '.'
 if len(sys.argv) > 1:
     path = sys.argv[1]
@@ -34,15 +52,39 @@ for f in pyfiles:
     try:
         mod = importlib.__import__(f, fromlist=['*'])
         importsuccess.append(f)
+        thinglist = dir(mod)
+        for thingname in thinglist:
+            thing = getattr(mod, thingname)
+            try:
+                if issubclass(thing, GenericLogic) and thingname != 'GenericLogic':
+                    print('MODULE ' + f + '.' + thingname)
+                    print_connectors(thing)
+                elif issubclass(thing, GUIBase) and thingname != 'GUIBase':
+                    print('MODULE ' + f + '.' + thingname)
+                    print_connectors(thing)
+                elif issubclass(thing, InterruptableTask) and thingname != 'InterruptableTask' :
+                    print('INTASK ' + f + ' ' + thingname)
+                    print(thing.pauseTasks)
+                elif issubclass(thing, PrePostTask) and thingname != 'PerPostTask':
+                    print('PPTASK ' + f + ' ' + thingname)
+                elif issubclass(thing, Base) and thingname != 'Base' and thingname != 'GenericLogic' and thingname != 'GUIBase':
+                    print('MODULE ' + f + '.' + thingname)
+                    print_connectors(thing)
+                else:
+                    pass
+            except:
+                pass
     except ImportError as e:
         importerror.append([f, e])
     except Exception as e:
         othererror.append([f, e])
 
 for e in othererror:
-    print(e[0])
-    print(e[1])
+    print(e[0], file=sys.stderr)
+    print(e[1], file=sys.stderr)
 
 for e in importerror:
-    print(e[0])
-    print(e[1])
+    print(e[0], file=sys.stderr)
+    print(e[1], file=sys.stderr)
+
+
