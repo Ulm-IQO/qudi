5aa9fae2764a58dafabc2afb5e2c4ef42d6ba9e0
Jan Binder <jan-git@nlogn.org>
2016-01-22 18:57:21 +0000

    ipython integration

--- tools/qudikernel.py
+++ tools/qudikernel.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
+import os
+import sys
 import rpyc
 import time
-import sys
+import signal
+import atexit
+
+from parentpoller import ParentPollerUnix, ParentPollerWindows
 
 class QuDi(object):
 
@@ -9,6 +14,9 @@ class QuDi(object):
         self.host = 'localhost'
         self.port = 12345
         self.conn_config = {'allow_all_attrs': True}
+        self.parent_handle = int(os.environ.get('JPY_PARENT_PID') or 0)
+        self.interrupt = int(os.environ.get('JPY_INTERRUPT_EVENT') or 0)
+        self.kernelthread = None
 
     def connect(self, **kwargs):
         self.connection = rpyc.connect(self.host, self.port, config=self.conn_config)
@@ -18,12 +26,36 @@ class QuDi(object):
 
     def startKernel(self, connfile):
         m = self.getModule('rkernel')
-        m.startKernel(connfile)
+        self.kernelthread = m.startKernel(connfile)
+        print('Kernel up!')
+
+    def stopKernel(self):
+        print('Shutting down: ', self.kernelthread)
+        m = self.getModule('rkernel')
+        if self.kernelthread is not None:
+            m.stopKernel(self.kernelthread)
+            print('Down!')
+
+    def initSignal(self):
+        signal.signal(signal.SIGINT, signal.SIG_IGN)
+
+    def initPoller(self):
+        if sys.platform == 'win32':
+            if self.interrupt or self.parent_handle:
+                self.poller = ParentPollerWindows(self.interrupt, self.parent_handle)
+        elif self.parent_handle:
+            self.poller = ParentPollerUnix()
+
 
 if __name__ == '__main__':
     q = QuDi()
+    q.initSignal()
+    q.initPoller()
     q.connect()
     q.startKernel(sys.argv[1])
+    atexit.register(q.stopKernel)
+    print('Sleeping.')
+    q.poller.run()
+    print('Quitting.')
+    q.stopKernel()
     
-    while True:
-        time.sleep(1)
