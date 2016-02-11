72d695512c499c9ba658f628d206064da2e89103
Lachlan Rogers <lachlan.j.rogers@quantum.diamonds>
2016-02-11 22:50:05 +0000

    Substantial reversion of the conceptual design of the motor interface to preserve generality.  It is now more efficient and more abstract - so its easier to use properly across a wide range of motor applications.

--- interface/motor_interface.py
+++ interface/motor_interface.py
@@ -2,33 +2,32 @@
 
 from core.util.customexceptions import InterfaceImplementationError
 
+
 class MotorInterface():
-    """ This is the Interface class to define the controls for the simple 
-        step motor device. The actual hardware implementation might have a 
+
+    """ This is the Interface class to define the controls for the simple
+        step motor device. The actual hardware implementation might have a
         different amount of axis. Implement each single axis as 'private'
         methods for the hardware class, which get called by the general method.
     """
 
-    def move_rel(self,  x=None, y=None, z=None, phi=None):
+    def move_rel(self,  axis=None, distance=None):
         """ Moves stage in given direction (relative movement)
-        
-        @param float x: amount of relative movement in x direction in SI metric
-        @param float y: amount of relative movement in y direction in SI metric
-        @param float z: amount of relative movement in z direction in SI metric
-        @param float phi: amount of relative movement in phi direction in SI metric
-        
+
+        @param string axis: named axis of the motor hardware
+
+        @param float distance: amount of relative movement in SI metric
+
         @return int: error code (0:OK, -1:error)
         """
         raise InterfaceImplementationError('MagnetStageInterface>move_rel')
         return -1
 
-    def move_abs(self, x=None, y=None, z=None, phi=None):
+    def move_abs(self, axis=None, position=None):
         """ Moves stage to absolute position (absolute movement)
-        
-        @param float x: move to absolute position in x-direction in SI metric
-        @param float y: move to absolute position in y-direction in SI metric
-        @param float z: move to absolute position in z-direction in SI metric
-        @param float phi: move to absolute position in phi-direction in SI metric
+
+        @param string axis: named axis of the motor hardware
+        @param float position: move to absolute position in SI metric
 
         @return int: error code (0:OK, -1:error)
         """
@@ -37,58 +36,59 @@ class MotorInterface():
 
     def abort(self):
         """ Stops movement of the stage
-        
+
         @return int: error code (0:OK, -1:error)
         """
         raise InterfaceImplementationError('MagnetStageInterface>abort')
-        return -1 
-        
-    def get_pos(self):
+        return -1
+
+    def get_pos(self, axis=None):
         """ Gets current position of the stage arms
-        
-        @return dict: a dictionary with four entries. Each value can be 
-                      retrieved by the specific key.
+
+        @param string axis: named axis of the motor hardware
+        @return float: the position of motor
         """
-        raise InterfaceImplementationError('MagnetStageInterface>get_pos')        
-        return {'x' : 0.0, 'y' : 0.0, 'z' : 0.0, 'phi' : 0.0}
-        
+        raise InterfaceImplementationError('MagnetStageInterface>get_pos')
+        return -1
+
     def get_status(self):
         """ Get the status of the position
-        
-        @return int status: status of the stage      
+
+        @return int status: status of the stage
         """
         raise InterfaceImplementationError('MagnetStageInterface>get_status')
         return -1
-          
-    def calibrate(self, x=False, y=False, z=False, phi=False):
-        """ Calibrates the stage. 
 
-        After calibration the stage moves to home position which will be the 
+    def calibrate(self, axis=None):
+        """ Calibrates the stage.
+
+        After calibration the stage moves to home position which will be the
         zero point for the passed axis.
-        
+
+        @param string axis: named axis of the motor hardware
+
         @return int: error code (0:OK, -1:error)
         """
         raise InterfaceImplementationError('MagnetStageInterface>calibrate')
         return -1
-            
-    def get_velocity(self):
+
+    def get_velocity(self, axis=None):
         """ Gets the velocity of the given dimension
-        
-        @return dict:  dictionary with keys denoting the velocities and the 
-                       items representing their value. Always in SI.
+
+        @param string axis: named axis of the motor hardware
+
+        @return float: the velocities in SI.
         """
         raise InterfaceImplementationError('MagnetStageInterface>get_velocity')
-        return {'x_vel' : 0.0, 'y_vel' : 0.0, 'z_vel' : 0.0, 'phi_vel' : 0.0}
-        
-    def set_velocity(self, x_vel=None, y_vel=None, z_vel=None, phi_vel=None): 
+        return -1
+
+    def set_velocity(self, axis=None, velocity=None):
         """ Write new value for velocity.
 
-        @param float x_vel: velocity for x direction, dimension in SI metric
-        @param float y_vel: velocity for y direction, dimension in SI metric
-        @param float z_vel: velocity for z direction, dimension in SI metric
-        @param float phi_vel: velocity for phi direction, dimension in SI metric
-        
+        @param string axis: named axis of the motor hardware
+        @param float velocity: velocity, dimension in SI metric
+
         @return int: error code (0:OK, -1:error)
         """
         raise InterfaceImplementationError('MagnetStageInterface>set_velocity')
-        return -1        
+        return -1
