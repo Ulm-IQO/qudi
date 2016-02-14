13c85c92ceb194890dd8ae5e0335261effd43742
Alexander Stark <alexander.stark@uni-ulm.de>
2016-02-14 19:02:11 +0000

    Implement a new and more general version for a MotorInterface in order to address all possible axis combination. Add method get_constraints to retrieve all axis information. Add proper documentation for the interface methods.

--- interface/motor_interface.py
+++ interface/motor_interface.py
@@ -11,23 +11,89 @@ class MotorInterface():
         methods for the hardware class, which get called by the general method.
     """
 
-    def move_rel(self,  axis=None, distance=None):
+    def get_constraints(self):
+        """ Retrieve the hardware constrains from the motor device.
+
+        @return dict: dict with constraints for the magnet hardware. These 
+                      constraints will be passed via the logic to the GUI so 
+                      that proper display elements with boundary conditions
+                      could be made.
+
+        Provides all the constraints for each axis of a motorized stage 
+        (like total travel distance, velocity, ...)
+        Each axis has its own dictionary, where the label is used as the 
+        identifier throughout the whole module. The dictionaries for each axis
+        are again grouped together in a constraints dictionary in the form
+
+            {'<label_axis0>': axis0 }
+
+        where axis0 is again a dict with the possible values defined below. The
+        possible keys in the constraint are defined here in the interface file.
+        If the hardware does not support the values for the constraints, then 
+        insert just None. If you are not sure about the meaning, look in other 
+        hardware files to get an impression.
+        """
+        constraints = {}
+
+        axis0 = {}
+        axis0['label'] = 'x'        # that axis label should be obtained from config
+        axis0['unit'] = 'm'        # the SI units
+        axis0['ramp'] = ['Sinus','Linear'], # a possible list of ramps
+        axis0['pos_min'] = 0,
+        axis0['pos_max'] = 100,  # that is basically the traveling range
+        axis0['pos_step'] = 100,  
+        axis0['vel_min'] = 0,
+        axis0['vel_max'] = 100,
+        axis0['vel_step'] = 0.01,
+        axis0['acc_min'] = 0.1
+        axis0['acc_max'] = 0.0
+        axis0['acc_step'] = 0.0
+
+        axis1 = {}
+        axis1['label'] = 'phi'      # that axis label should be obtained from config
+        axis1['unit'] = '°'        # the SI units
+        axis1['ramp'] = ['Sinus','Trapez'], # a possible list of ramps
+        axis1['pos_min'] = 0,
+        axis1['pos_max'] = 360,  # that is basically the traveling range
+        axis1['pos_step'] = 100,  
+        axis1['vel_min'] = 1,
+        axis1['vel_max'] = 20,
+        axis1['vel_step'] = 0.1,
+        axis1['acc_min'] = None
+        axis1['acc_max'] = None
+        axis1['acc_step'] = None
+
+        # assign the parameter container for x to a name which will identify it
+        constraints[axis0['label']] = axis0
+        constraints[axis1['label']] = axis1  
+
+        raise InterfaceImplementationError('PulserInterface>get_constraints')
+        return constraints
+
+    def move_rel(self,  param_dict):
         """ Moves stage in given direction (relative movement)
 
-        @param string axis: named axis of the motor hardware
+        @param dict param_dict: dictionary, which passes all the relevant 
+                                parameters, which should be changed. Usage:
+                                 {'axis_label': <the-abs-pos-value>}.
+                                 'axis_label' must correspond to a label given
+                                 to one of the axis.
 
-        @param float distance: amount of relative movement in SI metric
+        A smart idea would be to ask the position after the movement.
 
         @return int: error code (0:OK, -1:error)
         """
         raise InterfaceImplementationError('MagnetStageInterface>move_rel')
         return -1
 
-    def move_abs(self, axis=None, position=None):
+    def move_abs(self, param_dict):
         """ Moves stage to absolute position (absolute movement)
 
-        @param string axis: named axis of the motor hardware
-        @param float position: move to absolute position in SI metric
+        @param dict param_dict: dictionary, which passes all the relevant 
+                                parameters, which should be changed. Usage:
+                                 {'axis_label': <the-abs-pos-value>}.
+                                 'axis_label' must correspond to a label given
+                                 to one of the axis.
 
         @return int: error code (0:OK, -1:error)
         """
@@ -42,53 +108,78 @@ class MotorInterface():
         raise InterfaceImplementationError('MagnetStageInterface>abort')
         return -1
 
-    def get_pos(self, axis=None):
+    def get_pos(self, param_list=None):
         """ Gets current position of the stage arms
 
-        @param string axis: named axis of the motor hardware
-        @return float: the position of motor
+        @param list param_list: optional, if a specific position of an axis
+                                is desired, then the labels of the needed
+                                axis should be passed in the param_list.
+                                If nothing is passed, then from each axis the 
+                                position is asked.
+
+        @return dict: with keys being the axis labels and item the current 
+                      position.
         """
         raise InterfaceImplementationError('MagnetStageInterface>get_pos')
         return -1
 
-    def get_status(self):
+    def get_status(self, param_list=None):
         """ Get the status of the position
 
-        @return int status: status of the stage
+        @param list param_list: optional, if a specific status of an axis
+                                is desired, then the labels of the needed
+                                axis should be passed in the param_list.
+                                If nothing is passed, then from each axis the 
+                                status is asked.
+
+        @return dict: with the axis label as key and the status number as item. 
         """
         raise InterfaceImplementationError('MagnetStageInterface>get_status')
         return -1
 
-    def calibrate(self, axis=None):
+    def calibrate(self, param_list=None):
         """ Calibrates the stage.
 
-        After calibration the stage moves to home position which will be the
-        zero point for the passed axis.
-
-        @param string axis: named axis of the motor hardware
+        @param dict param_list: param_list: optional, if a specific calibration
+                                of an axis is desired, then the labels of the 
+                                needed axis should be passed in the param_list.
+                                If nothing is passed, then all connected axis 
+                                will be calibrated.
 
         @return int: error code (0:OK, -1:error)
+
+        After calibration the stage moves to home position which will be the
+        zero point for the passed axis. The calibration procedure will be 
+        different for each stage.
         """
         raise InterfaceImplementationError('MagnetStageInterface>calibrate')
         return -1
 
-    def get_velocity(self, axis=None):
-        """ Gets the velocity of the given dimension
+    def get_velocity(self, param_list=None):
+        """ Gets the current velocity for all connected axes.
 
-        @param string axis: named axis of the motor hardware
+        @param dict param_list: optional, if a specific velocity of an axis
+                                is desired, then the labels of the needed
+                                axis should be passed as the param_list.
+                                If nothing is passed, then from each axis the 
+                                velocity is asked.
 
-        @return float: the velocities in SI.
+        @return dict : with the axis label as key and the velocity as item.
         """
         raise InterfaceImplementationError('MagnetStageInterface>get_velocity')
         return -1
 
-    def set_velocity(self, axis=None, velocity=None):
+    def set_velocity(self, param_dict):
         """ Write new value for velocity.
 
-        @param string axis: named axis of the motor hardware
-        @param float velocity: velocity, dimension in SI metric
+        @param dict param_dict: dictionary, which passes all the relevant 
+                                parameters, which should be changed. Usage:
+                                 {'axis_label': <the-velocity-value>}.
+                                 'axis_label' must correspond to a label given
+                                 to one of the axis.
 
         @return int: error code (0:OK, -1:error)
         """
         raise InterfaceImplementationError('MagnetStageInterface>set_velocity')
         return -1
+
