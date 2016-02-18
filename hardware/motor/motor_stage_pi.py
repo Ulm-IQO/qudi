7d09a32c7ffed1b347ca1873be923c0380424cbd
Alexander Stark <alexander.stark@uni-ulm.de>
2016-02-18 23:46:21 +0000

    Change the PI stage towards the Interface definitions; correct mostly typos. But still a lot changes has to be done for Interface adaption.

--- hardware/motor/motor_stage_pi.py
+++ hardware/motor/motor_stage_pi.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """
-This file contains the QuDi hardware interface for fast counting devices.
+This file contains the hardware control of the motorized stage for PI.
 
 QuDi is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -17,28 +17,35 @@ You should have received a copy of the GNU General Public License
 along with QuDi. If not, see <http://www.gnu.org/licenses/>.
 
 Copyright (C) 2015 Christoph Mueller christoph-1.mueller@uni-ulm.de
+Copyright (C) 2016 Alexander Stark alexander.stark@uni-ulm.de
 """
 
-class MagnetStagePI(Base, MotorInterface):
+import visa
+import time
+
+from core.base import Base
+from interface.motor_interface import MotorInterface
+
+class MotorStagePI(Base, MotorInterface):
     """unstable: Christoph Müller
-    This is the Interface class to define the controls for the simple 
+    This is the Interface class to define the controls for the simple
     microwave hardware.
     """
-    _modclass = 'MagnetStagePI'
+    _modclass = 'MotorStagePI'
     _modtype = 'hardware'
     # connectors
     _out = {'magnet': 'MotorInterface'}
-    
+
     def __init__(self, manager, name, config, **kwargs):
-        cb_dict = {'onactivate': self.activation, 
+        cb_dict = {'onactivate': self.activation,
                    'ondeactivate': self.deactivation}
         Base.__init__(self, manager, name, config, cb_dict)
 
         #axis definition:
         self._x_axis = '1'
-        slef._y_axis = '3'
+        self._y_axis = '3'
         self._z_axis = '2'
-        
+
         #ranges of axis:
         self._min_x = -100. * 10000.
         self._max_x = 100. * 10000.
@@ -46,19 +53,19 @@ class MagnetStagePI(Base, MotorInterface):
         self._max_y = 100. * 10000.
         self._min_z = -100. * 10000.
         self._max_z = 100. * 10000.
-        
+
         #Translationfactor
         self.MicroStepSize = 0.000234375
-        
+
         #!!!!NOTE:  vielleicht sollte überall .ask anstatt .write genommen werden, das die stage glaube ich immer was zurückgibt....
-   
+
     def activation(self, e):
         self.rm = visa.ResourceManager()
         self._serial_connection_xyz = self.rm.open_resource('COM1', baud_rate=9600, timeout=1)            #magnet xyz-stage
         self._serial_connection_rot = self.rm.open_resource("COM6", baud_rate=9600, timeout=5)            #magnet rot-stage   TIMEOUT shorter?
         self._serial_connection_xyz.term_chars = '\n'
         self._serial_connection_rot.term_chars = '\n'
-    
+
     def deactivation(self, e):
         self._serial_connection_xyz.close()
         self._serial_connection_rot.close()
@@ -66,12 +73,12 @@ class MagnetStagePI(Base, MotorInterface):
 
     def step(self, x = None, y = None, z = None, phi = None):
         """Moves stage in given direction (relative movement)
-        
+
         @param float x: amount of realtive movement in x direction
         @param float y: amount of realtive movement in y direction
         @param float z: amount of realtive movement in z direction
         @param float phi: amount of realtive movement in phi direction
-        
+
         @return int: error code (0:OK, -1:error)
         """
         if x != None:
@@ -100,26 +107,26 @@ class MagnetStagePI(Base, MotorInterface):
                 self._go_to_pos(self._x_axis, move)
         if phi != None:
             self._move_relative_rot(step)
-        
+
         return 0
-       
-    
+
+
 
     def abort(self):
         """Stops movement of the stage
-        
+
         @return int: error code (0:OK, -1:error)
         """
         self._serial_connection_xyz.write(self._x_axis+'AB\n')
         self._serial_connection_xyz.write(self._y_axis+'AB\n')
         self._serial_connection_xyz.write(self._z_axis+'AB\n')
         self._write_rot([1,23,0])
-        return 0 
-        
-    
+        return 0
+
+
     def get_pos(self):
         """Gets current position of the stage arms
-        
+
         @return float x: current x stage position
         @return float y: current y stage position
         @return float z: current z stage position
@@ -127,29 +134,29 @@ class MagnetStagePI(Base, MotorInterface):
         """
         x = int(self._serial_connection_xyz.ask(self._x_axis+'TT')[8:])/100000.
         y = int(self._serial_connection_xyz.ask(self._y_axis+'TT')[8:])/100000.
-        z = int(self._serial_connection_xyz.ask(self._z_axis+'TT')[8:])/100000.         
+        z = int(self._serial_connection_xyz.ask(self._z_axis+'TT')[8:])/100000.
         phi_temp = self._ask_rot([1,60,0])
-        phi = phi_temp * self.MicroStepSize           
+        phi = phi_temp * self.MicroStepSize
         return x, y, z, phi
-        
-     
+
+
     def get_status(self):
         """Get the status of the position
-        
-        @return int status: status of the stage      
+
+        @return int status: status of the stage
         """
         raise InterfaceImplementationError('MWInterface>get_status')
         return -1
-        
-    
+
+
     def move(self, x = None, y = None, z = None, phi = None):
         """Moves stage to absolute position
-        
+
         @param float x: move to absolute position in x-direction
         @param float y: move to absolute position in y-direction
         @param float z: move to absolute position in z-direction
         @param float phi: move to absolute position in phi-direction
-        
+
         @return int: error code (0:OK, -1:error)
         """
         if x != None:
@@ -170,69 +177,69 @@ class MagnetStagePI(Base, MotorInterface):
                 print('out of range, choose smaller step')
             else:
                 self._go_to_pos(self._x_axis, move)
-            
+
         [a,b,c] = self._in_movement_xyz()
         while a != 0 or b != 0 or c != 0:
             print ('xyz-stage moving...')
             [a,b,c] = self._in_movement_xyz()
             time.sleep(0.2)
-            
-        if phi != None:                
+
+        if phi != None:
             movephi = phi
             self._move_absolute_rot(movephi)
-            
+
         print ('stage ready')
         return 0
-        
-        
+
+
     def calibrate_x(self):
-        """Calibrates the x-direction of the stage. 
+        """Calibrates the x-direction of the stage.
         For this it moves to the point zero in x.
-        
+
         @return int: error code (0:OK, -1:error)
         """
         raise InterfaceImplementationError('MagnetStageInterface>calibrate_x')
         return -1
-            
-    
+
+
     def calibrate_y(self):
-        """Calibrates the y-direction of the stage. 
+        """Calibrates the y-direction of the stage.
         For this it moves to the point zero in y.
-        
+
         @return int: error code (0:OK, -1:error)
         """
         raise InterfaceImplementationError('MagnetStageInterface>calibrate_y')
         return -1
-            
-    
+
+
     def calibrate_z(self):
-        """Calibrates the z-direction of the stage. 
+        """Calibrates the z-direction of the stage.
         For this it moves to the point zero in z.
-        
+
         @return int: error code (0:OK, -1:error)
         """
         raise InterfaceImplementationError('MagnetStageInterface>calibrate_z')
         return -1
-    
-            
+
+
     def calibrate_phi(self):
-        """Calibrates the phi-direction of the stage. 
+        """Calibrates the phi-direction of the stage.
         For this it turns to the point zero in phi.
-        
+
         @return int: error code (0:OK, -1:error)
-        
+
         moving the rotation stage to its home position; per default 0 degree
-        """        
+        """
         self._write_rot([1,1,0])
-        self._in_movement_rot()       # waits until rot_stage finished its move        
+        self._in_movement_rot()       # waits until rot_stage finished its move
         return 0
-    
-    
+
+
     def get_velocity(self, dimension = 'x'):
         """ Gets the velocity of the given dimension
-        
+
         @param str dimension: name of chosen dimension
-        
+
         @return float velocity: velocity of chosen dimension
         """
         """Get the velocity of x and y"""
@@ -246,14 +253,14 @@ class MagnetStagePI(Base, MotorInterface):
             data = self._ask_rot([1,53,42])
             vel = data_to_speed_phi(data)
         return vel
-        
-        
+
+
     def set_velocity(self, dimension = 'x', vel = 0.):
         """Write new value for velocity in chosen dimension
-        
+
         @param str dimension: name of chosen dimension
         @param float vel: velocity for chosen dimension
-        
+
         @return int: error code (0:OK, -1:error)
         """
         if dimension == 'x':
@@ -265,13 +272,13 @@ class MagnetStagePI(Base, MotorInterface):
         if dimension == 'phi':
             data = speed_to_data_phi(vel)
             self._write_rot([1,42,data])
-        return 0        
+        return 0
 
-    
+
 ########################## internal methods ##################################
-    
+
     def _write_rot(self, inst):  # requires [1, commandnumber, value]
-        ''' sending a command to the rotation stage, 
+        ''' sending a command to the rotation stage,
         requires [1, commandnumber, value]'''
         x = inst[0]
         y = inst[1]
@@ -281,7 +288,7 @@ class MagnetStagePI(Base, MotorInterface):
         z2 = 0
         z1 = 0
         base = 256
-            
+
         if z >= 0:
             if z/base**3 >= 1:
                 z4 = int(z/base**3)   #since  int(8.9999)=8  !
@@ -303,15 +310,15 @@ class MagnetStagePI(Base, MotorInterface):
                 z2 = int(z/base)
                 z -= z2*base
             z1 = z
-            
+
         sends = [x,y,z1,z2,z3,z4]
             # send instruction
             # inst must be a list of 6 bytes (no error checking)
         for i in range (6):
             self._serial_connection_rot.write(chr(sends[i]))
         return
-        
-        
+
+
     def _ask_rot(self):
         '''receiving an answer from the rotation stage'''
         # return 6 bytes from the receive buffer
@@ -320,18 +327,18 @@ class MagnetStagePI(Base, MotorInterface):
         for i in range (6):
             r[i] = ord(ser_rot.read(1))
         #x=r[0]
-        y = r[1]        
+        y = r[1]
         z1 = r[2]
         z2 = r[3]
         z3 = r[4]
         z4 = r[5]
         q = z1+z2*256+z3*256**2+z4*256**3
-        
+
         if y == 255:
-            print(('error nr. ' + str(q)))            
+            print(('error nr. ' + str(q)))
         return q
-        
-    
+
+
     def _in_movement_rot(self):
         st = self._ask_rot([1,54,0])
         while st != 0:
@@ -339,68 +346,68 @@ class MagnetStagePI(Base, MotorInterface):
             st = self._ask_rot([1,54,0])
             time.sleep(0.1)
         print ('rotation stage stopped. ready')
-        
-        
+
+
     def _in_movement_xyz(self):
         '''this method checks if the magnet is still in movement and returns
-        a list which of the axis are moving. Ex: return is [1,1,0]-> x and y ax are in movement and z axis is imobile.            
+        a list which of the axis are moving. Ex: return is [1,1,0]-> x and y ax are in movement and z axis is imobile.
         '''
         tmpx = self._serial_connection_xyz.ask(self._x_axis+'TS')[8:]
         time.sleep(0.1)
         tmpy = self._serial_connection_xyz.ask(self._y_axis+'TS')[8:]
         time.sleep(0.1)
         tmpz = self._serial_connection_xyz.ask(self._z_axis+'TS')[8:]
-        time.sleep(0.1)  
-        return [tmpx%2,tmpy%2,tmpz%2]        
+        time.sleep(0.1)
+        return [tmpx%2,tmpy%2,tmpz%2]
         del tmpx,tmpy,tmpz
-    
+
 
     def _move_absolute_rot(self, value):
         '''moves the rotation stage to an absolut position; value in degrees'''
         data = int(value/self.MicroStepSize)
         self.write_rot([1,20,data])
         self._in_movement_rot()         # waits until rot_stage finished its move
-    
-    
+
+
     def _move_relative_rot(self, value):
         '''moves the rotation stage by a relative value in degrees'''
         data = int(value/self.MicroStepSize)
         self.write_rot([1,21,data])
-        self._in_movement_rot()         # waits until rot_stage finished its move 
-    
-    
+        self._in_movement_rot()         # waits until rot_stage finished its move
+
+
     def _data_to_speed_rot(self, data):
         speed = data * 9.375 * self.MicroStepSize
         return speed
-        
-        
+
+
     def _speed_to_data_rot(self, speed):
         data = int(speed / 9.375 / self.MicroStepSize)
         return data
-        
+
     def _go_to_pos(self, axis = None, move = None):
         """moves one axis to an absolute position
-        
+
         @param string axis: ID of the axis, '1', '2' or '3'
         @param int move: absolute position
         """
         self._serial_connection_xyz.write(self._x_axis+'SP%s'%move)
         self._serial_connection_xyz.write(self._x_axis+'MP')
-        
-    
-    
-    
+
+
+
+
 ##################################################################################################################################################
 ##################################################################################################################################################
 ##################################################################################################################################################
 
-      
+
     def CalibrateXYZ():
         test_open()
         ser.write('123MA-2500000\n')
         time.sleep(1)
         ser.close()
-            
+
         [a,b,c] = in_movement()
         while a != 0 or b != 0 or c !=0 :
             print('moving to the corner...')
@@ -410,13 +417,13 @@ class MagnetStagePI(Base, MotorInterface):
             print('moving on z-Axis: ', c,'\n')
             time.sleep(0.5)
         ##############################################
-        print('in edge') 
+        print('in edge')
         test_open()
-        ser.write('123DH\n')       
+        ser.write('123DH\n')
         ser.write('123MA900000\n')
         time.sleep(.1)
-        print(str(ser.read(17)))     
-        print('define the tmps')                                           
+        print(str(ser.read(17)))
+        print('define the tmps')
         ser.close()
         ###############################################
         [a,b,c] = in_movement()
@@ -429,13 +436,13 @@ class MagnetStagePI(Base, MotorInterface):
             time.sleep(.5)
         ####################################################
         print('fast movement finished')
-            
+
         time.sleep(0.1)
         test_open()
-        ser.write('13FE1\n')    
-        print(ser.read(6)) 
+        ser.write('13FE1\n')
+        print(ser.read(6))
         ser.close()
-        [a,b,c] = in_movement()                                   
+        [a,b,c] = in_movement()
         while a != 0 or b != 0 or c != 0:
             print('find centerposition...')
             [a,b,c] = in_movement()
@@ -450,9 +457,9 @@ class MagnetStagePI(Base, MotorInterface):
         #######################################################
         print('calibration finished')
         GetPos()
-            
-    
-    
+
+
+
     def get_status():
         """Get the status of the position"""
         try:
@@ -466,12 +473,12 @@ class MagnetStagePI(Base, MotorInterface):
                 statusB=0
             else:
                 statusB=1
-    
+
         except:
             statusA =-1
             statusB = -1
         return (statusA,statusB)
-        
+
     def get_status_phi():
         ser_rot.open()
         send_rot([1,54,0])
