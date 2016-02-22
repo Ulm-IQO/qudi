cd4fca3e0beff54c512659a73bb03cdb2d43de16
Alexander Stark <alexander.stark@uni-ulm.de>
2016-02-22 17:39:19 +0000

    Adjust the SlowCounterInterfaceDummy slightly to be able to use it also as a gated counter.

--- hardware/slow_counter_interface_dummy.py
+++ hardware/slow_counter_interface_dummy.py
@@ -9,7 +9,7 @@ import time
 import numpy as np
 
 class SlowCounterInterfaceDummy(Base,SlowCounterInterface):
-    """This is the Interface class to define the controls for the simple 
+    """This is the Interface class to define the controls for the simple
     microwave hardware.
     """
     _modclass = 'slowcounterinterface'
@@ -20,15 +20,15 @@ class SlowCounterInterfaceDummy(Base,SlowCounterInterface):
     def __init__(self, manager, name, config, **kwargs):
         c_dict = {'onactivate': self.activation, 'ondeactivate': self.deactivation}
         Base.__init__(self, manager, name, configuration=config, callbacks = c_dict)
-        
-        self.logMsg('The following configuration was found.', 
+
+        self.logMsg('The following configuration was found.',
                     msgType='status')
-                    
+
         # checking for the right configuration
         for key in config.keys():
-            self.logMsg('{}: {}'.format(key,config[key]), 
+            self.logMsg('{}: {}'.format(key,config[key]),
                         msgType='status')
-    
+
     def activation(self, e):
         config = self.getConfiguration()
         if 'clock_frequency' in config.keys():
@@ -37,7 +37,7 @@ class SlowCounterInterfaceDummy(Base,SlowCounterInterface):
             self._clock_frequency=100
             self.logMsg('No clock_frequency configured taking 100 Hz instead.', \
             msgType='warning')
-            
+
         if 'samples_number' in config.keys():
             self._samples_number=config['samples_number']
         else:
@@ -56,95 +56,96 @@ class SlowCounterInterfaceDummy(Base,SlowCounterInterface):
 
     def deactivation(self, e):
         pass
-    
+
     def set_up_clock(self, clock_frequency = None, clock_channel = None):
-        """ Configures the hardware clock of the NiDAQ card to give the timing. 
-        
+        """ Configures the hardware clock of the NiDAQ card to give the timing.
+
         @param float clock_frequency: if defined, this sets the frequency of the clock
         @param string clock_channel: if defined, this is the physical channel of the clock
-        
+
         @return int: error code (0:OK, -1:error)
-        """ 
-        
+        """
+
         if clock_frequency != None:
             self._clock_frequency = float(clock_frequency)
-            
-        self.logMsg('slowcounterinterfacedummy>set_up_clock', 
+
+        self.logMsg('slowcounterinterfacedummy>set_up_clock',
                     msgType='warning')
-                    
+
         time.sleep(0.1)
-        
+
         return 0
-    
-    
-    def set_up_counter(self, 
-                       counter_channel = None, 
-                       photon_source = None, 
+
+
+    def set_up_counter(self,
+                       counter_channel = None,
+                       photon_source = None,
                        counter_channel2=None,
                        photon_source2=None,
-                       clock_channel = None):
-        """ Configures the actual counter with a given clock. 
-        
+                       clock_channel = None,
+                       counter_buffer=None):
+        """ Configures the actual counter with a given clock.
+
         @param string counter_channel: if defined, this is the physical channel of the counter
         @param string photon_source: if defined, this is the physical channel where the photons are to count from
         @param string clock_channel: if defined, this specifies the clock for the counter
-        
+
         @return int: error code (0:OK, -1:error)
         """
-        
-        self.logMsg('slowcounterinterfacedummy>set_up_counter', 
+
+        self.logMsg('slowcounterinterfacedummy>set_up_counter',
                     msgType='warning')
-                    
+
         time.sleep(0.1)
-        
+
         return 0
-        
-        
+
+
     def get_counter(self, samples=None):
-        """ Returns the current counts per second of the counter. 
-        
+        """ Returns the current counts per second of the counter.
+
         @param int samples: if defined, number of samples to read in one go
-        
+
         @return float: the photon counts per second
         """
-        
-#        self.logMsg('slowcounterinterfacedummy>get_counter', 
+
+#        self.logMsg('slowcounterinterfacedummy>get_counter',
 #                    msgType='warning')
-                    
+
         if samples == None:
             samples = int(self._samples_number)
         else:
             samples = int(samples)
-        
+
         count_data = np.empty([2,samples], dtype=np.uint32) # count data will be written here in the NumPy array
-        
+
         for i in range(samples):
             count_data[0][i] = self.mean_signal + random.uniform(-self.noise_amplitude/2, self.noise_amplitude/2)
-        
+
         if self._photon_source2 is not None:
             for i in range(samples):
                 count_data[1][i] = random.uniform(0, 1e5)
-            
+
         time.sleep(1./self._clock_frequency*samples)
-        
+
         return count_data
-    
+
     def close_counter(self):
-        """ Closes the counter and cleans up afterwards. 
-        
+        """ Closes the counter and cleans up afterwards.
+
         @return int: error code (0:OK, -1:error)
         """
-        
-        self.logMsg('slowcounterinterfacedummy>close_counter', 
+
+        self.logMsg('slowcounterinterfacedummy>close_counter',
                     msgType='warning')
         return 0
-        
+
     def close_clock(self,power=0):
-        """ Closes the clock and cleans up afterwards. 
-        
+        """ Closes the clock and cleans up afterwards.
+
         @return int: error code (0:OK, -1:error)
         """
-        
-        self.logMsg('slowcounterinterfacedummy>close_clock', 
+
+        self.logMsg('slowcounterinterfacedummy>close_clock',
                     msgType='warning')
         return 0
