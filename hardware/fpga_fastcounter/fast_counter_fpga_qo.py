e1bcceae7fed1f7bee5a89c20d9fcdbd872a9cfe
Alexander Stark <alexander.stark@uni-ulm.de>
2016-02-27 16:40:05 +0000

    Adapt the hardware file of the FPGA for the two possible types of bitfiles. Add the additional configuration item in the various config file.

--- hardware/fpga_fastcounter/fast_counter_fpga_qo.py
+++ hardware/fpga_fastcounter/fast_counter_fpga_qo.py
@@ -1,12 +1,33 @@
 # -*- coding: utf-8 -*-
 
+"""
+This file contains the QuDi hardware file for the FPGA based fast counter.
+
+QuDi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+QuDi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with QuDi. If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (C) 2016 Nikolas Tomek nikolas.tomek@uni-ulm.de
+Copyright (C) 2016 Alexander Stark alexander.stark@uni-ulm.de
+"""
+
+import numpy as np
+import struct
+import os
 
 from interface.fast_counter_interface import FastCounterInterface
 from core.base import Base
-import numpy as np
 import thirdparty.opal_kelly as ok
-import struct
-import os
+
 
 class FastCounterFPGAQO(Base, FastCounterInterface):
     """ unstable: Nikolas Tomek
@@ -18,21 +39,52 @@ class FastCounterFPGAQO(Base, FastCounterInterface):
     # declare connectors
     _out = {'fastcounter': 'FastCounterInterface'}
 
-    def __init__(self, manager, name, config = {}, **kwargs):
+    def __init__(self, manager, name, config={}, **kwargs):
         callback_dict = {'onactivate': self.activation,
                          'ondeactivate': self.deactivation}
         Base.__init__(self, manager, name, config, callback_dict, **kwargs)
 
     def activation(self, e):
+        """ Connect and configure the access to the FPGA.
+
+        @param object e: Event class object from Fysom.
+                         An object created by the state machine module Fysom,
+                         which is connected to a specific event (have a look in
+                         the Base Class). This object contains the passed event
+                         the state before the event happens and the destination
+                         of the state which should be reached after the event
+                         has happen.
+        """
+
         config = self.getConfiguration()
 
         if 'fpgacounter_serial' in config.keys():
-            self._serial=config['fpgacounter_serial']
+            self._serial = config['fpgacounter_serial']
         else:
-            self.logMsg('No serial number defined for fpga counter',
+            self.logMsg('No parameter "fpgacounter_serial" specified  in the '
+                        'config! Set the serial number for the currently used '
+                        'fpga counter!\n'
+                        'Open the Opal Kelly Frontpanel to obtain the serial '
+                        'number of the connected FPGA.\n'
+                        'Do not forget to close the Frontpanel before starting '
+                        'the QuDi program.', msgType='error')
+
+        if 'fpga_type' in config.keys():
+            self._fpga_type = config['fpga_type']
+        else:
+            self._fpga_type = 'XEM6310_LX150'
+            self.logMsg('No parameter "fpga_type" specified in the config!\n'
+                        'Possible types are "XEM6310_LX150" or '
+                        '"XEM6310_LX45".\n'
+                        'Taking the type "{0}" as '
+                        'default.'.format(self._fpga_type),
                         msgType='warning')
 
-        # create an instance of the Opal Kelly FrontPanel
+        # Create an instance of the Opal Kelly FrontPanel. The Frontpanel is a
+        # c dll which was wrapped with SWIG for Windows type systems to be
+        # accessed with python 3.4. You have to ensure to use the python 3.4
+        # version to be able to run the Frontpanel wrapper:
+
         self._fpga = ok.FrontPanel()
         # fast counter state
         self.statusvar = -1
@@ -50,10 +102,17 @@ class FastCounterFPGAQO(Base, FastCounterInterface):
                                         # FPGA.
         self._overflown = False         # overflow indicator
         self.count_data = None
+
+        self._internal_clock_hz = 950e6 # that is a fixed number, 950MHz
         # connect to the FPGA module
         self._connect()
 
     def deactivation(self, e):
+        """ Deactivate the FPGA.
+
+        @param object e: Event class object from Fysom. A more detailed
+                         explanation can be found in method activation.
+        """
         self.stop_measure()
         self.statusvar = 0
         del self._fpga
@@ -66,15 +125,18 @@ class FastCounterFPGAQO(Base, FastCounterInterface):
             self.logMsg('No FPGA connected to host PC or FrontPanel.exe is '
                         'running', msgType='error')
             return -1
+
         # open a connection to the FPGA with the specified serial number
         self._fpga.OpenBySerial(self._serial)
-        # upload the fast counter configuration bitfile to the FPGA
+
+        # upload the proper fast counter configuration bitfile to the FPGA
+        bitfile_name = 'fastcounter_' + self._fpga_type + '.bit'
 
         self._fpga.ConfigureFPGA(os.path.join(self.get_main_dir(), 'hardware',
                                               'fpga_fastcounter',
-                                              'fastcounter_top.bit'))
+                                              bitfile_name))
 
-        # check if the upload was successful and the Opal Kelly FrontPanel is
+        # Check if the upload was successful and the Opal Kelly FrontPanel is
         # enabled on the FPGA
         if not self._fpga.IsFrontPanelEnabled():
             self.logMsg('Opal Kelly FrontPanel is not enabled in FPGA',
@@ -82,13 +144,13 @@ class FastCounterFPGAQO(Base, FastCounterInterface):
             self.statusvar = -1
             return -1
         else:
-            # put the fast counter logic and its clock into reset mode
+            # Put the fast counter logic and its clock into reset mode
             self._fpga.SetWireInValue(0x00, 0xC0000000)
             self._fpga.UpdateWireIns()
             self.statusvar = 0
         return 0
 
-    def configure(self, bin_width_s, record_length_s, number_of_gates = 0):
+    def configure(self, bin_width_s, record_length_s, number_of_gates=0):
         """ Configuration of the fast counter.
 
         @param float bin_width_s: Length of a single time bin in the time trace
@@ -98,21 +160,28 @@ class FastCounterFPGAQO(Base, FastCounterInterface):
         @param int number_of_gates: optional, number of gates in the pulse
                                     sequence. Ignore for not gated counter.
         """
+
         # set class variables
-        self._binwidth = int(np.rint(bin_width_s * 1e9 * 950 / 1000))
-        binwidth_s = self._binwidth * 1000 / 950e9
+        self._binwidth = int(np.rint(bin_width_s * self._internal_clock_hz))
+
+        # calculate the actual binwidth depending on the internal clock:
+        binwidth_s = self._binwidth / self._internal_clock_hz
+
         self._gate_length_bins = int(np.rint(record_length_s / bin_width_s))
         gate_length_s = self._gate_length_bins * binwidth_s
+
         self._number_of_gates = number_of_gates
         self._histogram_size = number_of_gates * 8192
+
         # reset overflow indicator
         self._overflown = False
+
         # release the fast counter clock but leave the logic in reset mode
         # set histogram size in the hardware
         self._fpga.SetWireInValue(0x00, 0x40000000 + self._histogram_size)
         self._fpga.UpdateWireIns()
         self.statusvar = 1
-        return (binwidth_s, gate_length_s, number_of_gates)
+        return binwidth_s, gate_length_s, number_of_gates
 
     def start_measure(self):
         """ Start the fast counter. """
@@ -132,18 +201,18 @@ class FastCounterFPGAQO(Base, FastCounterInterface):
     def get_data_trace(self):
         """ Polls the current timetrace data from the fast counter.
 
-        Return value is a numpy array (dtype = int64).
+        @return numpy.array: 2 dimensional array of dtype = int64. This counter
+                             is gated the the return array has the following
+                             shape:
+                                returnarray[gate_index, timebin_index]
+
         The binning, specified by calling configure() in forehand, must be taken
         care of in this hardware class. A possible overflow of the histogram
         bins must be caught here and taken care of.
-        If the counter is NOT GATED it will return a 1D-numpy-array with
-            returnarray[timebin_index]
-        If the counter is GATED it will return a 2D-numpy-array with
-            returnarray[gate_index, timebin_index]
         """
         # initialize the read buffer for the USB transfer.
         # one timebin of the data to read is 32 bit wide and the data is
-        # transfered in bytes.
+        # transferred in bytes.
 
         data_buffer = bytearray(self._histogram_size*4)
         # check if the timetagger had an overflow.
@@ -155,19 +224,24 @@ class FastCounterFPGAQO(Base, FastCounterInterface):
             self._fpga.UpdateWireIns()
             self._fpga.SetWireInValue(0x00, self._histogram_size)
             self._fpga.UpdateWireIns()
+
             # save latest count data into a new class variable to preserve it
             self._old_data = self.count_data.copy()
             self._overflown = True
+
         # trigger the data read in the FPGA
         self._fpga.SetWireInValue(0x00, 0x20000000 + self._histogram_size)
         self._fpga.UpdateWireIns()
         self._fpga.SetWireInValue(0x00, self._histogram_size)
         self._fpga.UpdateWireIns()
+
         # read data from the FPGA
         self._fpga.ReadFromBlockPipeOut(0xA0, 1024, data_buffer)
+
         # encode the bytearray data into 32-bit integers
         buffer_encode = np.array(struct.unpack("<"+"L"*self._histogram_size,
                                  data_buffer))
+
         # bin the data according to the specified bin width
         if self._binwidth != 1:
             buffer_encode = buffer_encode[:(buffer_encode.size //
