09dde361132584358767957c1245f0d0e7945b77
Alexander Stark <alexander.stark@uni-ulm.de>
2016-03-09 15:36:55 +0000

    Fully comment the magnet_logic.py and implement the stop_movement command.

--- logic/magnet_logic.py
+++ logic/magnet_logic.py
@@ -25,12 +25,12 @@ class MagnetLogic(GenericLogic):
     """This is the Interface class to define the controls for the simple
     magnet hardware.
     """
-    _modclass = 'magnetlogic'
+
+    _modclass = 'MagnetLogic'
     _modtype = 'logic'
+
     ## declare connectors
     _in = {'magnetstage': 'MagnetInterface'}
-
-
     _out = {'magnetlogic': 'MagnetLogic'}
 
     def __init__(self, manager, name, config, **kwargs):
@@ -48,32 +48,79 @@ class MagnetLogic(GenericLogic):
                         msgType='status')
 
 
-
-
-
     def activation(self, e):
-        """ Initialisation performed during activation of the module.
+        """ Definition and initialisation of the GUI.
+
+        @param object e: Fysom.event object from Fysom class.
+                         An object created by the state machine module Fysom,
+                         which is connected to a specific event (have a look in
+                         the Base Class). This object contains the passed event,
+                         the state before the event happened and the destination
+                         of the state which should be reached after the event
+                         had happened.
         """
 
         self._magnet_device = self.connector['in']['magnetstage']['object']
 
 
-#        self.testing()
-
     def deactivation(self, e):
+        """ Deactivate the module properly.
+
+        @param object e: Fysom.event object from Fysom class. A more detailed
+                         explanation can be found in the method activation.
+        """
         pass
 
-
     def get_hardware_constraints(self):
+        """ Retrieve the hardware constraints.
+
+        @return dict: dict with constraints for the magnet hardware. The keys
+                      are the labels for the axis and the items are again dicts
+                      which contain all the limiting parameters.
+        """
+
         return self._magnet_device.get_constraints()
 
     def move_rel(self, param_dict):
+        """ Move the specified axis in the param_dict relative with an assigned
+            value.
+
+        @param dict param_dict: dictionary, which passes all the relevant
+                                parameters. E.g., for a movement of an axis
+                                labeled with 'x' by 23 the dict should have the
+                                form:
+                                    param_dict = { 'x' : 23 }
+        """
 
         self._magnet_device.move_rel(param_dict)
 
     def get_pos(self, param_list=None):
+        """ Gets current position of the stage.
+
+        @param list param_list: optional, if a specific position of an axis
+                                is desired, then the labels of the needed
+                                axis should be passed as the param_list.
+                                If nothing is passed, then from each axis the
+                                position is asked.
+
+        @return dict: with keys being the axis labels and item the current
+                      position.
+        """
+
         pos_dict = self._magnet_device.get_pos(param_list)
         return pos_dict
 
     def move_abs(self, param_dict):
+        """ Moves stage to absolute position (absolute movement)
+
+        @param dict param_dict: dictionary, which passes all the relevant
+                                parameters, which should be changed. Usage:
+                                 {'axis_label': <a-value>}.
+                                 'axis_label' must correspond to a label given
+                                 to one of the axis.
+        """
         self._magnet_device.move_abs(param_dict)
+
+    def stop_movement(self):
+        """ Stops movement of the stage. """
+        self._magnet_device.abort()
