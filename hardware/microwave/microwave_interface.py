352ec7c001d216f7dd22a67c6b6188f28f7cddb9
Alexander Stark <alexander.stark@uni-ulm.de>
2016-03-09 16:05:30 +0000

    Give the poor file at least some docstrings and a proper description. Prepare it also to move to the interface folder.

--- hardware/microwave/microwave_interface.py
+++ hardware/microwave/microwave_interface.py
@@ -1,138 +1,170 @@
 # -*- coding: utf-8 -*-
 
+"""
+This file contains the QuDi Interface file for control wavemeter hardware.
+
+QuDi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+QuDi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with QuDi. If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (C) 2015 Kay Jahnke kay.jahnke@alumni.uni-ulm.de
+Copyright (C) 2015 Florian S. Frank florian.frank@uni-ulm.de
+Copyright (C) 2016 Alexander Stark alexander.stark.uni-ulm.de
+"""
+
 from core.util.customexceptions import InterfaceImplementationError
 
-class MWInterface():
-    """This is the Interface class to define the controls for the simple 
+class MicrowaveInterface():
+    """This is the Interface class to define the controls for the simple
     microwave hardware.
     """
-    
+
+    _modclass = 'MicrowaveInterface'
+    _modtype = 'interface'
+
+
     def on(self):
-        """ Switches on any preconfigured microwave output. 
-        
+        """ Switches on any preconfigured microwave output.
+
         @return int: error code (0:OK, -1:error)
-        """ 
-        raise InterfaceImplementationError('MWInterface>on')
+        """
+        raise InterfaceImplementationError('MicrowaveInterface>on')
         return -1
-    
+
     def off(self):
-        """ Switches off any microwave output. 
-        
+        """ Switches off any microwave output.
+
         @return int: error code (0:OK, -1:error)
         """
-        raise InterfaceImplementationError('MWInterface>off')
+        raise InterfaceImplementationError('MicrowaveInterface>off')
         return -1
-        
+
     def get_power(self):
-        """ Gets the microwave output power. 
-        
+        """ Gets the microwave output power.
+
         @return float: the power set at the device
         """
-        raise InterfaceImplementationError('MWInterface>get_power')
+        raise InterfaceImplementationError('MicrowaveInterface>get_power')
         return 0.0
-        
+
     def set_power(self, power=0.):
-        """ Sets the microwave output power. 
-        
-        @param float power: this power is set at the device
-        
+        """ Sets the microwave output power.
+
+        @param float power: the power (in dBm) set for this device
+
         @return int: error code (0:OK, -1:error)
         """
-        raise InterfaceImplementationError('MWInterface>set_power')
+        raise InterfaceImplementationError('MicrowaveInterface>set_power')
         return -1
-        
+
     def get_frequency(self):
-        """ Gets the frequency of the microwave output. 
-    
-        @return float: the power set at the device
+        """ Gets the frequency of the microwave output.
+
+        @return float: frequency (in Hz), which is currently set for this device
         """
-        raise InterfaceImplementationError('MWInterface>get_frequency')
+        raise InterfaceImplementationError('MicrowaveInterface>get_frequency')
         return 0.0
-        
+
     def set_frequency(self, freq=0.):
-        """ Sets the frequency of the microwave output. 
-        
-        @param float power: this power is set at the device
-        
+        """ Sets the frequency of the microwave output.
+
+        @param float freq: the frequency (in Hz) set for this device
+
         @return int: error code (0:OK, -1:error)
         """
-        raise InterfaceImplementationError('MWInterface>set_frequency')
+        raise InterfaceImplementationError('MicrowaveInterface>set_frequency')
         return -1
-        
+
     def set_cw(self, freq=None, power=None, useinterleave=None):
-        """ Sets the microwave output to CW. 
-        
-        @param float power: this power is set at the device
-        
+        """ Sets the MW mode to cw and additionally frequency and power
+
+        @param float freq: frequency to set in Hz
+        @param float power: power to set in dBm
+        @param bool useinterleave: If this mode exists you can choose it.
+
         @return int: error code (0:OK, -1:error)
+
+        Interleave option is used for arbitrary waveform generator devices.
         """
-        raise InterfaceImplementationError('MWInterface>set_cw')
+        raise InterfaceImplementationError('MicrowaveInterface>set_cw')
         return -1
-        
+
     def set_list(self, freq=None, power=None):
-        """ Sets the microwave output to list.
-        
-        @return int: Returns the number of items of the list.
+        """ Sets the MW mode to list mode
+
+        @param list freq: list of frequencies in Hz
+        @param float power: MW power of the frequency list in dBm
+
+        @return int: error code (0:OK, -1:error)
         """
-        raise InterfaceImplementationError('MWInterface>set_list')
+        raise InterfaceImplementationError('MicrowaveInterface>set_list')
         return -1
-        
+
     def reset_listpos(self):
-        """
-        
+        """ Reset of MW List Mode position to start from first given frequency
+
         @return int: error code (0:OK, -1:error)
         """
-        raise InterfaceImplementationError('MWInterface>reset_listpos')
+        raise InterfaceImplementationError('MicrowaveInterface>reset_listpos')
         return -1
-                
+
     def list_on(self):
-        """ Switches on the list mode. 
-        
+        """ Switches on the list mode.
+
         @return int: error code (0:OK, -1:error)
         """
-        raise InterfaceImplementationError('MWInterface>list_on')
+        raise InterfaceImplementationError('MicrowaveInterface>list_on')
         return -1
-        
+
     def set_sweep(self, frequency_start=None, frequency_stop=None, frequency_delta=None):
         """
         """
-        raise InterfaceImplementationError('MWInterface>set_sweep')
+        raise InterfaceImplementationError('MicrowaveInterface>set_sweep')
         return -1
-        
+
     def sweep_pos(self, frequency=None):
         """
         """
-        raise InterfaceImplementationError('MWInterface>sweep_pos')
+        raise InterfaceImplementationError('MicrowaveInterface>sweep_pos')
         return -1
-        
+
     def trigger(self, source,pol):
         """
-        
+
         @return int: error code (0:OK, -1:error)
         """
-        raise InterfaceImplementationError('MWInterface>trigger')
+        raise InterfaceImplementationError('MicrowaveInterface>trigger')
         return -1
-        
+
     def set_modulation(self, flag=None):
         """
         """
-        raise InterfaceImplementationError('MWInterface>set_modulation')
+        raise InterfaceImplementationError('MicrowaveInterface>set_modulation')
         return -1
-        
+
     def output(self):
         """
         """
-        raise InterfaceImplementationError('MWInterface>output')
+        raise InterfaceImplementationError('MicrowaveInterface>output')
         return -1
-        
+
     def am(self, depth=None):
         """
-        
+
         @return float:
         """
-        raise InterfaceImplementationError('MWInterface>am')
+        raise InterfaceImplementationError('MicrowaveInterface>am')
         return -1
-        
-    
-        
-    
+
+
+
+
