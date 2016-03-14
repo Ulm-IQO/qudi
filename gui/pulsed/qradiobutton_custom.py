e3413fc3345e0f796340f15504c902a084dd2fc1
Alexander Stark <alexander.stark@uni-ulm.de>
2016-03-14 22:25:37 +0000

    ### START WITH COMMITING NEW PULSER INTERFACE ### Although the dummy is working now, I cannot guarantee that it works now smoothly with the rest. The other hardware files will be adapted in later commits. === Start with including a customized QRadioButton Widget, which provides read-only display (that is not implemented directly in Qt, so you have to catch all mouse events separately).

---
+++ gui/pulsed/qradiobutton_custom.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+
+"""
+This file contains the a wrapper around QRadioButton, to customize it.
+
+QuDi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+QuDi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with QuDi. If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (C) 2016 Alexander Stark alexander.stark@uni-ulm.de
+"""
+
+from PyQt4 import QtGui
+from PyQt4 import QtCore
+
+
+class CustomQRadioButton(QtGui.QRadioButton):
+    """ Class which customize QRadioButton behaviour.
+
+    The following customization have been applied:
+        - Make the QRadioButton readonly, i.e. catch all the Signals and pass
+          them of not, depending on the state of the Widget.
+    """
+
+    readOnly = QtCore.pyqtProperty(bool, isReadOnly, setReadOnly)
+
+    def __init__(self, *args):
+        # just pass all stuff to superclass, will fail if passing **kwargs
+        super(CustomQRadioButton, self).__init__(*args)
+        self._readOnly = False
+
+    def isReadOnly(self):
+        """ Check the current state of the Radiobox. """
+        return self._readOnly
+
+    def mousePressEvent(self, event):
+        """ Handle what happens on press event of the mouse.
+
+        @param event: QEvent of a Mouse Release action
+        """
+        if self.isReadOnly():
+            event.accept()
+        else:
+            super(CustomQRadioButton, self).mousePressEvent(event)
+
+    # This method is called, if QToolTip is going to be displayed
+    # def mouseMoveEvent( self, event ):
+    #     if ( self.isReadOnly() ):
+    #         event.accept()
+    #     else:
+    #         super(CustomQRadioButton, self).mouseMoveEvent(event)
+
+    def mouseReleaseEvent(self, event):
+        """ Handle what happens on release of the mouse.
+
+        @param event: QEvent of a Mouse Release action
+        """
+        if self.isReadOnly() :
+            event.accept()
+        else:
+            super(CustomQRadioButton, self).mouseReleaseEvent(event)
+
+    # Handle event in which the widget has focus and the spacebar is pressed.
+    def keyPressEvent(self, event):
+        if self.isReadOnly():
+            event.accept()
+        else:
+            super(CustomQRadioButton, self).keyPressEvent(event)
+
+    @QtCore.pyqtSlot(bool)
+    def setReadOnly(self, state):
+        """ Set the Readonly state.
+
+        @param bool state: True or False, for having a readonly QRadioButton.
+        """
+        self._readOnly = state
+
+    
