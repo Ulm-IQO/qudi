e4ea47bef30d047d49f4a5874549b5a024cff149
Jan Binder <jan-git@nlogn.org>
2016-04-15 00:13:26 +0000

    license and copyright information

--- tools/listmods.py
+++ tools/listmods.py
@@ -1,4 +1,23 @@
 # -*- coding: utf-8 -*-
+"""
+List types of QuDi python files and theri object properties
+
+QuDi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+QuDi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with QuDi. If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (C) 2016 Jan M. Binder jan.binder@uni-ulm.de
+"""
+
 import os
 import sys
 import fnmatch
