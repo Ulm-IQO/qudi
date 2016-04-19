0c274256d7cdd6af6e2c6d42173729921fa68a22
Jan Binder <jan-git@nlogn.org>
2016-04-19 13:19:25 +0000

    license and copyright notice

--- hardware/slow_counter_dummy.py
+++ hardware/slow_counter_dummy.py
@@ -1,4 +1,24 @@
 # -*- coding: utf-8 -*-
+"""
+This file contains the QuDi hardware dummy for slow counting devices.
+
+QuDi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+QuDi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with QuDi. If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (C) 2015 Kay D. Jahnke <kay.jahnke@alumni.uni-ulm.de>
+Copyright (C) 2015 Jan Binder <jan.binder@uni-ulm.de>
+Copyright (C) 2015 Alexander Stark alexander.stark@uni-ulm.de
+"""
 
 import numpy as np
 
