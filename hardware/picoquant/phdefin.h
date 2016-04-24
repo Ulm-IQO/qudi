81eaf329e67634714364b2573dd48823df553753
Alexander Stark <alexander.stark@uni-ulm.de>
2016-04-24 22:38:49 +0000

    Add license, author and insert references.

--- hardware/picoquant/phdefin.h
+++ hardware/picoquant/phdefin.h
@@ -1,6 +1,31 @@
+/*
+
+This file contains the header file of the defined constants of the Picoharp300 device.
+
+QuDi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+QuDi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with QuDi. If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (C) 2015 Alexander Stark alexander.stark@uni-ulm.de
+
+*/
+
+
 /* 
-	PHLib programming library for PicoHarp 300
-	PicoQuant GmbH, December 2013
+Taken from:
+http://www.picoquant.com/products/category/tcspc-and-time-tagging-modules/picoharp-300-stand-alone-tcspc-module-with-usb-interface
+
+PHLib programming library for PicoHarp 300
+PicoQuant GmbH, December 2013
 */
 
 
