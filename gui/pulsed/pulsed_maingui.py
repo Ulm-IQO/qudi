Remove in magnet logic the wait method during the absolute move. That has to be implemented properly. Correct in PulsedMeasurementLogic the FT calculation, since it was just not right. Add more comments to that. Remove the deprecated method seq_parameters_changed. Include in the sine fit the display of the current period, with the appropriate error.

gui/pulsed/pulsed_maingui.py
@@ -2715,8 +2715,6 @@ class PulsedMeasurementGui(GUIBase):
 
         # Connect InputWidgets to events
         self._mw.ana_param_num_laser_pulse_SpinBox.editingFinished.connect(self.num_of_lasers_changed)
-        # self._mw.ana_param_x_axis_start_ScienDSpinBox.editingFinished.connect(self.seq_parameters_changed)
-        # self._mw.ana_param_x_axis_inc_ScienDSpinBox.editingFinished.connect(self.seq_parameters_changed)
 
         self._mw.time_param_ana_periode_DoubleSpinBox.editingFinished.connect(self.analysis_timing_changed)
         self.analysis_timing_changed()
@@ -2844,7 +2842,7 @@ class PulsedMeasurementGui(GUIBase):
 
 
     def pull_data_clicked(self):
-        """Pulls and analysis the data when the 'action_pull_data'-button is clicked"""
+        """ Pulls and analysis the data when the 'action_pull_data'-button is clicked. """
         self._pulsed_meas_logic.manually_pull_data()
         return
 
@@ -2871,7 +2869,11 @@ class PulsedMeasurementGui(GUIBase):
         if self._mw.ana_param_errorbars_CheckBox.isChecked():
             beamwidth=self._pulsed_meas_logic.compute_width_of_errorbars()
             # create ErrorBarItem
-            self.signal_image_error_bars.setData(x=self._pulsed_meas_logic.signal_plot_x, y=self._pulsed_meas_logic.signal_plot_y, top=self._pulsed_meas_logic.measuring_error,bottom=self._pulsed_meas_logic.measuring_error,beam=beamwidth)
+            self.signal_image_error_bars.setData(x=self._pulsed_meas_logic.signal_plot_x,
+                                                 y=self._pulsed_meas_logic.signal_plot_y,
+                                                 top=self._pulsed_meas_logic.measuring_error,
+                                                 bottom=self._pulsed_meas_logic.measuring_error,
+                                                 beam=beamwidth)
             if not self.errorbars_present:
                 self._mw.pulse_analysis_PlotWidget.addItem(self.signal_image_error_bars)
                 self.errorbars_present = True
@@ -2883,15 +2885,15 @@ class PulsedMeasurementGui(GUIBase):
 
 
         # dealing with the actual signal
-        self.signal_image.setData(self._pulsed_meas_logic.signal_plot_x, self._pulsed_meas_logic.signal_plot_y)
+        self.signal_image.setData(x=self._pulsed_meas_logic.signal_plot_x,
+                                  y=self._pulsed_meas_logic.signal_plot_y)
         self.change_second_plot()
         return
 
 
-
     def refresh_measuring_error_plot(self):
-        #print(self._pulsed_meas_logic.measuring_error)
-        self.measuring_error_image.setData(self._pulsed_meas_logic.signal_plot_x, self._pulsed_meas_logic.measuring_error*1000)
+        self.measuring_error_image.setData(x=self._pulsed_meas_logic.signal_plot_x,
+                                           y=self._pulsed_meas_logic.measuring_error*1000)
 
     def refresh_elapsed_time(self):
         """ Refreshes the elapsed time and sweeps of the measurement. """
@@ -2908,9 +2910,6 @@ class PulsedMeasurementGui(GUIBase):
             expected_time = self._mw.time_param_expected_dur_DoubleSpinBox.value()
         self._mw.time_param_elapsed_sweep_ScienSpinBox.setValue(self._pulsed_meas_logic.elapsed_time/(expected_time/1e3))
 
-
-
-
     def show_external_mw_source_checked(self):
         """ Shows or hides input widgets which are necessary if an external mw is turned on"""
         if not self._mw.ext_control_use_mw_CheckBox.isChecked():
@@ -2925,7 +2924,6 @@ class PulsedMeasurementGui(GUIBase):
             self._mw.ext_control_mw_power_Label.setVisible(True)
             self._mw.ext_control_mw_power_DoubleSpinBox.setVisible(True)
         return
-
 
     def measurement_ticks_editor(self):
         """ Shows or hides input widgets which are necessary if the x axis id defined or not."""
@@ -2955,12 +2953,13 @@ class PulsedMeasurementGui(GUIBase):
         else:
             self._mw.second_plot_GroupBox.setVisible(True)
 
-            #Here FFT is seperated from the other option. The reason for that is preventing of code doubling
-            if self._mw.second_plot_ComboBox.currentText()=='FFT':
-                fft_x,fft_y=self._pulsed_meas_logic.compute_fft()
+            # Here FFT is seperated from the other option. The reason for that
+            # is preventing of code doubling
+            if self._mw.second_plot_ComboBox.currentText() == 'FFT':
+                fft_x, fft_y = self._pulsed_meas_logic.compute_fft()
                 self.second_plot_image.setData(fft_x, fft_y)
-                self._mw.pulse_analysis_second_PlotWidget.setLogMode(x=False,y=False)
-                if self._as.ana_param_second_plot_x_axis_name_LineEdit.text()=='':
+                self._mw.pulse_analysis_second_PlotWidget.setLogMode(x=False, y=False)
+                if self._as.ana_param_second_plot_x_axis_name_LineEdit.text() == '':
                     self._mw.pulse_analysis_second_PlotWidget.setLabel('left', 'FT-Amplitude')
                     self._mw.pulse_analysis_second_PlotWidget.setLabel('bottom', 'frequency (GHz)')
                 else:
@@ -2972,7 +2971,7 @@ class PulsedMeasurementGui(GUIBase):
                 #FIXME: Is not working when there is a 0 in the values, therefore ignoring the first measurment point
                 self.second_plot_image.setData(self._pulsed_meas_logic.signal_plot_x[1:], self._pulsed_meas_logic.signal_plot_y[1:])
 
-                if self._as.ana_param_second_plot_x_axis_name_LineEdit.text()=='':
+                if self._as.ana_param_second_plot_x_axis_name_LineEdit.text()== '':
                     self._mw.pulse_analysis_second_PlotWidget.setLabel('left', self._as.ana_param_y_axis_name_LineEdit.text())
                     self._mw.pulse_analysis_second_PlotWidget.setLabel('bottom', self._as.ana_param_x_axis_name_LineEdit.text())
 
@@ -2980,38 +2979,17 @@ class PulsedMeasurementGui(GUIBase):
                     self._mw.pulse_analysis_second_PlotWidget.setLabel('bottom', self._as.ana_param_second_plot_x_axis_name_LineEdit.text())
                     self._mw.pulse_analysis_second_PlotWidget.setLabel('left', self._as.ana_param_second_plot_y_axis_name_LineEdit.text())
 
-                if self._mw.second_plot_ComboBox.currentText()=='unchanged data':
-                    self._mw.pulse_analysis_second_PlotWidget.setLogMode(x=False,y=False)
+                if self._mw.second_plot_ComboBox.currentText() == 'unchanged data':
+                    self._mw.pulse_analysis_second_PlotWidget.setLogMode(x=False, y=False)
 
-                elif self._mw.second_plot_ComboBox.currentText()=='Log(x)':
-                    self._mw.pulse_analysis_second_PlotWidget.setLogMode(x=True,y=False)
+                elif self._mw.second_plot_ComboBox.currentText() == 'Log(x)':
+                    self._mw.pulse_analysis_second_PlotWidget.setLogMode(x=True, y=False)
 
-                elif self._mw.second_plot_ComboBox.currentText()=='Log(y)':
+                elif self._mw.second_plot_ComboBox.currentText() == 'Log(y)':
                     self._mw.pulse_analysis_second_PlotWidget.setLogMode(x=False,y=True)
 
-                elif self._mw.second_plot_ComboBox.currentText()=='Log(x)&Log(y)':
-                    self._mw.pulse_analysis_second_PlotWidget.setLogMode(x=True,y=True)
-
-
-
-
-    def seq_parameters_changed(self):
-        """ This method changes the sequence parameters"""
-
-        laser_num = self._mw.ana_param_num_laser_pulse_SpinBox.value()
-        measurement_tick_start = self._mw.ana_param_x_axis_start_ScienDSpinBox.value()
-        measurement_tick_incr = self._mw.ana_param_x_axis_inc_ScienDSpinBox.value()
-        mw_frequency = self._mw.ext_control_mw_freq_DoubleSpinBox.value()
-        mw_power = self._mw.ext_control_mw_power_DoubleSpinBox.value()
-        #self._mw.lasertoshow_spinBox.setRange(0, laser_num)
-
-
-        measurement_tick_vector = np.arange(measurement_tick_start, measurement_tick_start + measurement_tick_incr*laser_num, measurement_tick_incr)
-        # self._pulsed_meas_logic.running_sequence_parameters['tau_vector'] = measurement_tick_vector
-
-        self._pulsed_meas_logic.microwave_freq = mw_frequency
-        self._pulsed_meas_logic.microwave_power = mw_power
-        return
+                elif self._mw.second_plot_ComboBox.currentText() == 'Log(x)&Log(y)':
+                    self._mw.pulse_analysis_second_PlotWidget.setLogMode(x=True, y=True)
 
     def analysis_timing_changed(self):
         """ This method handles the analysis timing"""

logic/pulsed_measurement_logic.py
@@ -677,24 +677,54 @@ class PulsedMeasurementLogic(GenericLogic):
         return
 
     def compute_fft(self):
-        """Computing the fourier transform of the data
+        """ Computing the fourier transform of the data.
 
-        @return fft_x[:middle]: returns the frequencies for the FFT, middle makes sure that only positive values are displayed
-        @return fft_y[:middle]: returns the FFT spectrum, middle makes sure that only positive values are displayed
+        @return tuple (fft_x, fft_y):
+                    fft_x: the frequencies for the FT
+                    fft_y: the FT spectrum
+
+        Pay attention that the return values of the FT have only half of the
+        entries compared to the used signal input.
+
+        In general, a window function should be applied to the time domain data
+        before calculating the FT, to reduce spectral leakage. The Hann window
+        for instance is almost never a bad choice. Use it like:
+            y_ft = np.fft.fft(y_signal * np.hanning(len(y_signal)))
+
+        Keep always in mind the relation for the Fourier transform:
+            T = delta_t * N_samples
+        where delta_t is the distance between the time points and N_samples are
+        the amount of points in the time domain. Consequently the sample rate is
+            f_samplerate = T / N_samples
+
+        Keep in mind that the FT returns value from 0 to f_samplerate, or
+        equivalently -f_samplerate/2 to f_samplerate/2.
+
+
         """
-        # FiXME: This is the first implementation. It is working, but there is probably a lot room for improvement
-        # subtract baseline
-        mean_y=sum(self.signal_plot_y)/len(self.signal_plot_y)
-        corrected_y=self.signal_plot_y-mean_y
+        # Make a baseline correction to avoid a constant offset near zero
+        # frequencies:
+        mean_y = sum(self.signal_plot_y) / len(self.signal_plot_y)
+        corrected_y = self.signal_plot_y - mean_y
 
-        #FIXME: Not sure if real or imaginary part is the better choice here
-        fft_y=np.abs((np.fft.fft(corrected_y)).real)
+        # The absolute values contain the fourier transformed y values
+        fft_y = np.abs(np.fft.fft(corrected_y))
 
-        # Take just the positive values
-        middle = round(corrected_y.shape[-1]/2)
-        fft_x = np.fft.fftfreq(corrected_y.shape[-1])
+        # Due to the sampling theorem you can only identify frequencies at half
+        # of the sample rate, therefore the FT contains an almost symmetric
+        # spectrum (the asymmetry results from aliasing effects). Therefore take
+        # the half of the values for the display.
+        middle = int((len(corrected_y)+1)//2)
 
-        return fft_x[:middle],fft_y[:middle]
+        # sample spacing of x_axis, if x is a time axis than it corresponds to a
+        # timestep:
+        x_spacing = np.round(self.signal_plot_x[-1] - self.signal_plot_x[-2], 12)
+
+        # use the helper function of numpy to calculate the x_values for the
+        # fourier space. That function will handle an occuring devision by 0:
+        fft_x = np.fft.fftfreq(len(corrected_y), d=x_spacing)
+
+        return abs(fft_x[:middle]), fft_y[:middle]
 
     def get_fit_functions(self):
         """Giving the available fit functions
@@ -735,14 +765,17 @@ class PulsedMeasurementLogic(GenericLogic):
             sine, params = self._fit_logic.make_sine_model()
             pulsed_fit_y = sine.eval(x=pulsed_fit_x, params=result.params)
 
-            #pulsed_fit_y = rabi_amp * np.sin(np.multiply(pulsed_fit_x,1/rabi_freq*2*np.pi)+rabi_shift)*np.exp(np.multiply(pulsed_fit_x,-rabi_decay))+rabi_offset
-
             param_dict['Contrast'] = {'value': np.round(np.abs(2*result.params['amplitude'].value*100), 2),
                                       'error': np.round(2 * result.params['amplitude'].stderr*100, 2),
                                       'unit' : '%'}
             param_dict['Frequency'] = {'value': np.round(result.params['frequency'].value/1e6, 3),
                                        'error': np.round(result.params['frequency'].stderr/1e6, 3),
                                        'unit' : 'MHz'}
+            # use proper error propagation formula:
+            error_per = 1/(result.params['frequency'].value/1e9)**2 * result.params['frequency'].stderr/1e9
+            param_dict['Period'] = {'value': np.round(1/(result.params['frequency'].value/1e9), 2),
+                                    'error': np.round(error_per, 2),
+                                    'unit' : 'ns'}
             param_dict['Offset'] = {'value': np.round(result.params['offset'].value, 3),
                                     'error': np.round(result.params['offset'].stderr, 2),
                                     'unit' : 'norm. signal'}
@@ -751,16 +784,6 @@ class PulsedMeasurementLogic(GenericLogic):
                                    'unit' : '°'}
 
             fit_result = self._create_formatted_output(param_dict)
-
-            # fit_result = str('Contrast: ' + str(np.round(np.abs(2*result.params['amplitude'].value), 3)) + u" \u00B1 " +
-            #                                 str(np.round(2 * result.params['amplitude'].stderr, 2)) + "\n" +
-            #                  'Frequency [MHz]: ' + str(np.round(1000*result.params['frequency'].value, 3)) + u" \u00B1 " +
-            #                                        str(np.round(1000*result.params['frequency'].stderr, 2)) + "\n" +
-            #                  'Offset: ' + str(np.round(result.params['offset'].value,3)) + u" \u00B1 " +
-            #                               str(np.round(result.params['offset'].stderr,2)) + "\n" +
-            #                  #'Decay [ns]: ' + str(rabi_decay) + " + " + str(rabi_decay_error) + "\n" +
-            #                  'Phase [rad]: ' + str(np.round(result.params['phase'].value, 3)) + u" \u00B1 " +
-            #                                    str(np.round(result.params['frequency'].stderr, 2)) + "\n")
 
             return pulsed_fit_x, pulsed_fit_y, fit_result
 
@@ -786,14 +809,6 @@ class PulsedMeasurementLogic(GenericLogic):
 
             fit_result = self._create_formatted_output(param_dict)
 
-            # fit_result = (   'Minimum : ' + str(np.round(result.params['center'].value,3)) + u" \u00B1 "
-            #                     + str(np.round(result.params['center'].stderr,2)) + ' [ns]' + '\n'
-            #                     + 'linewidth : ' + str(np.round(result.params['fwhm'].value,3)) + u" \u00B1 "
-            #                     + str(np.round(result.params['fwhm'].stderr,2)) + ' [ns]' + '\n'
-            #                     + 'contrast : ' + str(np.round((result.params['amplitude'].value/(-1*np.pi*result.params['sigma'].value*result.params['c'].value)),3)*100) + '[%]'
-            #                     )
-
-
             return pulsed_fit_x, pulsed_fit_y, fit_result
 
 
@@ -802,7 +817,7 @@ class PulsedMeasurementLogic(GenericLogic):
             result = self._fit_logic.make_lorentzianpeak_fit(axis=self.signal_plot_x,
                                                              data=self.signal_plot_y,
                                                              add_parameters=None)
-            lorentzian,params=self._fit_logic.make_lorentzian_model()
+            lorentzian, params=self._fit_logic.make_lorentzian_model()
             pulsed_fit_y = lorentzian.eval(x=pulsed_fit_x, params=result.params)
 
             param_dict['Maximum'] = {'value': np.round(result.params['center'].value, 3),