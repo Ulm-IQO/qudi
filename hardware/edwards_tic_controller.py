399b723776fbb7cda347f766e5a09161fb6bca78
Jan M. Binder <jan-git@nlogn.org>
2016-06-06 15:25:37 +0200

    add description of module

--- hardware/edwards_tic_controller.py
+++ hardware/edwards_tic_controller.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 """
+This module reads operational parameters from Edwards Vacuum
+TIC series controllers for Edwards Vacuum turbomolecular pumps
+and backing pumps.
 
 QuDi is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
