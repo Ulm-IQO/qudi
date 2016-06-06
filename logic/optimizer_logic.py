30655763ea24928678a4a2388cb4bdfd4ee1a946
Yawghmoth <florian.frank@uni-ulm.de>
2016-06-06 15:33:35 +0200

    solved ticket #39
    ROI now equals the optimizer region

--- logic/optimizer_logic.py
+++ logic/optimizer_logic.py
@@ -52,6 +52,7 @@ class OptimizerLogic(GenericLogic):
     # public signals
     signal_image_updated = QtCore.Signal()
     signal_refocus_started = QtCore.Signal()
+    signal_refocus_XY_size_changed = QtCore.Signal()
     signal_refocus_finished = QtCore.Signal(str, list)
 
     def __init__(self, manager, name, config, **kwargs):
@@ -72,7 +73,6 @@ class OptimizerLogic(GenericLogic):
         self.optimizer_XY_res = 10
         self.refocus_Z_size = 2
         self.optimizer_Z_res = 30
-
         self.hw_settle_time = 0.1  # let scanner reach start of xy and z scans
 
         # Initialization of settings option for optimization sequence
@@ -190,6 +190,11 @@ class OptimizerLogic(GenericLogic):
             return -1
         else:
             return 0
+
+    def set_refocus_XY_size(self,size):
+        self.refocus_XY_size = size
+        self.signal_refocus_XY_size_changed.emit()
+
 
     def start_refocus(self, initial_pos=None, caller_tag='unknown'):
         """Starts the optimization scan around initial_pos
