42a0773cdca27e47eb769ef657adb0ae4818ffa4
Jan M. Binder <jan-git@nlogn.org>
2016-06-12 00:46:41 +0200

    simple laser interface and dummy

--- interface/simple_laser_interface.py
+++ interface/simple_laser_interface.py
@@ -17,13 +17,24 @@ along with QuDi. If not, see <http://www.gnu.org/licenses/>.
 
 Copyright (C) 2016 Jan M. Binder jan.binder@uni-ulm.de
 """
-from core.util.customexceptions import InterfaceImplementationError
+from core.util.customexceptions import *
 from enum import Enum
 
-class ControlModes(Enum):
+class ControlMode(Enum):
     MIXED = 0
     POWER = 1
     CURRENT = 2
+
+class ShutterState(Enum):
+    CLOSED = 0
+    OPEN = 1
+    UNKNOWN = 2
+    NOSHUTTER = 4
+
+class LaserState(Enum):
+    OFF = 0
+    ON = 1
+    UNKNOWN = 2
 
 class SimpleLaserInterface:
     _modtype = 'SimpleLaserInterface'
@@ -33,15 +44,55 @@ class SimpleLaserInterface:
         """ Return laser power
         @return float: Laser power in watts
         """
-        raise InterfaceImplementationError('SimpleLaserInterface->getData')
-        return -1
+        raise InterfaceImplementationError('{}->{}'.format(type(self).__name__, function_signature()))
+        
+    def set_power(self, power):
+        raise InterfaceImplementationError('{}->{}'.format(type(self).__name__, function_signature()))
 
+    def get_power_setpoint(self):
+        raise InterfaceImplementationError('{}->{}'.format(type(self).__name__, function_signature()))
+    
     def get_current(self):
         """ Return laser current
         @return float: laser current in amperes
         """
-        raise InterfaceImplementationError('SimpleLaserInterface->getChannels')
-        return -1
+        raise InterfaceImplementationError('{}->{}'.format(type(self).__name__, function_signature()))
+
+    def get_current_setpoint(self):
+        raise InterfaceImplementationError('{}->{}'.format(type(self).__name__, function_signature()))
+
+    def set_current(self, current):
+        raise InterfaceImplementationError('{}->{}'.format(type(self).__name__, function_signature()))
 
     def get_control_mode(self):
-        raise InterfaceImplementationError('SimpleLaserInterface->getChannels')
+        raise InterfaceImplementationError('{}->{}'.format(type(self).__name__, function_signature()))
+
+    def set_control_mode(self, control_mode):
+        raise InterfaceImplementationError('{}->{}'.format(type(self).__name__, function_signature()))
+        
+    def on(self):
+        raise InterfaceImplementationError('{}->{}'.format(type(self).__name__, function_signature()))
+
+    def off(self):
+        raise InterfaceImplementationError('{}->{}'.format(type(self).__name__, function_signature()))
+
+    def get_laser_state(self):
+        raise InterfaceImplementationError('{}->{}'.format(type(self).__name__, function_signature()))
+    
+    def set_laser_state(self, state):
+        raise InterfaceImplementationError('{}->{}'.format(type(self).__name__, function_signature()))
+
+    def get_shutter_state(self):
+        raise InterfaceImplementationError('{}->{}'.format(type(self).__name__, function_signature()))
+
+    def set_shutter_state(self, state):
+        raise InterfaceImplementationError('{}->{}'.format(type(self).__name__, function_signature()))
+
+    def get_temperatures(self):
+        raise InterfaceImplementationError('{}->{}'.format(type(self).__name__, function_signature()))
+
+    def set_temperatures(self):
+        raise InterfaceImplementationError('{}->{}'.format(type(self).__name__, function_signature()))
+
+    def get_extra_info(self):
+        raise InterfaceImplementationError('{}->{}'.format(type(self).__name__, function_signature()))
