8d1e62306b257f03f12957f71c8daf851521941c
Jan M. Binder <jan-git@nlogn.org>
2016-06-13 14:56:18 +0200

    interface documentation

--- interface/simple_laser_interface.py
+++ interface/simple_laser_interface.py
@@ -29,12 +29,13 @@ class ShutterState(Enum):
     CLOSED = 0
     OPEN = 1
     UNKNOWN = 2
-    NOSHUTTER = 4
+    NOSHUTTER = 3
 
 class LaserState(Enum):
     OFF = 0
     ON = 1
-    UNKNOWN = 2
+    BLOCKED = 2
+    UNKNOWN = 3
 
 class SimpleLaserInterface:
     _modtype = 'SimpleLaserInterface'
@@ -42,57 +43,115 @@ class SimpleLaserInterface:
 
     def get_power(self):
         """ Return laser power
-        @return float: Laser power in watts
+        @return float: Actual laser power in watts
         """
         raise InterfaceImplementationError('{}->{}'.format(type(self).__name__, function_signature()))
         
     def set_power(self, power):
+        """ Set laer power ins watts
+          @param float power: laser power setpoint in watts
+
+          @return float: laser power setpoint in watts
+        """
         raise InterfaceImplementationError('{}->{}'.format(type(self).__name__, function_signature()))
 
     def get_power_setpoint(self):
+        """ Return laser power setpoint
+        @return float: Laser power setpoint in watts
+        """
         raise InterfaceImplementationError('{}->{}'.format(type(self).__name__, function_signature()))
     
     def get_current(self):
         """ Return laser current
-        @return float: laser current in amperes
+        @return float: Actual laser current in amperes
         """
         raise InterfaceImplementationError('{}->{}'.format(type(self).__name__, function_signature()))
 
     def get_current_setpoint(self):
+        """ Return laser current
+        @return float: Laser current setpoint in amperes
+        """
         raise InterfaceImplementationError('{}->{}'.format(type(self).__name__, function_signature()))
 
     def set_current(self, current):
+        """ Set laser current
+        @param float current: Laser current setpoint in amperes
+        @return float: Laser current setpoint in amperes
+        """
         raise InterfaceImplementationError('{}->{}'.format(type(self).__name__, function_signature()))
 
     def get_control_mode(self):
+        """ Get control mode of laser
+          @return enum ControlMode: control mode
+        """
         raise InterfaceImplementationError('{}->{}'.format(type(self).__name__, function_signature()))
 
     def set_control_mode(self, control_mode):
+        """ Set laser control mode.
+          @param enum control_mode: desired control mode
+          @return enum ControlMode: actual control mode
+        """
         raise InterfaceImplementationError('{}->{}'.format(type(self).__name__, function_signature()))
         
     def on(self):
+        """ Turn on laser. Does not open shutter if one is present.
+          @return enum LaserState: actual laser state
+        """
         raise InterfaceImplementationError('{}->{}'.format(type(self).__name__, function_signature()))
 
     def off(self):
+        """ Turn ooff laser. Does not close shutter if one is present.
+          @return enum LaserState: actual laser state
+        """
         raise InterfaceImplementationError('{}->{}'.format(type(self).__name__, function_signature()))
 
     def get_laser_state(self):
+        """ Get laser state.
+          @return enum LaserState: laser state
+        """
         raise InterfaceImplementationError('{}->{}'.format(type(self).__name__, function_signature()))
     
     def set_laser_state(self, state):
+        """ Set laser state.
+          @param enum state: desired laser state
+          @return enum LaserState: actual laser state
+        """
         raise InterfaceImplementationError('{}->{}'.format(type(self).__name__, function_signature()))
 
     def get_shutter_state(self):
+        """ Get shutter state. Has a state for no shutter present.
+          @return enum ShutterState: actual shutter state
+        """
         raise InterfaceImplementationError('{}->{}'.format(type(self).__name__, function_signature()))
 
     def set_shutter_state(self, state):
+        """ Set shutter state.
+          @param enum state: desired shutter state
+          @return enum ShutterState: actual shutter state
+        """
         raise InterfaceImplementationError('{}->{}'.format(type(self).__name__, function_signature()))
 
     def get_temperatures(self):
+        """ Get all available temperatures from laser.
+          @return dict: dict of name, value for temperatures
+        """
         raise InterfaceImplementationError('{}->{}'.format(type(self).__name__, function_signature()))
 
-    def set_temperatures(self):
+    def get_temperature_setpoints(self):
+        """ Get all available temperature setpoints from laser.
+          @return dict: dict of name, value for temperature setpoints
+        """
+        raise InterfaceImplementationError('{}->{}'.format(type(self).__name__, function_signature()))
+
+    def set_temperatures(self, temps):
+        """ Set laser temperatures.
+          @param temps: dict of name, value to be set
+          @return dict: dict of name, value of temperatures that were set
+        """
         raise InterfaceImplementationError('{}->{}'.format(type(self).__name__, function_signature()))
 
     def get_extra_info(self):
+        """ Show dianostic information about lasers.
+          @return str: diagnostic info as a string
+        """
         raise InterfaceImplementationError('{}->{}'.format(type(self).__name__, function_signature()))
