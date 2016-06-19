f61842a47d9c56b144adfe41a073501bd9acdbfc
Jan M. Binder <jan-git@nlogn.org>
2016-06-20 01:26:41 +0200

    add a module to get the latest value from an influxdb time series database

---
+++ hardware/influx_data_client.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+A module to control the QO Raspberry Pi based H-Bridge hardware.
+
+QuDi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+QuDi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with QuDi. If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (C) 2016 Jan M. Binder <jan.binder@uni-ulm.de>
+"""
+
+from core.base import Base
+from interface.process_interface import ProcessInterface
+
+from pyqtgraph.Qt import QtCore
+from influxdb import InfluxDBClient
+
+class InfluxDataClient(Base, ProcessInterface):
+    _modclass = 'InfluxDataClient'
+    _modtype = 'hardware'
+
+    ## declare connectors
+    _out = {'data': 'ProcessInterface'}
+    
+    def __init__(self, manager, name, config = {}, **kwargs):
+        c_dict = {'onactivate': self.activation, 'ondeactivate': self.deactivation}
+        Base.__init__(self, manager, name, configuration=config, callbacks = c_dict, **kwargs)
+        
+    def activation(self, e):
+        config = self.getConfiguration()
+
+        if 'user' in config:
+            self.user = config['user']
+
+        if 'password' in config:
+            self.pw = config['password']
+
+        if 'dbname' in config:
+            self.dbname = config['dbname']
+
+        if 'host' in config:
+            self.host = config['host']
+           
+        if 'port' in config:
+            self.port = config['port']
+        else:
+            self.port = 8086
+
+        if 'dataseries' in config:
+            self.series = config['dataseries']
+
+        if 'field' in config:
+            self.field = config['field']
+
+        if 'criterion' in config:
+            self.cr = config['criterion']
+
+        self.connect_db()
+
+    def deactivation(self, e):
+        del self.conn
+
+    def connect_db(self):
+        self.conn = InfluxDBClient(self.host, self.port, self.user, self.pw, self.dbname)
+
+    def getProcessValue(self):
+        """ Return a measured value """
+        q = 'SELECT last({}) FROM {} WHERE (time > now() - 10m AND {})'.format(self.field, self.series, self.cr)
+        #print(q)
+        res = self.conn.query(q)
+        return list(res[('{}'.format(self.series), None)])[0]['last']
+
+    def getProcessUnit(self):
+        """ Return the unit that hte value is measured in as a tuple of ('abreviation', 'full unit name') """
+        return '°C', ' degrees Celsius'
+
