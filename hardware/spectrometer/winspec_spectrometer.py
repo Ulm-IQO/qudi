d696e9e0e032f470a4946b13418d4d36ea0fe1ac
lachlan <lachlan.j.rogers@quantum.diamonds>
2016-06-28 00:21:44 +0200

    Replaced all copyright lines in hardware files with a standard pointer to the top-level directory COPYRIGHT.txt file.  Proper author attribution is handled by the public git repository

--- hardware/spectrometer/winspec_spectrometer.py
+++ hardware/spectrometer/winspec_spectrometer.py
@@ -18,9 +18,9 @@ You should have received a copy of the GNU General Public License
 along with QuDi. If not, see <http://www.gnu.org/licenses/>.
 
 Derived from the pyPL project (https://github.com/kaseyrussell/pyPL)
-Copyright 2010 Kasey Russell ( email: krussell _at_ post.harvard.edu )
+Copyright (c) the Qudi Developers. See the COPYRIGHT.txt file at the
+top-level directory of this distribution and at <https://github.com/Ulm-IQO/qudi/>
 
-Copyright (C) 2015 Jan M. Binder jan.binder@uni-ulm.de
 """
 
 from core.base import Base
