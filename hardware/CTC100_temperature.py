5b2d8220b0d2ad30d64f1bd85740fd30db85ea46
lachlan <lachlan.j.rogers@quantum.diamonds>
2016-06-28 00:26:34 +0200

    Small fixes in copyright notice changeover

--- hardware/CTC100_temperature.py
+++ hardware/CTC100_temperature.py
@@ -18,7 +18,6 @@ along with QuDi. If not, see <http://www.gnu.org/licenses/>.
 
 Copyright (c) the Qudi Developers. See the COPYRIGHT.txt file at the
 top-level directory of this distribution and at <https://github.com/Ulm-IQO/qudi/>
-top-level directory of this distribution and at <https://github.com/Ulm-IQO/qudi/>
 """
 
 from core.base import Base
