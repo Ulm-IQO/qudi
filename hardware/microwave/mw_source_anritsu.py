19cb851a2420510e18d61b3522be7115c633ec9c
lachlan <lachlan.j.rogers@quantum.diamonds>
2016-06-28 13:46:47 +0200

    Replacing some 3rd party copyright lines which were removed by the brutal global search-and-replace

--- hardware/microwave/mw_source_anritsu.py
+++ hardware/microwave/mw_source_anritsu.py
@@ -16,6 +16,9 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with QuDi. If not, see <http://www.gnu.org/licenses/>.
 
+Parts of this file were developed from a PI3diamond module which is
+Copyright (C) 2009 Helmut Rathgen <helmut.rathgen@gmail.com>
+
 Copyright (c) the Qudi Developers. See the COPYRIGHT.txt file at the
 top-level directory of this distribution and at <https://github.com/Ulm-IQO/qudi/>
 """
--- hardware/spectrometer/winspec_spectrometer.py
+++ hardware/spectrometer/winspec_spectrometer.py
@@ -18,6 +18,8 @@ You should have received a copy of the GNU General Public License
 along with QuDi. If not, see <http://www.gnu.org/licenses/>.
 
 Derived from the pyPL project (https://github.com/kaseyrussell/pyPL)
+Copyright 2010 Kasey Russell ( email: krussell _at_ post.harvard.edu )
+
 Copyright (c) the Qudi Developers. See the COPYRIGHT.txt file at the
 top-level directory of this distribution and at <https://github.com/Ulm-IQO/qudi/>
 
