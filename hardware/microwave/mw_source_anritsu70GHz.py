fcb15f6675c9e224a9b13aee991d9258336974a8
Jan M. Binder <jan-git@nlogn.org>
2016-07-18 20:52:40 +0200

    make anritsu mg3696b list mode work and add limits

--- hardware/microwave/mw_source_anritsu70GHz.py
+++ hardware/microwave/mw_source_anritsu70GHz.py
@@ -37,7 +37,7 @@ class MicrowaveAnritsu70GHz(Base, MicrowaveInterface):
     _modtype = 'hardware'
 
     ## declare connectors
-    _out = {'mwsourceanritsu70GHz': 'MicrowaveInterface'}
+    _out = {'mwsourceanritsu': 'MicrowaveInterface'}
 
     def __init__(self, manager, name, config = {}, **kwargs):
         c_dict = {'onactivate': self.activation,
@@ -64,27 +64,54 @@ class MicrowaveAnritsu70GHz(Base, MicrowaveInterface):
         # trying to load the visa connection to the module
         self.rm = visa.ResourceManager()
         try:
-            self._gpib_connection = self.rm.open_resource(self._gpib_address,
-                                                          timeout=self._gpib_timeout)
+            self._gpib_connection = self.rm.open_resource(
+                self._gpib_address,
+                timeout=self._gpib_timeout*1000)
         except:
             self.logMsg('This is MWanritsu70GHz: could not connect to the GPIB '
                         'address >>{}<<.'.format(self._gpib_address),
                         msgType='error')
             raise
-
-        self.logMsg('MWanritsu70GHz initialised and connected to hardware.',
+        # native command mode, some things are missing in SCPI mode
+        self._gpib_connection.write('SYST:LANG \"NATIVE\"')
+        self.model = self._gpib_connection.query('*IDN?').split(',')[1]
+        self.logMsg('Anritsu {} initialised and connected to hardware.'.format(self.model),
                     msgType='status')
 
     def deactivation(self,e=None):
         self._gpib_connection.close()
         self.rm.close()
 
+    def get_limits(self):
+        """ Right now, this is for Anritsu MG3696B only."""
+        limits = {
+            'frequency': {
+                'min': 10*10e6,
+                'max': 70*10e9
+                },
+            'power': {
+                'min': -20,
+                'max': 10
+                },
+            'list': {
+                'minstep': 0.001,
+                'maxstep': 70*10e9,
+                'maxentries': 2000
+                },
+            'sweep': {
+                'minstep': 0.001,
+                'maxstep': 70*10e9,
+                'maxentries': 10000
+                }
+            }
+        return limits
+
     def on(self):
         """ Switches on any preconfigured microwave output.
 
         @return int: error code (0:OK, -1:error)
         """
-
+        print("on")
         self._gpib_connection.write('RF1')
 
         return 0
@@ -94,6 +121,7 @@ class MicrowaveAnritsu70GHz(Base, MicrowaveInterface):
 
         @return int: error code (0:OK, -1:error)
         """
+        print("off")
         self._gpib_connection.write('RF0')
 
         return 0
@@ -113,7 +141,7 @@ class MicrowaveAnritsu70GHz(Base, MicrowaveInterface):
         @return int: error code (0:OK, -1:error)
         """
         if power is not None:
-            self._gpib_connection.write('RF0 L0 {:f} DM RF1'.format(power))
+            self._gpib_connection.write('L0 {:f} DM'.format(power))
             return 0
         else:
             return -1
@@ -133,9 +161,10 @@ class MicrowaveAnritsu70GHz(Base, MicrowaveInterface):
         @return int: error code (0:OK, -1:error)
         """
         if freq is not None:
-            self._gpib_connection.write('RF0 F0 {:f} HZ RF1'.format(freq))
+            self._gpib_connection.write('F0 {:f} HZ'.format(freq))
             return 0
-        else: return -1
+        else:
+            return -1
 
     def set_cw(self, freq=None, power=None, useinterleave=None):
         """ Sets the MW mode to cw and additionally frequency and power
@@ -149,14 +178,17 @@ class MicrowaveAnritsu70GHz(Base, MicrowaveInterface):
         Interleave option is used for arbitrary waveform generator devices.
         """
         error = 0
+        print("set cw")
         if freq != None:
             error = self.set_frequency(freq)
         else:
             return -1
+
         if power != None:
             error = self.set_power(power)
         else:
             return -1
+        self._gpib_connection.write('ACW')
         return error
 
     def set_list(self, freq=None, power=None):
@@ -169,46 +201,42 @@ class MicrowaveAnritsu70GHz(Base, MicrowaveInterface):
         """
         error = 0
 
-        if self.set_cw(freq[0],power) != 0:
+        if self.set_cw(freq[0], power) != 0:
             error = -1
 
-        f = ''
-        p = ''
+        flist = '{:f} HZ, '.format(freq[0])
+        plist = '{:f} DM, '.format(power)
 
-        for s in freq[:-1]:
-            f += '{:i} HZ, '.format(s)
-            p += '{:f} DM, '.format(power)
+        for f in freq[:-1]:
+            flist += '{:f} HZ, '.format(f)
+            plist += '{:f} DM, '.format(power)
 
-        f += '{:i} HZ'.format(freq[-1])
-        p += '{:f} DM'.format(power)
-        stop = '{:i}'.format(len(freq)-1)
+        flist += '{:f} HZ'.format(freq[-1])
+        plist += '{:f} DM'.format(power)
+        stop = len(freq)
 
-        while len(stop) < 4:
-            stop = '0' + stop
-
-        self._gpib_connection.write('RF0 LST ELN0 ELI0000 LF ' + f + ' LP ' + p + 'LIB0000 LIE{:s}'.format(stop) + 'RF1')
-
+        self._gpib_connection.write(
+            'ELN0 ELI0000 '
+            'LF ' + flist
+            + ' LP ' + plist
+            + 'LIB0000 LIE{:04d}'.format(stop))
         return error
 
-
-    def reset_listpos(self):#
+    def reset_listpos(self):
         """ Reset of MW List Mode position to start from first given frequency
 
         @return int: error code (0:OK, -1:error)
         """
-        self._gpib_connection.write('ELI0')
-        self._gpib_connection.write('*WAI')
-
+        self._gpib_connection.write('ELI0000')
         return 0
-
 
     def list_on(self):
         """ Switches on the list mode.
 
         @return int: error code (0:OK, -1:error)
         """
-        pass
-
+        self._gpib_connection.write('LST LEA RF1')
+        return 0
 
     def set_ex_trigger(self, source, pol):
         """ Set the external trigger for this device with proper polarization.
@@ -219,7 +247,40 @@ class MicrowaveAnritsu70GHz(Base, MicrowaveInterface):
 
         @return int: error code (0:OK, -1:error)
         """
+        print("trigger")
+        self._gpib_connection.write('MNT')
+        return 0
 
-        self._gpib_connection.write(':TRIG:SOUR '+source)
-        self._gpib_connection.write(':TRIG:SLOP '+pol)
-        self._gpib_connection.write('*WAI')
+    def set_sweep(self, start, stop, step, power):
+        """
+
+        @param start:
+        @param stop:
+        @param step:
+        @param power:
+        @return:
+        """
+        print("sweep on")
+        self.set_power(power)
+        self._gpib_connection.write('F1 {} Hz, SYZ {} Hz, F2 {} Hz, SF1'.format(start - step, step, stop))
+        nrsteps = int(self._gpib_connection.query('OSS'))
+        print('steps', nrsteps)
+        return nrsteps - 1
+
+    def reset_sweep(self):
+        """ Reset of MW List Mode position to start from first given frequency
+
+        @return int: error code (0:OK, -1:error)
+        """
+        print("reset sweep")
+        self._gpib_connection.write('RSS')
+        return 0
+
+    def sweep_on(self):
+        """ Switches on the list mode.
+
+        @return int: error code (1: ready, 0:not ready, -1:error)
+        """
+        print("sweep on")
+        self._gpib_connection.write('SSP RF1')
+        return 0
