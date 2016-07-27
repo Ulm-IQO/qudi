f646daa00556c25b7ae83d14a73a90e37e6d72e2
Tobias Gehring <tobias.gehring@fysik.dtu.dk>
2016-07-27 13:03:38 +0200

    added docstrings to FysomAdapter

--- core/FysomAdapter.py
+++ core/FysomAdapter.py
@@ -1,12 +1,32 @@
+"""
+This module provides an adapter around Fysom which can be used as a drop in
+replacement but supports cooperative multiple inheritance.
+"""
+
 import fysom
 
+
 class Fysom():
+    """
+    This provides an adapter class for fysom which makes it compatible with
+    cooperative multiple inheritance.
+    """
     def __init__(self, cfg={}, initial=None, events=None, callbacks=None,
             final=None, **kwargs):
+        """
+        Constructor. It creates the Fysom object.
+        """
         self._fysom = fysom.Fysom(cfg, initial, events, callbacks, final)
         super().__init__(**kwargs)
 
     def __getattr__(self, name):
+        """
+        This basically copies the attributes of Fysom. Note that Fysom
+        makes use of dynamical attributes heavily. If the attribute is not
+        found in the fysom object, AttributeError raised.
+
+        @param name str: name of attribute
+        """
         if hasattr(self._fysom, name):
             return getattr(self._fysom, name)
         else:
