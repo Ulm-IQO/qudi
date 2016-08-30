78f9bc53979f343f925d7765df315a2fc0551d50
Jan M. Binder <jan-git@nlogn.org>
2016-08-30 11:25:40 +0200

    travis: debug qt api being chosen wrongly

--- core/__init__.py
+++ core/__init__.py
@@ -23,16 +23,19 @@ __version__ = '0.1'
 
 # import Qt
 import os
+
 if not 'QT_API' in os.environ:
     # use PyQt4 as default
     os.environ['QT_API'] = 'pyqt'
 else:
+    print('Specified Qt API:', os.environ['QT_API'])
     # if pyqt4 check environment variable is 'pyqt' and not 'pyqt4' (ipython,
     # matplotlib, etc)
     if os.environ['QT_API'].lower() == 'pyqt4':
         os.environ['QT_API'] = 'pyqt'
 
 import qtpy
+print('Used Qt API:', qtpy.API_NAME)
 
 import sys
 # Make icons work on non-X11 platforms, import a custom theme
