d01b73224b429ae0eab480efb3ea7b7fe1fc3e20
Jan M. Binder <jan-git@nlogn.org>
2016-09-03 20:55:09 +0200

    add bash script that runs the tests and kills qudi afterwards

---
+++ tools/test.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+function print_log {
+echo "======== QuDi Logfile ========"
+cat qudi.log
+if [ -e crash.log ]; then
+  echo "======== QuDi Crashfile ========";
+  cat crash.log;
+fi
+}
+
+python start.py &
+QUDIPID=$!
+
+sleep 10
+
+jupyter-nbconvert --execute notebooks/debug.ipynb
+jupyter-nbconvert --execute notebooks/matplotlib.ipynb
+
+jupyter-nbconvert --execute notebooks/shutdown.ipynb
+
+sleep 20
+
+if kill $PID; then
+    echo "Shutdown has failed: $PID was killed" >&2
+    print_log
+    exit 1
+fi
+
+print_log
