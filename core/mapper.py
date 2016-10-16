Ulm-IQO/qudi

commit fdfc60784c9fac8bfc7c0e6adde8fe32aa671225

isinstance() supports comparison with multiple classes

core/mapper.py
@@ -128,9 +128,7 @@ class Mapper():
             return 'currentIndex'
         elif isinstance(widget, QLineEdit):
             return 'text'
-        elif (isinstance(widget, QSpinBox)
-              or isinstance(widget, QDoubleSpinBox)
-              or isinstance(widget, QAbstractSlider)):
+        elif (isinstance(widget, (QSpinBox, QDoubleSpinBox, QAbstractSlider)):
             return 'value'
         elif isinstance(widget, QPlainTextEdit):
             return 'plainText'

commit 37dabed58dd24f261835fc283b2cb23f5cf060a6

fixed syntax error introduced in commit fdfc60784c9fac8bfc7c0e6adde8fe32aa671225

core/mapper.py
@@ -128,7 +128,9 @@ class Mapper():
             return 'currentIndex'
         elif isinstance(widget, QLineEdit):
             return 'text'
-        elif (isinstance(widget, (QSpinBox, QDoubleSpinBox, QAbstractSlider)):
+        elif (isinstance(widget, (QSpinBox,
+                                  QDoubleSpinBox,
+                                  QAbstractSlider))):
             return 'value'
         elif isinstance(widget, QPlainTextEdit):
             return 'plainText'