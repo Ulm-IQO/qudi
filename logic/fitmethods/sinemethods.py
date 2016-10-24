51f3cbc7b41b629ef9e6111cd49852c89ba4c2e6
Alexander Stark <alexander.stark@uni-ulm.de>
2016-10-24 16:34:58 +0200

    Reformulate the sine fitting by starting from a bare sine model. That serves as an atomic unit. Build on that sine model every other model. Split therefore the estimator according to the different sine functions. Improve the phase estimation of the sinus fit.

--- logic/fitmethods/sinemethods.py
+++ logic/fitmethods/sinemethods.py
@@ -26,9 +26,7 @@ logger = logging.getLogger(__name__)
 import numpy as np
 from lmfit.models import Model
 from lmfit import Parameters
-from scipy.interpolate import splrep, sproot, splev
-from scipy.signal import wiener, filtfilt, butter, gaussian, freqz
-from scipy.ndimage import filters
+
 
 ############################################################################
 #                                                                          #
@@ -36,7 +34,49 @@ from scipy.ndimage import filters
 #                                                                          #
 ############################################################################
 
-def make_sine_model(self):
+
+
+def make_baresine_model(self, prefix=None):
+    """ Create a bare sine model without amplitude and offset.
+
+    @param str prefix: optional, if multiple models should be used and the
+                       parameters of each model should be distinguished from
+                       each other.
+
+    @return tuple: (object model, object params)
+
+    Explanation of the objects:
+        object lmfit.model.CompositeModel model:
+            A model the lmfit module will use for that fit. Here a
+            gaussian model. Returns an object of the class
+            lmfit.model.CompositeModel.
+
+        object lmfit.parameter.Parameters params:
+            It is basically an OrderedDict, so a dictionary, with keys
+            denoting the parameters as string names and values which are
+            lmfit.parameter.Parameter (without s) objects, keeping the
+            information about the current value.
+    """
+
+    def bare_sine_function(x, frequency, phase):
+        """ Function of a sine.
+
+        @param x: independant variable - e.g. time
+        @param frequency: frequency
+        @param phase: phase
+
+        @return: reference to method of a sine function in order to use it as a
+                 model
+        """
+
+        return np.sin(2*np.pi*frequency*x+phase)
+
+    model = Model(bare_sine_function, independent_vars='x', prefix=prefix)
+    params = model.make_params()
+
+    return model, params
+
+def make_sine_model(self, prefix=None):
     """ This method creates a model of sine.
 
     @return tuple: (object model, object params)
@@ -56,32 +96,36 @@ def make_sine_model(self):
     For further information have a look in:
     http://cars9.uchicago.edu/software/python/lmfit/builtin_models.html#models.GaussianModel
     """
-    def sine_function(x, amplitude, frequency, phase):
-        """
-        Function of a sine.
-        @param x: variable variable - e.g. time
-        @param amplitude: amplitude
-        @param frequency: frequency
-        @param phase: phase
+    baresine_model, params = self.make_baresine_model(prefix=prefix)
+    amplitude_model, params = self.make_amplitude_model(prefix=prefix)
 
-        @return: sine function: in order to use it as a model
-        """
+    sine_model = amplitude_model*baresine_model
+    params = sine_model.make_params()
 
-        return amplitude*np.sin(2*np.pi*frequency*x+phase)
+    return sine_model, params
 
-    constant_model, params = self.make_constant_model()
-    model = Model(sine_function) + constant_model
 
-    params = model.make_params()
+def make_sineoffset_model(self, prefix=None):
+    """ Create a complete sine function model.
 
-    return model, params
+    @param str prefix: optional,
+    @return:
+    """
 
+    baresine_model, params = self.make_baresine_model(prefix=prefix)
+    amplitude_model, params = self.make_amplitude_model(prefix=prefix)
+    constant_model, params = self.make_constant_model(prefix=prefix)
+
+    complete_sine_model = amplitude_model*baresine_model + constant_model
+    params = complete_sine_model.make_params()
+
+    return complete_sine_model, params
 
 def make_sine_fit(self, axis=None, data=None, add_parameters=None):
-    """ This method performes a sine fit on the provided data.
+    """ Perform a simple sine fit on the provided data.
 
     @param array[] axis: axis values
-    @param array[]  x_data: data
+    @param array[] data: data
     @param dict add_parameters: Additional parameters
 
     @return object result: lmfit.model.ModelFit object, all parameters
@@ -91,7 +135,6 @@ def make_sine_fit(self, axis=None, data=None, add_parameters=None):
     """
 
     sine, params = self.make_sine_model()
-
     error, params = self.estimate_sine(axis, data, params)
 
     # overwrite values of additional parameters
@@ -108,13 +151,119 @@ def make_sine_fit(self, axis=None, data=None, add_parameters=None):
     return result
 
 
+def make_sineoffset_fit(self, axis=None, data=None, add_parameters=None):
+    """ Perform a sine fit with a constant offset on the provided data.
+
+    @param array[] axis: axis values
+    @param array[] data: data
+    @param dict add_parameters: Additional parameters
+
+    @return object result: lmfit.model.ModelFit object, all parameters
+                           provided about the fitting, like: success,
+                           initial fitting values, best fitting values, data
+                           with best fit with given axis,...
+    """
+
+    sine, params = self.make_sineoffset_model()
+
+    error, params = self.estimate_sineoffset(axis, data, params)
+
+    # overwrite values of additional parameters
+    if add_parameters is not None:
+        params = self._substitute_parameter(parameters=params,
+                                            update_dict=add_parameters)
+    try:
+        result = sine.fit(data, x=axis, params=params)
+    except:
+        logger.warning('The sine fit did not work.')
+        result = sine.fit(data, x=axis, params=params)
+        print(result.message)
+
+    return result
+
+
 def estimate_sine(self, x_axis=None, data=None, params=None):
+    """ Sine estimator, with an amplitude, frequency and phase.
+
+    @param array x_axis: 1D array for the x-axis
+    @param array data:  1D array for the y values
+    @param params: Parameter contrainer
+
+    @return : tuple(error, params)
+    """
+
+    # Convert for safety:
+    x_axis = np.array(x_axis)
+    data = np.array(data)
+
+
+    error = 0
+    # check if parameters make sense
+    parameters = [x_axis, data]
+    for var in parameters:
+        if not isinstance(var, (frozenset, list, set, tuple, np.ndarray)):
+            logger.error('Given parameter is no array.')
+            error = -1
+        elif len(np.shape(var)) != 1:
+            logger.error('Given parameter is no one dimensional array.')
+            error = -1
+    if not isinstance(params, Parameters):
+        logger.error('Parameters object is not valid in estimate_gaussian.')
+        error = -1
+
+    # estimate amplitude
+    ampl_val = max(np.abs(data.min()), np.abs(data.max()))
+
+    # it is assumed that no offset is used
+    # perform fourier transform with zeropadding to get higher resolution
+    data_level_zeropaded = np.zeros(int(len(data)*2))
+    data_level_zeropaded[:len(data)] = data
+
+    fourier = np.fft.fft(data_level_zeropaded)
+    stepsize = x_axis[1]-x_axis[0]  # for frequency axis
+    freq = np.fft.fftfreq(data_level_zeropaded.size, stepsize)
+    frequency_max = np.abs(freq[np.log(fourier).argmax()])
+
+
+    # find minimal distance to the next meas point in the corresponding time value>
+    min_x_diff = np.ediff1d(x_axis).min()
+
+    # How many points are used to sample the estimated frequency with min_x_diff:
+    iter_steps = int(1/(frequency_max*min_x_diff))
+    if iter_steps < 1:
+        iter_steps = 1
+
+    sum_res = np.zeros(iter_steps)
+
+    # Procedure: Create sin waves with different phases and perform a summation.
+    #            The sum shows how well the sine was fitting to the actual data.
+    #            The best fitting sine should be a maximum of the summed
+    #            convoluted time trace.
+
+    for iter_s in range(iter_steps):
+        func_val = ampl_val * np.sin(2*np.pi*frequency_max*x_axis + iter_s/iter_steps *2*np.pi)
+        sum_res[iter_s] = (data + func_val).sum()
+
+    # The minimum indicates where the sine function was fittng the worst,
+    # therefore subtract pi. This will also ensure that the estimated phase will
+    # be in the interval [-pi,pi].
+    phase = sum_res.argmin()/iter_steps *2*np.pi - np.pi
+
+    # values and bounds of initial parameters
+    params['amplitude'].set(value=ampl_val)
+    params['frequency'].set(value=frequency_max, min=0.0, max=1/(stepsize)*3)
+    params['phase'].set(value=phase, min=-np.pi, max=np.pi)
+
+    return error, params
+
+def estimate_sineoffset(self, x_axis=None, data=None, params=None):
     """ This method provides a estimation of a initial values
      for a sine function.
 
     @param array x_axis: x values
     @param array data: value of each data point corresponding to x values
-    @param Parameters object params: object includes parameter dictionary which can be set
+    @param Parameters object params: object includes parameter dictionary which
+                                     can be set
 
     @return tuple (error, params):
 
@@ -143,42 +292,14 @@ def estimate_sine(self, x_axis=None, data=None, params=None):
     offset = np.average(data)
 
     # level data
-    data_level = data - offset
+    data_level = data-offset
 
-    # estimate amplitude
-    params['amplitude'].value = max(np.abs(data_level.min()), np.abs(data_level.max()))
+    error, params = self.estimate_sine(x_axis=None, data=data_level, params=params)
 
-    # perform fourier transform with zeropadding to get higher resolution
-    data_level_zeropaded = np.zeros(int(len(data_level)*2))
-    data_level_zeropaded[:len(data_level)] = data_level
-    fourier = np.fft.fft(data_level_zeropaded)
-    stepsize = x_axis[1]-x_axis[0]  # for frequency axis
-    freq = np.fft.fftfreq(data_level_zeropaded.size, stepsize)
-    frequency_max = np.abs(freq[np.log(fourier).argmax()])
-
-    # estimating the phase from the first point
-    # TODO: This only works when data starts at 0
-    phase_tmp = (data_level[0])/params['amplitude'].value
-    phase = abs(np.arcsin(phase_tmp))
-
-
-    if np.gradient(data)[0] < 0 and data_level[0] > 0:
-        phase = np.pi - phase
-    elif np.gradient(data)[0] < 0 and data_level[0] < 0:
-        phase += np.pi
-    elif np.gradient(data)[0] > 0 and data_level[0] < 0:
-        phase = 2.*np.pi - phase
-
-    params['frequency'].value = frequency_max
-    params['phase'].value = phase
-    params['offset'].value = offset
-
-    #Adding constraints
-    params['frequency'].min = 0.0
-    params['frequency'].max = 1/(stepsize) * 3
-
+    params['offset'].set(value=offset)
 
     return error, params
+
 
 ############################################################################
 #                                                                          #
@@ -186,7 +307,7 @@ def estimate_sine(self, x_axis=None, data=None, params=None):
 #                                                                          #
 ############################################################################
 
-def make_sineexponentialdecay_model(self):
+def make_sineexponentialdecay_model(self, prefix=None):
     """
     This method creates a model of sine with exponential decay.
 
@@ -208,27 +329,16 @@ def make_sineexponentialdecay_model(self):
     http://cars9.uchicago.edu/software/python/lmfit/builtin_models.html#models.GaussianModel
     """
 
-    def sine_function(x, amplitude, frequency, phase):
-        """
-        Function of a sine.
-        @param x: variable variable - e.g. time
-        @param amplitude: amplitude
-        @param frequency: frequency
-        @param phase: phase
+    sine_model, params = self.make_sine_model(prefix=prefix)
+    bareexponentialdecay_model, params = self.make_bareexponentialdecay_model(prefix=prefix)
+    constant_model, params = self.make_constant_model(prefix=prefix)
 
-        @return: sine function: in order to use it as a model
-        """
-
-        return amplitude * np.sin(2 * np.pi * frequency * x + phase)
-
-    bareexponentialdecay_model, params = self.make_bareexponentialdecay_model()
-    constant_model, params = self.make_constant_model()
-    model = Model(sine_function)*bareexponentialdecay_model + constant_model
+    model = sine_model*bareexponentialdecay_model + constant_model
     params = model.make_params()
 
     return model, params
 
-def estimate_sineexponentialdecay(self,x_axis=None, data=None, params=None):
+def estimate_sineexponentialdecay(self, x_axis=None, data=None, params=None):
     """
     This method provides a estimation of a initial values
      for a sine exponential decay function.
@@ -244,8 +354,13 @@ def estimate_sineexponentialdecay(self,x_axis=None, data=None, params=None):
         Parameters object params: set parameters of initial values
     """
 
+    # Convert for safety:
+    x_axis = np.array(x_axis)
+    data = np.array(data)
+
     error = 0
     parameters = [x_axis, data]
+
     #varification of data
     for var in parameters:
         if not isinstance(var, (frozenset, list, set, tuple, np.ndarray)):
@@ -259,12 +374,13 @@ def estimate_sineexponentialdecay(self,x_axis=None, data=None, params=None):
         error = -1
 
     # set the offset as the median of the data
-    offset = np.median(data)
+    offset = np.mean(data)
+
     # level data
     data_level = data - offset
 
     # estimate amplitude
-    params['amplitude'].value = max(np.abs(data_level.min()), np.abs(data_level.max()))
+    ampl_val = max(np.abs(data_level.min()), np.abs(data_level.max()))
 
     # perform fourier transform with zeropadding to get higher resolution
     data_level_zeropaded = np.zeros(int(len(data_level) * 2))
@@ -272,43 +388,58 @@ def estimate_sineexponentialdecay(self,x_axis=None, data=None, params=None):
     fourier = np.fft.fft(data_level_zeropaded)
     stepsize = x_axis[1] - x_axis[0]  # for frequency axis
     freq = np.fft.fftfreq(data_level_zeropaded.size, stepsize)
-    fourier_power = (fourier * fourier.conj()).real
+    fourier_power = abs(fourier)
     frequency_max = np.abs(freq[fourier_power.argmax()])
-    params['frequency'].value = frequency_max
 
-    #remove noise
+    # remove noise
     a = np.std(fourier_power[:int(len(freq)/2)])
     for i in range(0,int(len(fourier)/2)):
-        if fourier_power[i]<=a:
+        if fourier_power[i] <= a:
             fourier_power[i] = 0
 
-    #calculating the width of the FT peak for the estimation of lifetime
-    s = 0
-    for i in range(0,int(len(freq) / 2)):
-        s+= fourier_power[i]*abs(freq[1]-freq[0])/max(fourier_power[:int(len(freq) / 2)])
-    params['lifetime'].value = 0.5 / s
+    # calculating the width of the FT peak for the estimation of lifetime
+    peak_width = 0
+    for i in range(0, int(len(freq) / 2)):
+        peak_width += fourier_power[i]*abs(freq[1]-freq[0])/max(fourier_power[:int(len(freq) / 2)])
 
-    # estimating the phase from the first point
-    # TODO: This only works when data starts at 0
-    phase_tmp = (data_level[0]) / params['amplitude'].value
-    phase = abs(np.arcsin(phase_tmp))
+    lifetime = 0.5 / peak_width
 
-    if np.gradient(data)[0] < 0 and data_level[0] > 0:
-        phase = np.pi - phase
-    elif np.gradient(data)[0] < 0 and data_level[0] < 0:
-        phase += np.pi
-    elif np.gradient(data)[0] > 0 and data_level[0] < 0:
-        phase = 2. * np.pi - phase
+    # find minimal distance to the next meas point in the corresponding time value>
+    min_x_diff = np.ediff1d(x_axis).min()
+
+    # How many points are used to sample the estimated frequency with min_x_diff:
+    iter_steps = int(1/(frequency_max*min_x_diff))
+    if iter_steps < 1:
+        iter_steps = 1
+
+    sum_res = np.zeros(iter_steps)
+
+    # Procedure: Create sin waves with different phases and perform a summation.
+    #            The sum shows how well the sine was fitting to the actual data.
+    #            The best fitting sine should be a maximum of the summed
+    #            convoluted time trace.
+
+    for iter_s in range(iter_steps):
+        func_val = ampl_val * np.sin(2*np.pi*frequency_max*x_axis + iter_s/iter_steps *2*np.pi)
+        sum_res[iter_s] = (data_level + func_val).sum()
+
+    # The minimum indicates where the sine function was fittng the worst,
+    # therefore subtract pi. This will also ensure that the estimated phase will
+    # be in the interval [-pi,pi].
+    phase = sum_res.argmin()/iter_steps *2*np.pi - np.pi
 
     # values and bounds of initial parameters
-    params['phase'].value = phase
-    params['offset'].value = offset
+    params['phase'].set(value=phase, min=-np.pi, max=np.pi)
+    params['amplitude'].set(value=ampl_val)
+    params['offset'].set(value=offset)
 
-    params['lifetime'].min = 3 * (x_axis[1]-x_axis[0])
-    params['lifetime'].max = 1/(abs(freq[1]-freq[0])*1.5)
-    params['frequency'].min = min(0.1 / (x_axis[-1]-x_axis[0]),freq[3])
-    params['frequency'].max = min(0.5 / stepsize, freq.max()-abs(freq[2]-freq[0]))
-    params['amplitude'].min = 0
+    params['lifetime'].set(value=lifetime,
+                           min=3*(x_axis[1]-x_axis[0]),
+                           max=1/(abs(freq[1]-freq[0])*1.5))
+
+    params['frequency'].set(value=frequency_max,
+                            min=min(0.1 / (x_axis[-1]-x_axis[0]),freq[3]),
+                            max=min(0.5 / stepsize, freq.max()-abs(freq[2]-freq[0])))
 
     return error, params
 
@@ -317,7 +448,7 @@ def make_sineexponentialdecay_fit(self, axis=None, data=None, add_parameters=Non
     This method performes a sine exponential decay fit on the provided data.
 
     @param array[] axis: axis values
-    @param array[]  x_data: data
+    @param array[] data: data
     @param dict add_parameters: Additional parameters
 
     @return object result: lmfit.model.ModelFit object, all parameters
