09d40fd9928ba2995314d6f5b14dee525abaec6f
Alexander Stark <alexander.stark@uni-ulm.de>
2016-11-18 20:01:41 +0100

    Complete review of the sine fitting methods. Structure all models, estimators and fits.

--- logic/fitmethods/sinemethods.py
+++ logic/fitmethods/sinemethods.py
@@ -29,65 +29,18 @@ from lmfit import Parameters
 from core.util.units import compute_dft
 
 
-############################################################################
-#                                                                          #
-#                               Sinus fitting                              #
-#                                                                          #
-############################################################################
-
-def make_baresine_model(self, prefix=None):
-    """ This method creates a model of bare sine without amplitude and
-    offset.
-
-    @param string prefix: variable prefix
-
-    @return tuple: (object model, object params)
-
-    Explanation of the objects:
-        object lmfit.model.CompositeModel model:
-            A model the lmfit module will use for that fit. Here a
-            gaussian model. Returns an object of the class
-            lmfit.model.CompositeModel.
-
-        object lmfit.parameter.Parameters params:
-            It is basically an OrderedDict, so a dictionary, with keys
-            denoting the parameters as string names and values which are
-            lmfit.parameter.Parameter (without s) objects, keeping the
-            information about the current value.
-
-    For further information have a look in:
-    http://cars9.uchicago.edu/software/python/lmfit/builtin_models.html#models.GaussianModel
-    """
-    def sine_function(x, frequency, phase):
-        """
-        Function of a sine.
-        @param x: variable variable - e.g. time
-        @param frequency: frequency
-        @param phase: phase
-
-        @return: sine function: in order to use it as a model
-        """
-
-        return np.sin(2*np.pi*frequency*x+phase)
-    if prefix is not None:
-        model = Model(sine_function, prefix=prefix)
-    else:
-        model = Model(sine_function)
-
-    params = model.make_params()
-
-    return model, params
-
-# def make_sine_model(self):
-#     """ This method creates a model of sine.
-
+################################################################################
+#                                                                              #
+#                               Bare Sinus fitting                             #
+#                                                                              #
+################################################################################
 
 def make_baresine_model(self, prefix=None):
     """ Create a bare sine model without amplitude and offset.
 
-    @param str prefix: optional, if multiple models should be used and the
-                       parameters of each model should be distinguished from
-                       each other.
+    @param str prefix: optional, if multiple models should be used in a
+                       composite way and the parameters of each model should be
+                       distinguished from each other to prevent name collisions.
 
     @return tuple: (object model, object params)
 
@@ -102,14 +55,17 @@ def make_baresine_model(self, prefix=None):
             denoting the parameters as string names and values which are
             lmfit.parameter.Parameter (without s) objects, keeping the
             information about the current value.
+
+        For further information have a look in:
+    http://cars9.uchicago.edu/software/python/lmfit/builtin_models.html#models.GaussianModel
     """
 
     def bare_sine_function(x, frequency, phase):
-        """ Function of a sine.
+        """ Function of a bare sine.
 
-        @param x: independant variable - e.g. time
-        @param frequency: frequency
-        @param phase: phase
+        @param numpy.array x: independant variable - e.g. time
+        @param float frequency: frequency
+        @param float phase: phase
 
         @return: reference to method of a sine function in order to use it as a
                  model
@@ -122,25 +78,23 @@ def make_baresine_model(self, prefix=None):
 
     return model, params
 
+
+################################################################################
+#                                                                              #
+#                                Sinus fitting                                 #
+#                                                                              #
+################################################################################
+
+
 def make_sine_model(self, prefix=None):
-    """ This method creates a model of sine.
+    """ Create a model of sine with an amplitude.
 
-    @return tuple: (object model, object params)
+    @param str prefix: optional, if multiple models should be used in a
+                       composite way and the parameters of each model should be
+                       distinguished from each other to prevent name collisions.
 
-    Explanation of the objects:
-        object lmfit.model.CompositeModel model:
-            A model the lmfit module will use for that fit. Here a
-            gaussian model. Returns an object of the class
-            lmfit.model.CompositeModel.
-
-        object lmfit.parameter.Parameters params:
-            It is basically an OrderedDict, so a dictionary, with keys
-            denoting the parameters as string names and values which are
-            lmfit.parameter.Parameter (without s) objects, keeping the
-            information about the current value.
-
-    For further information have a look in:
-    http://cars9.uchicago.edu/software/python/lmfit/builtin_models.html#models.GaussianModel
+    @return tuple: (object model, object params), for more description see in
+                   the method make_baresine_model.
     """
     baresine_model, params = self.make_baresine_model(prefix=prefix)
     amplitude_model, params = self.make_amplitude_model(prefix=prefix)
@@ -150,112 +104,27 @@ def make_sine_model(self, prefix=None):
 
     return sine_model, params
 
-
-def make_sineoffset_model(self, prefix=None):
-    """ Create a complete sine function model.
-
-    @param str prefix: optional,
-    @return:
-    """
-
-    baresine_model, params = self.make_baresine_model(prefix=prefix)
-    amplitude_model, params = self.make_amplitude_model(prefix=prefix)
-    constant_model, params = self.make_constant_model(prefix=prefix)
-
-    complete_sine_model = amplitude_model*baresine_model + constant_model
-    params = complete_sine_model.make_params()
-
-    return complete_sine_model, params
-
-def make_sine_fit(self, axis=None, data=None, add_parameters=None):
-    """ Perform a simple sine fit on the provided data.
-
-    @param array[] axis: axis values
-    @param array[] data: data
-    @param dict add_parameters: Additional parameters
-
-    @return object result: lmfit.model.ModelFit object, all parameters
-                           provided about the fitting, like: success,
-                           initial fitting values, best fitting values, data
-                           with best fit with given axis,...
-    """
-
-    sine, params = self.make_sine_model()
-    error, params = self.estimate_sine(axis, data, params)
-
-    # overwrite values of additional parameters
-    if add_parameters is not None:
-        params = self._substitute_parameter(parameters=params,
-                                            update_dict=add_parameters)
-    try:
-        result = sine.fit(data, x=axis, params=params)
-    except:
-        logger.warning('The sine fit did not work.')
-        result = sine.fit(data, x=axis, params=params)
-        print(result.message)
-
-    return result
-
-
-def make_sineoffset_fit(self, axis=None, data=None, add_parameters=None):
-    """ Perform a sine fit with a constant offset on the provided data.
-
-    @param array[] axis: axis values
-    @param array[] data: data
-    @param dict add_parameters: Additional parameters
-
-    @return object result: lmfit.model.ModelFit object, all parameters
-                           provided about the fitting, like: success,
-                           initial fitting values, best fitting values, data
-                           with best fit with given axis,...
-    """
-
-    sine, params = self.make_sineoffset_model()
-
-    error, params = self.estimate_sineoffset(axis, data, params)
-
-    # overwrite values of additional parameters
-    if add_parameters is not None:
-        params = self._substitute_parameter(parameters=params,
-                                            update_dict=add_parameters)
-    try:
-        result = sine.fit(data, x=axis, params=params)
-    except:
-        logger.warning('The sine fit did not work.')
-        result = sine.fit(data, x=axis, params=params)
-        print(result.message)
-
-    return result
-
-
-def estimate_sine(self, x_axis=None, data=None, params=None):
+def estimate_sine(self, x_axis, data, params):
     """ Sine estimator, with an amplitude, frequency and phase.
 
-    @param array x_axis: 1D array for the x-axis
-    @param array data:  1D array for the y values
-    @param params: Parameter contrainer
+    @param numpy.array x_axis: 1D axis values
+    @param numpy.array data: 1D data, should have the same dimension as x_axis.
+    @param lmfit.Parameters params: object includes parameter dictionary which
+                                    can be set
 
-    @return : tuple(error, params)
+    @return tuple (error, params):
+
+    Explanation of the return parameter:
+        int error: error code (0:OK, -1:error)
+        lmfit.Parameters params: derived OrderedDict object contains the initial
+                                 values for the fit.
     """
 
     # Convert for safety:
     x_axis = np.array(x_axis)
     data = np.array(data)
 
-
-    error = 0
-    # check if parameters make sense
-    parameters = [x_axis, data]
-    for var in parameters:
-        if not isinstance(var, (frozenset, list, set, tuple, np.ndarray)):
-            logger.error('Given parameter is no array.')
-            error = -1
-        elif len(np.shape(var)) != 1:
-            logger.error('Given parameter is no one dimensional array.')
-            error = -1
-    if not isinstance(params, Parameters):
-        logger.error('Parameters object is not valid in estimate_gaussian.')
-        error = -1
+    error = self._check_1D_input(x_axis=x_axis, data=data, params=params)
 
     # estimate amplitude
     ampl_val = max(np.abs(data.min()), np.abs(data.max()))
@@ -310,14 +179,70 @@ def estimate_sine(self, x_axis=None, data=None, params=None):
 
     return error, params
 
-def estimate_sineoffset(self, x_axis=None, data=None, params=None):
-    """ This method provides a estimation of a initial values
-     for a sine function.
 
-    @param array x_axis: x values
-    @param array data: value of each data point corresponding to x values
-    @param Parameters object params: object includes parameter dictionary which
-                                     can be set
+def make_sine_fit(self, x_axis, data, add_parameters=None):
+    """ Perform a simple sine fit on the provided data.
+
+    @param numpy.array x_axis: 1D axis values
+    @param numpy.array data: 1D data, should have the same dimension as x_axis.
+    @param dict add_parameters: optional, additional parameters for the fit,
+                                which will be used instead of the values from
+                                the estimator.
+
+    @return object result: lmfit.model.ModelFit object, all parameters
+                           provided about the fitting, like: success,
+                           initial fitting values, best fitting values, data
+                           with best fit with given axis,...
+    """
+
+    sine, params = self.make_sine_model()
+    error, params = self.estimate_sine(x_axis, data, params)
+
+    # overwrite values of additional parameters
+    if add_parameters is not None:
+        params = self._substitute_parameter(parameters=params,
+                                            update_dict=add_parameters)
+    try:
+        result = sine.fit(data, x=x_axis, params=params)
+    except:
+        logger.warning('The sine fit did not work.')
+        result = sine.fit(data, x=x_axis, params=params)
+        print(result.message)
+
+    return result
+
+################################################################################
+#                                                                              #
+#                         Sinus with offset fitting                            #
+#                                                                              #
+################################################################################
+
+def make_sineoffset_model(self, prefix=None):
+    """ Create a sine model with amplitude and offset.
+
+    @param str prefix: optional, if multiple models should be used in a
+                       composite way and the parameters of each model should be
+                       distinguished from each other to prevent name collisions.
+
+    @return tuple: (object model, object params), for more description see in
+                   the method make_baresine_model.
+    """
+
+    sine_model, params = self.make_sine_model(prefix=prefix)
+    constant_model, params = self.make_constant_model(prefix=prefix)
+
+    sine_offset_model = sine_model + constant_model
+    params = sine_offset_model.make_params()
+
+    return sine_offset_model, params
+
+def estimate_sineoffset(self, x_axis, data, params):
+    """ Provides an estimator to obtain initial values for sine fitting.
+
+    @param numpy.array x_axis: 1D axis values
+    @param numpy.array data: 1D data, should have the same dimension as x_axis.
+    @param lmfit.Parameters params: object includes parameter dictionary which
+                                    can be set
 
     @return tuple (error, params):
 
@@ -326,27 +251,13 @@ def estimate_sineoffset(self, x_axis=None, data=None, params=None):
         Parameters object params: set parameters of initial values
     """
 
-    error = 0
-    # check if parameters make sense
-    parameters = [x_axis, data]
-    for var in parameters:
-        if not isinstance(var, (frozenset, list, set, tuple, np.ndarray)):
-            logger.error('Given parameter is no array.')
-            error = -1
-        elif len(np.shape(var)) != 1:
-            logger.error('Given parameter is no one dimensional array.')
-            error = -1
-    if not isinstance(params, Parameters):
-        logger.error('Parameters object is not valid in estimate_gaussian.')
-        error = -1
-
-    # set parameters
+    error = self._check_1D_input(x_axis=x_axis, data=data, params=params)
 
     # set the offset as the average of the data
     offset = np.average(data)
 
     # level data
-    data_level = data-offset
+    data_level = data - offset
 
     error, params = self.estimate_sine(x_axis=x_axis, data=data_level, params=params)
 
@@ -354,6 +265,37 @@ def estimate_sineoffset(self, x_axis=None, data=None, params=None):
 
     return error, params
 
+def make_sineoffset_fit(self, x_axis, data, add_parameters=None):
+    """ Perform a sine fit with a constant offset on the provided data.
+
+    @param numpy.array x_axis: 1D axis values
+    @param numpy.array data: 1D data, should have the same dimension as x_axis.
+    @param dict add_parameters: optional, additional parameters for the fit,
+                                which will be used instead of the values from
+                                the estimator.
+
+    @return object result: lmfit.model.ModelFit object, all parameters
+                           provided about the fitting, like: success,
+                           initial fitting values, best fitting values, data
+                           with best fit with given axis,...
+    """
+
+    sine, params = self.make_sineoffset_model()
+
+    error, params = self.estimate_sineoffset(x_axis, data, params)
+
+    # overwrite values of additional parameters
+    if add_parameters is not None:
+        params = self._substitute_parameter(parameters=params,
+                                            update_dict=add_parameters)
+    try:
+        result = sine.fit(data, x=x_axis, params=params)
+    except:
+        logger.warning('The sine fit did not work.')
+        result = sine.fit(data, x=x_axis, params=params)
+        print(result.message)
+
+    return result
 
 ############################################################################
 #                                                                          #
