3d0b8952866f22b4afc4aacd720da9844c2034aa
AndreaFilipovski <andrea.kurz@uni-ulm.de>
2016-12-12 15:41:34 +0100

    Added the to my current knowledge necessary methods to the interfaces AnalogReaderInterface and PiezoStepperInterface.

--- interface/analog_reader_interface.py
+++ interface/analog_reader_interface.py
@@ -26,6 +26,74 @@ from core.util.interfaces import InterfaceMetaclass
 
 class AnalogReaderInterface(metaclass=InterfaceMetaclass):
     """ This is the Interface class to define the controls for the simple
-    alaog input hardware.
+    analog input hardware.
     """
-    pass
+
+    _modtype = 'AnalogReaderInterface'
+    _modclass = 'interface'
+
+    @abc.abstractmethod
+    def set_up_clock(self, clock_frequency=None, clock_channel=None):
+        """ Configures the hardware clock of the NiDAQ card to give the timing.
+
+        @param float clock_frequency: if defined, this sets the frequency of
+                                      the clock
+        @param string clock_channel: if defined, this is the physical channel
+                                     of the clock
+
+        @return int: error code (0:OK, -1:error)
+        """
+        pass
+
+    @abc.abstractmethod
+    def set_up_analog_reader(self,
+                             analog_channel=None,
+                             voltage_levels=None,
+                             clock_channel=None,
+                             analog_buffer=None,
+                             analog_mode=None,
+                             channel=None):
+        """ Configures the actual counter with a given clock.
+
+        @param str analog_channel: optional, physical channel of the voltage input, list of two
+                                    for differential reading
+        @param str voltage_levels: optional, list with two inputs that specifies the max. and
+                                  minimum voltage levels of the input
+        @param str clock_channel: optional, specifies the clock channel for the
+                                  counter
+        @param int analog_buffer: optional, a buffer of specified integer
+                                   length, where in each bin the count numbers
+                                   are saved.
+        @param str analog_mode: optional, decides on the read mode of the counter (res,
+        nres or differential)
+        @param int channel: optional handler, distinguishes between different analog inputs as
+                                   up to 31 are possible in parallel.
+
+        @return int: error code (0:OK, -1:error)
+        """
+        pass
+
+    def get_analog_levels(self, samples=None):
+        """ Returns the current voltages per bin at the analog input.
+
+        @param int samples: if defined, number of samples to read in one go
+
+        @return numpy.array(uint32): the voltage per time bin
+        """
+        pass
+
+    @abc.abstractmethod
+    def close_analog_reader(self):
+        """ Closes the counter and cleans up afterwards.
+
+        @return int: error code (0:OK, -1:error)
+        """
+        pass
+
+    @abc.abstractmethod
+    def close_clock(self):
+        """ Closes the clock and cleans up afterwards.
+
+        @return int: error code (0:OK, -1:error)
+        """
+        pass
--- interface/piezo_stepper_interface.py
+++ interface/piezo_stepper_interface.py
@@ -28,4 +28,74 @@ class PiezoStepperInterface(metaclass=InterfaceMetaclass):
     """ This is the Interface class to define the controls for the simple
     piezo stepper hardware.
     """
-    pass
+
+    _modtype = 'PiezoStepperInterface'
+    _modclass = 'interface'
+
+
+    @abc.abstractmethod
+    def reset_hardware(self):
+        """ Resets the hardware, so the connection is lost and other programs
+            can access it.
+
+        @return int: error code (0:OK, -1:error)
+        """
+        pass
+
+    @abc.abstractmethod
+    def get_position_range(self):
+        """ Returns the physical range of the scanner.
+
+        @return float [4][2]: array of 4 ranges with an array containing lower
+                              and upper limit
+        """
+        pass
+
+    @abc.abstractmethod
+    def set_position_range(self, myrange=None):
+        """ Sets the physical range of the scanner.
+
+        @param float [n][2] myrange: array of n ranges with an array containing
+                                     lower and upper limit, n defined in config
+
+        @return int: error code (0:OK, -1:error)
+        """
+        pass
+
+    @abc.abstractmethod
+    def set_amplitude(self, amplitude=None):
+        """ Sets the voltage of the steps.
+
+        @param float amplitude: float specifying the voltage used per step
+
+        @return int: error code (0:OK, -1:error)
+        """
+        pass
+
+    @abc.abstractmethod
+    def set_temperature(self, temp):
+        """Changes the temperature settings of the setup to the given temperature adjusting ranges
+
+        @param float temp: current hardware measured temperature of the setup
+        @return int, float : error code (0:OK, -1:error), temperature the setup was set to
+        """
+        pass
+
+    @abc.abstractmethod
+    def step(self, steps=None, axis=None):
+        """ Steps the stepper a given number of steps
+
+        @param steps: Number of steps, None equals one, -1 steps continuously
+        @param int axis: Defines the axis which is to be stepped
+
+        @return: error code (0:OK, -1:error)
+        """
+        pass
+
+    @abc.abstractmethod
+    def close_stepper(self):
+        """ Closes the stepper and cleans up afterwards.
+
+        @return int: error code (0:OK, -1:error)
+        """
+        pass
