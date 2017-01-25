55caa3643ed0a547f32bc4ac1cc447ae5dc01aac
Alexander Stark <alexander.stark@uni-ulm.de>
2017-01-25 10:58:01 +0100

    Replace pyqtgraph import by qtpy.

--- logic/pulsed_master_logic.py
+++ logic/pulsed_master_logic.py
@@ -21,7 +21,7 @@ top-level directory of this distribution and at <https://github.com/Ulm-IQO/qudi
 """
 
 from logic.generic_logic import GenericLogic
-from pyqtgraph.Qt import QtCore
+from qtpy import QtCore
 from collections import OrderedDict
 import numpy as np
 
