7c59c07b4cc635f851ca774e374caaf43e38b340
Jan M. Binder <jan-git@nlogn.org>
2017-01-31 17:03:15 +0100

    small update to fitsettings
    this does not make anything work, but merely prepares for it in the future

--- gui/fitsettings.py
+++ gui/fitsettings.py
@@ -34,28 +34,32 @@ class FitSettingsWidget(QtWidgets.QWidget):
         super().__init__()
 
         self._Layout = QtWidgets.QGridLayout(self)  # Creation of the grid Layout
-        self.custom_params_checkbox = QtWidgets.QCheckBox('Use custom values')
-        self._Layout.addWidget(self.custom_params_checkbox, 0, 0)
+        self.useLabel = QtWidgets.QLabel('Edit?')
         self.valueLabel = QtWidgets.QLabel('Value')
-        self.minimumLabel = QtWidgets.QLabel('Min')
-        self.maximumLabel = QtWidgets.QLabel('Max')
-        self.varyLabel = QtWidgets.QLabel('Vary')
-        self.exprLabel = QtWidgets.QLabel('Expr')
-        self._Layout.addWidget(self.valueLabel, 1, 1)
-        self._Layout.addWidget(self.minimumLabel, 1, 2)
-        self._Layout.addWidget(self.maximumLabel, 1, 3)
-        self._Layout.addWidget(self.varyLabel, 1, 5)
-        self._Layout.addWidget(self.exprLabel, 1, 4)
+        self.minimumLabel = QtWidgets.QLabel('Minimum')
+        self.maximumLabel = QtWidgets.QLabel('Maximum')
+        self.exprLabel = QtWidgets.QLabel('Expression')
+        self.varyLabel = QtWidgets.QLabel('Vary?')
+
+        self._Layout.addWidget(self.useLabel, 0, 0)
+        self._Layout.addWidget(self.valueLabel, 0, 2)
+        self._Layout.addWidget(self.minimumLabel, 0, 3)
+        self._Layout.addWidget(self.maximumLabel, 0, 4)
+        self._Layout.addWidget(self.exprLabel, 0, 5)
+        self._Layout.addWidget(self.varyLabel, 0, 6)
 
         self.widgets = {}
+        self.paramUseSettings = {}
         n = 2
         for name, param in parameters.items():
-            self.widgets[name+"_label"] = parameterNameLabel = QtWidgets.QLabel(str(name),parent=self)
-            self.widgets[name+'_value'] = valueSpinbox =  SpinBox(parent=self)
-            self.widgets[name+'_min'] = minimumSpinbox = SpinBox(parent=self)
-            self.widgets[name+'_max'] = maximumSpinbox = SpinBox(parent=self)
-            self.widgets[name+'_expr'] = expressionLineEdit = QtWidgets.QLineEdit(parent=self)
-            self.widgets[name+'_vary'] = varyCheckbox = QtWidgets.QCheckBox(parent=self)
+            self.paramUseSettings[name] = False
+            self.widgets[name + '_use'] = useCheckbox = QtWidgets.QCheckBox(parent=self)
+            self.widgets[name + '_label'] = parameterNameLabel = QtWidgets.QLabel(str(name), parent=self)
+            self.widgets[name + '_value'] = valueSpinbox =  SpinBox(parent=self)
+            self.widgets[name + '_min'] = minimumSpinbox = SpinBox(parent=self)
+            self.widgets[name + '_max'] = maximumSpinbox = SpinBox(parent=self)
+            self.widgets[name + '_expr'] = expressionLineEdit = QtWidgets.QLineEdit(parent=self)
+            self.widgets[name + '_vary'] = varyCheckbox = QtWidgets.QCheckBox(parent=self)
             valueSpinbox.setDecimals(3)
             valueSpinbox.setSingleStep(0.01)
             valueSpinbox.setMaximum(np.inf)
@@ -69,40 +73,46 @@ class FitSettingsWidget(QtWidgets.QWidget):
             maximumSpinbox.setMaximum(np.inf)
             maximumSpinbox.setMinimum(-np.inf)
             if param.value is not None and not math.isnan(param.value):
+                useCheckbox.setChecked(self.paramUseSettings[name])
                 valueSpinbox.setValue(param.value)
                 minimumSpinbox.setValue(param.min)
                 minimumSpinbox.setValue(param.max)
                 expressionLineEdit.setText(param.expr)
                 varyCheckbox.setChecked(param.vary)
 
-            self._Layout.addWidget(parameterNameLabel, n, 0)
-            self._Layout.addWidget(valueSpinbox, n, 1)
-            self._Layout.addWidget(minimumSpinbox, n, 2)
-            self._Layout.addWidget(maximumSpinbox, n, 3)
-            self._Layout.addWidget(expressionLineEdit, n, 4)
-            self._Layout.addWidget(varyCheckbox, n, 5)
+            self._Layout.addWidget(useCheckbox, n, 0)
+            self._Layout.addWidget(parameterNameLabel, n, 1)
+            self._Layout.addWidget(valueSpinbox, n, 2)
+            self._Layout.addWidget(minimumSpinbox, n, 3)
+            self._Layout.addWidget(maximumSpinbox, n, 4)
+            self._Layout.addWidget(expressionLineEdit, n, 5)
+            self._Layout.addWidget(varyCheckbox, n, 6)
             n += 1
 
     def updateFitSettings(self, parameters):
         """ Updates the fit parameters with the new values from the settings window
         """
         for name, param in parameters.items():
-            param.value = self.widgets[name+'_value'].value()
-            param.min = self.widgets[name+'_min'].value()
-            param.max = self.widgets[name+'_max'].value()
-            param.expr = str(self.widgets[name+'_expr'].displayText())
-            param.vary = self.widgets[name+'_vary'].checkState()
-        return self.custom_params_checkbox.isChecked()
+            self.paramUseSettings[name] = self.widgets[name + '_use'].checkState()
+            param.use = self.paramUseSettings[name] 
+            param.value = self.widgets[name + '_value'].value()
+            param.min = self.widgets[name + '_min'].value()
+            param.max = self.widgets[name + '_max'].value()
+            param.expr = str(self.widgets[name + '_expr'].displayText())
+            param.vary = self.widgets[name + '_vary'].checkState()
+        return self.paramUseSettings
 
-    def keepFitSettings(self, parameters, use_custom_parameters):
+    def keepFitSettings(self, parameters, paramUse):
         """ Keeps the old fit settings
         """
         for name, param in parameters.items():
             if parameters[name].value is not None and not math.isnan(param.value):
-                self.widgets[name+'_value'].setValue(param.value)
-                self.widgets[name+'_min'].setValue(param.min)
-                self.widgets[name+'_max'].setValue(param.max)
-                self.widgets[name+'_expr'].setText(param.expr)
-                self.widgets[name+'_vary'].setChecked(param.vary)
-        self.custom_params_checkbox.setChecked(use_custom_parameters)
+                self.paramUseSettings[name] = paramUse[name]
+                self.widgets[name + '_use'].setChecked(paramUse[name])
+                self.widgets[name + '_value'].setValue(param.value)
+                self.widgets[name + '_min'].setValue(param.min)
+                self.widgets[name + '_max'].setValue(param.max)
+                self.widgets[name + '_expr'].setText(param.expr)
+                self.widgets[name + '_vary'].setChecked(param.vary)
+        return self.paramUseSettings
 
