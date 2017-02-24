8682a517f48ebab4d98808ede24aab44f69d7057
Nikolas Tomek <tomek.qudi@gmail.com>
2017-02-24 17:25:41 +0100

    get_loaded_asset() in pulser hardware modules is now never returning None but always a string.

--- hardware/pulser_dummy.py
+++ hardware/pulser_dummy.py
@@ -100,7 +100,7 @@ class PulserDummy(Base, PulserInterface):
 
         self.uploaded_assets_list = []
         self.uploaded_files_list = []
-        self.current_loaded_asset = None
+        self.current_loaded_asset = ''
         self.is_output_enabled = True
 
         # settings for remote access on the AWG PC
@@ -392,7 +392,7 @@ class PulserDummy(Base, PulserInterface):
         Unused for digital pulse generators without storage capability
         (PulseBlaster, FPGA).
         """
-        self.current_loaded_asset = None
+        self.current_loaded_asset = ''
         return
 
     def get_status(self):
