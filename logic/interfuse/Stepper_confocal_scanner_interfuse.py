6dcc122a1741ceeffa54913b4c21bc9e6a2f1e57
AndreaFilipovski <andrea.kurz@uni-ulm.de>
2017-03-28 16:43:50 +0200

    Added init and activate method Adapted device definitions to the fact that now a scanner ("confocalscanner") and a stepper ("piezostepper") should be usable

--- logic/interfuse/Stepper_confocal_scanner_interfuse.py
+++ logic/interfuse/Stepper_confocal_scanner_interfuse.py
@@ -26,67 +26,76 @@ from core.base import Base
 from interface.confocal_scanner_interface import ConfocalScannerInterface
 from interface.piezo_stepper_interface import PiezoStepperInterface
 from interface.analog_reader_interface import AnalogReaderInterface
+from interface.slow_counter_interface import SlowCounterInterface
 
+#TODO: Discuss how to implement switch between scan modes. That is scanner and stepper.
 
 class StepperScannerInterfuse(Base, ConfocalScannerInterface, PiezoStepperInterface,
-                      AnalogReaderInterface):
+                              AnalogReaderInterface, SlowCounterInterface):
 
     _modtype = 'ConfocalScannerInterface'
     _modclass = 'hardware'
 
     # connectors
-    _in = {'confocalscanner1': 'ConfocalScannerInterface',
+    _in = {'counter': 'SlowCounterInterface',
            'piezostepper1': 'PiezoStepperInterface',
-           'analogreader1': 'AnalogReaderInterface'}
+           'analogreader1': 'AnalogReaderInterface',
+           'confocalscanner': 'ConfocalScannerInterface'}
     _out = {'stepperscanner': 'ConfocalScannerInterface'}
 
+    def __init__(self, config, **kwargs):
+        super().__init__(config=config, **kwargs)
+
+        self.log.info('The following configuration was found.')
+
+        # checking for the right configuration
+        for key in config.keys():
+            self.log.info('{0}: {1}'.format(key, config[key]))
+        #TODO: I am not sure if this is correct
 
     def on_activate(self, e):
         """ Initialisation performed during activation of the module.
         """
-
-        self._counter = self.get_in_connector('confocalscanner1')
-        self._scanning_device = self.get_in_connector('piezostepper1')
-        self._analogreader = self.get_in_connector('analogreader1')
+        self._counter = self.get_in_connector('counter')
+        self._scanning_device1 = self.get_in_connector('piezostepper1')
+        self._scanning_device2 = self.get_in_connector('confocalscanner')
+        self._analog_reader = self.get_in_connector('analogreader1')
 
 
     def on_deactivate(self, e):
         self.reset_hardware()
 
-    @abc.abstractmethod
     def reset_hardware(self):
         """ Resets the hardware, so the connection is lost and other programs
             can access it.
 
         @return int: error code (0:OK, -1:error)
         """
-        return self._counter.reset_hardware()
+        #TODO: Not sure what to do for the NIDAQ here, that is as there is no reset hardware in
+        # counter. And also, should there be one in analog_reader?
+        return self._scanning_device1.reset_hardware()
 
-
-    @abc.abstractmethod
     def get_position_range(self):
         """ Returns the physical range of the scanner.
         This is a direct pass-through to the stepper hardware.
 
-        @return float [4][2]: array of 4 ranges with an array containing lower
-                              and upper limit
+        @return float [n][2]: array of n ranges with an array containing lower
+                              and upper limit,  n defined in config
         """
-        return self._scanning_device.get_position_range()
 
+        return self._scanning_device1.get_position_range()
 
-    @abc.abstractmethod
     def set_position_range(self, myrange=None):
         """ Sets the physical range of the scanner.
         This is a direct pass-through to the stepper hardware.
 
-        @param float [4][2] myrange: array of 4 ranges with an array containing
-                                     lower and upper limit
+        @param float [n][2] myrange: array of n ranges with an array containing
+                                     lower and upper limit,  n defined in config
 
         @return int: error code (0:OK, -1:error)
         """
-        return self._scanning_device.get_position_range(myrange)
+        return self._scanning_device1.get_position_range(myrange)
 
-    @abc.abstractmethod
     def set_voltage_range(self, myrange=None):
         """ Sets the voltage range of the NI Card.
 
@@ -96,7 +105,6 @@ class StepperScannerInterfuse(Base, ConfocalScannerInterface, PiezoStepperInterf
         """
         pass
 
-    @abc.abstractmethod
     def set_up_scanner_clock(self, clock_frequency=None, clock_channel=None):
         """ Configures the hardware clock of the NiDAQ card to give the timing.
 
@@ -109,7 +117,6 @@ class StepperScannerInterfuse(Base, ConfocalScannerInterface, PiezoStepperInterf
         """
         pass
 
-    @abc.abstractmethod
     def set_up_scanner(self, counter_channel=None, photon_source=None,
                        clock_channel=None, scanner_ao_channels=None):
         """ Configures the actual scanner with a given clock.
@@ -127,7 +134,6 @@ class StepperScannerInterfuse(Base, ConfocalScannerInterface, PiezoStepperInterf
         """
         pass
 
-    @abc.abstractmethod
     def scanner_set_position(self, x=None, y=None, z=None, a=None):
         """Move stage to x, y, z, a (where a is the fourth voltage channel).
 
@@ -170,7 +176,6 @@ class StepperScannerInterfuse(Base, ConfocalScannerInterface, PiezoStepperInterf
         # until a certain accuracy is achieved
         pass
 
-    @abc.abstractmethod
     def get_scanner_position(self):
         """ Get the current position of the scanner hardware.
 
@@ -180,7 +185,6 @@ class StepperScannerInterfuse(Base, ConfocalScannerInterface, PiezoStepperInterf
         # position values
         pass
 
-    @abc.abstractmethod
     def set_up_line(self, length=100):
         """ Sets up the analoque output for scanning a line.
 
@@ -190,7 +194,6 @@ class StepperScannerInterfuse(Base, ConfocalScannerInterface, PiezoStepperInterf
         """
         pass
 
-    @abc.abstractmethod
     def scan_line(self, line_path=None):
         """ Scans a line and returns the counts on that line.
 
@@ -201,7 +204,6 @@ class StepperScannerInterfuse(Base, ConfocalScannerInterface, PiezoStepperInterf
         """
         pass
 
-    @abc.abstractmethod
     def close_scanner(self):
         """ Closes the scanner and cleans up afterwards.
 
@@ -209,7 +211,6 @@ class StepperScannerInterfuse(Base, ConfocalScannerInterface, PiezoStepperInterf
         """
         pass
 
-    @abc.abstractmethod
     def close_scanner_clock(self, power=0):
         """ Closes the clock and cleans up afterwards.
 
