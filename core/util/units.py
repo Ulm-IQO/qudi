Introduce the window functions and their normalization factor for the correct fourier amplitude calculation. Make it possible to use it in the fourier transform.

core/util/units.py
@@ -416,9 +416,37 @@ def in_range(value, lower_limit, upper_limit):
 
 
 def get_ft_windows():
-    pass
+    """ Retrieve the available windows to be applied on signal data before FT.
 
-def compute_ft(x_val, y_val, zeropad_num=0, window='none', base_corr=True, psd=False):
+    @return: dict with keys being the window name and items being again a dict
+             containing the actual function and the normalization factor to
+             calculate correctly the amplitude spectrum in the Fourier Transform
+
+    To find out the amplitude normalization factor check either the scipy
+    implementation on
+        https://github.com/scipy/scipy/blob/v0.15.1/scipy/signal/windows.py#L336
+    or just perform a sum of the window (oscillating parts of the window should
+    be averaged out and constant offset factor will remain):
+        MM=1000000  # choose a big number
+        print(sum(signal.hanning(MM))/MM)
+    """
+
+    win = {'none': {'func': np.ones, 'ampl_norm': 1.0},
+           'hamming': {'func': signal.hamming, 'ampl_norm': 1.0/0.54},
+           'hann': {'func': signal.hann, 'ampl_norm': 1.0/0.5},
+           'blackman': {'func': signal.blackman, 'ampl_norm': 1.0/0.42},
+           'triang': {'func': signal.triang, 'ampl_norm': 1.0/0.5},
+           'flattop': {'func': signal.flattop, 'ampl_norm': 1.0/0.2156},
+           'bartlett': {'func': signal.bartlett, 'ampl_norm': 1.0/0.5},
+           'parzen': {'func': signal.parzen, 'ampl_norm': 1.0/0.375},
+           'bohman': {'func': signal.bohman, 'ampl_norm': 1.0/0.4052847},
+           'blackmanharris': {'func': signal.blackmanharris, 'ampl_norm': 1.0/0.35875},
+           'nuttall': {'func': signal.nuttall, 'ampl_norm': 1.0/0.3635819},
+           'barthann': {'func': signal.barthann, 'ampl_norm': 1.0/0.5}}
+    return win
+
+def compute_ft(x_val, y_val, zeropad_num=0, window='none', base_corr=True,
+               psd=False):
     """ Compute the Discrete fourier Transform of the power spectral density
 
     @param numpy.array x_val: 1D array
@@ -436,6 +464,10 @@ def compute_ft(x_val, y_val, zeropad_num=0, window='none', base_corr=True, psd=F
                             Set zeropad_num=1 to obtain output arrays which
                             have the same size as the input arrays.
                             Default is zeropad_num=0.
+    @param str window: optional, the window function which should be applied to
+                       the y values before Fourier Transform is calculated.
+    @param bool base_corr: Select whether baseline correction shoud be performed
+                           before calculating the FT.
     @param bool psd: optional, select whether the Discrete Fourier Transform or
                      the Power Spectral Density (PSD, which is just the FT of
                      the absolute square of the y-values) should be computed.
@@ -447,14 +479,13 @@ def compute_ft(x_val, y_val, zeropad_num=0, window='none', base_corr=True, psd=F
                     len(dft_x) = len(dft_y) = (len(y_val)/2)*(zeropad_num+1)
 
     Pay attention that the return values of the FT have only half of the
-    entries compared to the used signal input.
+    entries compared to the used signal input (if zeropad=0).
 
-    In general, a window function should be applied on the time domain data
-    before calculating the FT, to reduce spectral leakage. The Hann window for
-    instance is almost never a bad choice. Use it like:
-        y_ft = np.fft.fft(y_signal * np.hanning(len(y_signal)))
+    In general, a window function should be applied on the y data before
+    calculating the FT, to reduce spectral leakage. The Hann window for
+    instance is almost never a bad choice. Use it like window='hann'
 
-    Keep always in mind that the relation for the Fourier transform:
+    Keep always in mind the relation to the Fourier transform space:
         T = delta_t * N_samples
     where delta_t is the distance between the time points and N_samples are the
     amount of points in the time domain. Consequently the sample rate is
@@ -469,11 +500,7 @@ def compute_ft(x_val, y_val, zeropad_num=0, window='none', base_corr=True, psd=F
     your signal, i.e. the amplitude and phase of harmonics in your signal.
     """
 
-    avail_windows = {'none': np.ones, 'hamming': signal.hamming,
-                     'hann': signal.hann, 'blackman': signal.blackman,
-                     'triang': signal.triang, 'flattop': signal.flattop}
-    ampl_window_norm = {'none': 1.0, 'hamming': 1.0/0.54, 'hann': 1.0/0.5,
-                        'blackman': 1.0/0.42, 'triang': 1.0/0.5, 'flattop': 1.0/0.2156}
+    avail_windows = get_ft_windows()
 
     x_val = np.array(x_val)
     y_val = np.array(y_val)
@@ -488,10 +515,10 @@ def compute_ft(x_val, y_val, zeropad_num=0, window='none', base_corr=True, psd=F
     ampl_norm_fact = 1.0
     # apply window to data to account for spectral leakage:
     if window in avail_windows:
-        window_val = avail_windows[window](len(y_val))
+        window_val = avail_windows[window]['func'](len(y_val))
         corrected_y = corrected_y * window_val
         # to get the correct amplitude in the amplitude spectrum
-        ampl_norm_fact = ampl_window_norm[window]
+        ampl_norm_fact = avail_windows[window]['ampl_norm']
 
     # zeropad for sinc interpolation:
     zeropad_arr = np.zeros(len(corrected_y)*(zeropad_num+1))