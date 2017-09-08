00ff858bdd1e3105ca46ee0d0a07bc0161e2b734
Alexander Stark <alexander.stark@uni-ulm.de>
2017-09-08 19:49:53 +0200

    Make the control parameter for the fourier plot accessible in the pulsed measurement logic. Insert further checks, to catch the fact if a fit a drawn but no fit results are actually saved the result object.

--- gui/pulsed/pulsed_maingui.py
+++ gui/pulsed/pulsed_maingui.py
@@ -1311,35 +1311,52 @@ class PulsedMeasurementGui(GUIBase):
         self._pa.pulse_analysis_PlotWidget.addItem(self.fit_image)
 
         # Configure the errorbars of the data in the main pulse analysis display:
-        self.signal_image_error_bars = pg.ErrorBarItem(x=np.array(range(10)), y=np.zeros(10),
-                                                       top=0., bottom=0., pen=palette.c2)
-        self.signal_image_error_bars2 = pg.ErrorBarItem(x=np.array(range(10)), y=np.zeros(10),
-                                                        top=0., bottom=0., pen=palette.c5)
+        self.signal_image_error_bars = pg.ErrorBarItem(x=np.array(range(10)),
+                                                       y=np.zeros(10),
+                                                       top=0., bottom=0.,
+                                                       pen=palette.c2)
+        self.signal_image_error_bars2 = pg.ErrorBarItem(x=np.array(range(10)),
+                                                        y=np.zeros(10),
+                                                        top=0., bottom=0.,
+                                                        pen=palette.c5)
 
         # Configure the second pulse analysis display:
-        self.second_plot_image = pg.PlotDataItem(np.array(range(10)), np.zeros(10), pen=palette.c1)
+        self.second_plot_image = pg.PlotDataItem(np.array(range(10)),
+                                                 np.zeros(10), pen=palette.c1)
         self._pa.pulse_analysis_second_PlotWidget.addItem(self.second_plot_image)
         self.second_plot_image2 = pg.PlotDataItem(pen=palette.c4)
         self._pa.pulse_analysis_second_PlotWidget.addItem(self.second_plot_image2)
-        self._pa.pulse_analysis_second_PlotWidget.showGrid(x=True, y=True, alpha=0.8)
+        self._pa.pulse_analysis_second_PlotWidget.showGrid(x=True, y=True,
+                                                           alpha=0.8)
 
         # Configure the lasertrace plot display:
-        self.sig_start_line = pg.InfiniteLine(pos=0, pen=QtGui.QPen(palette.c3, 2), movable=True)
+        self.sig_start_line = pg.InfiniteLine(pos=0,
+                                              pen=QtGui.QPen(palette.c3, 2),
+                                              movable=True)
         self.sig_start_line.setHoverPen(QtGui.QPen(palette.c3), width=10)
-        self.sig_end_line = pg.InfiniteLine(pos=0, pen=QtGui.QPen(palette.c3, 2), movable=True)
+        self.sig_end_line = pg.InfiniteLine(pos=0,
+                                            pen=QtGui.QPen(palette.c3, 2),
+                                            movable=True)
         self.sig_end_line.setHoverPen(QtGui.QPen(palette.c3), width=10)
-        self.ref_start_line = pg.InfiniteLine(pos=0, pen=QtGui.QPen(palettedark.c4, 2), movable=True)
+        self.ref_start_line = pg.InfiniteLine(pos=0,
+                                              pen=QtGui.QPen(palettedark.c4, 2),
+                                              movable=True)
         self.ref_start_line.setHoverPen(QtGui.QPen(palette.c4), width=10)
-        self.ref_end_line = pg.InfiniteLine(pos=0, pen=QtGui.QPen(palettedark.c4, 2), movable=True)
+        self.ref_end_line = pg.InfiniteLine(pos=0,
+                                            pen=QtGui.QPen(palettedark.c4, 2),
+                                            movable=True)
         self.ref_end_line.setHoverPen(QtGui.QPen(palette.c4), width=10)
         # Configure the measuring error display:
-        self.measuring_error_image = pg.PlotDataItem(np.array(range(10)), np.zeros(10),
+        self.measuring_error_image = pg.PlotDataItem(np.array(range(10)),
+                                                     np.zeros(10),
                                                      pen=palette.c1)
-        self.measuring_error_image2 = pg.PlotDataItem(np.array(range(10)), np.zeros(10),
+        self.measuring_error_image2 = pg.PlotDataItem(np.array(range(10)),
+                                                      np.zeros(10),
                                                       pen=palette.c3)
         self._pe.measuring_error_PlotWidget.addItem(self.measuring_error_image)
         self._pe.measuring_error_PlotWidget.addItem(self.measuring_error_image2)
-        self._pe.measuring_error_PlotWidget.setLabel('left', 'measuring error', units='a.u.')
+        self._pe.measuring_error_PlotWidget.setLabel('left', 'measuring error',
+                                                     units='a.u.')
         #self._pe.measuring_error_PlotWidget.setLabel('bottom', 'tau', units='s')
 
 
