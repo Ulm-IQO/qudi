c5b79a9bd03e38588e212b6be538ad5835d9635a
Tobias Gehring <tobias.gehring@fysik.dtu.dk>
2017-12-24 13:04:59 +0100

    fixed bug in get_home_dir and get_main_dir

--- core/util/modules.py
+++ core/util/modules.py
@@ -25,7 +25,7 @@ Originally distributed under MIT/X11 license. See documentation/MITLicense.txt f
 import os
 
 
-def get_main_dir(self):
+def get_main_dir():
     """ Returns the absolut path to the directory of the main software.
 
          @return string: path to the main tree of the software
@@ -35,7 +35,7 @@ def get_main_dir(self):
         os.path.join(os.path.dirname(__file__), '../..'))
 
 
-def get_home_dir(self):
+def get_home_dir():
     """ Returns the path to the home directory, which should definitely
         exist.
         @return string: absolute path to the home directory
