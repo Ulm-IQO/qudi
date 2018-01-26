a9d8463b74cd92c80ba61f2588becd14e484ecfe
Neverhorst <tomek.qudi@gmail.com>
2018-01-26 17:32:43 +0100

    WIP on an alternative implementation of a scientific DoubleSpinBox using mainly a custom validator.

--- qtwidgets/scientific_spinbox.py
+++ qtwidgets/scientific_spinbox.py
@@ -19,45 +19,203 @@ along with Qudi. If not, see <http://www.gnu.org/licenses/>.
 Copyright (C) 2016 Alexander Stark alexander.stark@uni-ulm.de
 """
 
-from qtwidgets.pyqtgraphmod.SpinBox import SpinBox
+from qtpy import QtGui, QtWidgets
+import numpy as np
+import re
+from pyqtgraph import functions as fn
+__all__=['ScienDSpinBox']
+
+class FloatValidator(QtGui.QValidator):
+    """
+    This is a validator for float values represented as strings in scientific notation.
+    (i.e. "1.35e-9", ".24E+8", "14e3" etc.)
+    Also supports SI unit prefix like 'M', 'n' etc.
+    """
+    def __init__(self):
+        self.float_re = None
+        self.set_prefix_suffix()  # Set regular expression
+        return
+
+    def validate(self, string, position):
+        print('text to validate:', string, '\nlength:', len(string), '\nposition:', position)
+
+        match = self.float_re.search(string)
+
+        if match:
+            if match.groups()[self.group_map['match']] != string:
+                return self.Invalid, string, position
+
+            if position > len(string):
+                position = len(string)
+            elif position < 1:
+                position = 1
+            if match.groups()[self.group_map['mantissa']] is None or string[position-1] in 'eE.-+ ':
+                return self.Intermediate, string, position
+
+            return self.Intermediate, string, position
+        else:
+            return self.Acceptable, string, position
+
+        if self._is_valid_float_string(string):
+            state = self.Acceptable
+        elif string == '' or string[position-1] in 'e.-+ ':
+            state = self.Intermediate
+        else:
+            state = self.Invalid
+        return state, string, position
+
+    def fixup(self, text):
+        match = self.float_re.search(text)
+        if match:
+            return match.groups()[0].lstrip().rstrip()
+        else:
+            return ''
+
+    def set_prefix_suffix(self, prefix=None, suffix=None):
+        self.group_map = dict()
+        self.group_map['match'] = 0
+        if suffix is None and prefix is not None:
+            self.float_re = re.compile(
+                r'(({0})\s?([+-]?(\d+)\.?(\d*))?([eE][+-]?\d+)?\s?([YZEPTGMkmµunpfazy]?))'
+                r''.format(prefix))
+            self.group_map['prefix'] = 1
+            self.group_map['mantissa'] = 2
+            self.group_map['integer'] = 3
+            self.group_map['fractional'] = 4
+            self.group_map['exponent'] = 5
+            self.group_map['si'] = 6
+        elif suffix is not None and prefix is None:
+            self.float_re = re.compile(
+                r'(([+-]?(\d+)\.?(\d*))?([eE][+-]?\d+)?\s?([YZEPTGMkmµunpfazy]?)({0}))'
+                r''.format(suffix))
+            self.group_map['mantissa'] = 1
+            self.group_map['integer'] = 2
+            self.group_map['fractional'] = 3
+            self.group_map['exponent'] = 4
+            self.group_map['si'] = 5
+            self.group_map['suffix'] = 6
+        elif suffix is not None and prefix is not None:
+            self.float_re = re.compile(
+                r'(({0})\s?([+-]?(\d+)\.?(\d*))?([eE][+-]?\d+)?\s?([YZEPTGMkmµunpfazy]?)({1}))'
+                r''.format(prefix, suffix))
+            self.group_map['prefix'] = 1
+            self.group_map['mantissa'] = 2
+            self.group_map['integer'] = 3
+            self.group_map['fractional'] = 4
+            self.group_map['exponent'] = 5
+            self.group_map['si'] = 6
+            self.group_map['suffix'] = 7
+        else:
+            self.float_re = re.compile(
+                r'(([+-]?(\d+)\.?(\d*))?([eE][+-]?\d+)?\s?([YZEPTGMkmµunpfazy]?))')
+            self.group_map['mantissa'] = 1
+            self.group_map['integer'] = 2
+            self.group_map['fractional'] = 3
+            self.group_map['exponent'] = 4
+            self.group_map['si'] = 5
+        return
+
+    def _is_valid_float_string(self, string):
+        match = self.float_re.search(string)
+        if match:
+            return match.groups()[0] == string
+        else:
+            return False
 
 
-class ScienDSpinBox(SpinBox):
-    """ Wrapper Class from PyQtGraph to display a QDoubleSpinBox in Scientific
-        way.
+class ScienDSpinBox(QtWidgets.QDoubleSpinBox):
+    """
+    Wrapper Class from PyQt5 (or QtPy) to display a QDoubleSpinBox in Scientific way.
 
-    This class can be directly used in Qt Designer by promoting the
-    QDoubleSpinBox to ScienDSpinBox. State the path to this file (in python
-    style, i.e. dots are separating the directories) as the header file and use
-    the name of the present class.
+    This class can be directly used in Qt Designer by promoting the QDoubleSpinBox to ScienDSpinBox.
+    State the path to this file (in python style, i.e. dots are separating the directories) as the
+    header file and use the name of the present class.
     """
 
     def __init__(self, *args, **kwargs):
-        SpinBox.__init__(
-                self,
-                *args,
-                int=False,
-                #suffix='s',
-                siPrefix=True,
-                dec=True,
-                step=0.1,
-                minStep=0.0001,
-                bounds=(0.0, 99.99),    # set the bounds to be convenient to the default values of the Qt Designer.
-                **kwargs
-        )
+        super().__init__(*args, **kwargs)
+        self.setMinimum(-np.inf)
+        self.setMaximum(np.inf)
+        self.validator = FloatValidator()
+        self.setDecimals(1000)
 
-class ScienSpinBox(SpinBox):
-    """ Wrapper Class from PyQtGraph to display a QSpinBox in Scientific way.
+    def validate(self, text, position):
+        # text = text.replace(self.suffix(), '').replace(self.prefix(), '')
+        state, string, position = self.validator.validate(text, position)
+        return state, string, position  #self.prefix() + string + self.suffix(), position
 
-    This class can be directly used in Qt Designer by promoting the
-    QSpinBox to ScienSpinBox. State the path to this file (in python style,
-    i.e. dots are separating the directories) as the header file and use the
-    name of the present class.
-    """
+    def fixup(self, text):
+        return self.validator.fixup(text)
 
-    def __init__(self, *args, **kwargs):
-        SpinBox.__init__(self,
-                         *args,
-                         int=True,
-                         bounds=(0, 99), # set the bounds to be convenient to the default values of the Qt Designer.
-                         **kwargs)
+    def valueFromText(self, text):
+        text = text.lstrip().rstrip()  # get rid of leading and trailing whitespaces
+        text = text.replace(self.suffix(), '').replace(self.prefix(), '') # get rid of prefix/suffix
+        return fn.siEval(text)
+
+    def textFromValue(self, value):
+        (scale_factor, suffix) = fn.siScale(value)
+        scaled_val = value * scale_factor
+        string = ''
+        if abs(scaled_val) < 10:
+            string = fn.siFormat(value, precision=4)
+        elif abs(scaled_val) < 100:
+            string = fn.siFormat(value, precision=5)
+        elif abs(scaled_val) < 1000:
+            string = fn.siFormat(value, precision=6)
+        return string
+
+    def stepBy(self, steps):
+        text = self.cleanText()
+        groups = self.validator.float_re.search(text).groups()
+        decimal_num = float(groups[1])
+        decimal_num += steps
+        new_string = '{0:g}'.format(decimal_num) + ((' ' + groups[4]) if groups[4] else '')
+        new_value = fn.siEval(new_string)
+        if new_value < self.minimum():
+            new_value = self.minimum()
+        if new_value > self.maximum():
+            new_value = self.maximum()
+        new_string = self.textFromValue(new_value)
+        self.lineEdit().setText(new_string)
+        return
+
+
+# class ScienDSpinBox(SpinBox):
+#     """ Wrapper Class from PyQtGraph to display a QDoubleSpinBox in Scientific
+#         way.
+#
+#     This class can be directly used in Qt Designer by promoting the
+#     QDoubleSpinBox to ScienDSpinBox. State the path to this file (in python
+#     style, i.e. dots are separating the directories) as the header file and use
+#     the name of the present class.
+#     """
+#
+#     def __init__(self, *args, **kwargs):
+#         SpinBox.__init__(
+#                 self,
+#                 *args,
+#                 int=False,
+#                 #suffix='s',
+#                 siPrefix=True,
+#                 dec=True,
+#                 step=0.1,
+#                 minStep=0.0001,
+#                 bounds=(0.0, 99.99),    # set the bounds to be convenient to the default values of the Qt Designer.
+#                 **kwargs
+#         )
+#
+# class ScienSpinBox(SpinBox):
+#     """ Wrapper Class from PyQtGraph to display a QSpinBox in Scientific way.
+#
+#     This class can be directly used in Qt Designer by promoting the
+#     QSpinBox to ScienSpinBox. State the path to this file (in python style,
+#     i.e. dots are separating the directories) as the header file and use the
+#     name of the present class.
+#     """
+#
+#     def __init__(self, *args, **kwargs):
+#         SpinBox.__init__(self,
+#                          *args,
+#                          int=True,
+#                          bounds=(0, 99), # set the bounds to be convenient to the default values of the Qt Designer.
+#                          **kwargs)
