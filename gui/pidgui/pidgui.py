6b05eff38c8958786bfcd3bc407fe3adcad54c3c
Alrik <alrik.durand@gmail.com>
2018-05-04 19:38:05 +0200

    Updated pid logic so that it can adapt to present features or not. The way it is done is a bit too heavy right now I tried to be as non breaking with the GUI as possible but this module need redesign anyway. Also update the GUI just a bit (for it not to crash)

--- gui/pidgui/pidgui.py
+++ gui/pidgui/pidgui.py
@@ -143,7 +143,7 @@ class PIDGui(GUIBase):
         self.plot1.vb.sigResized.connect(self.updateViews)
 
         # setting the x axis length correctly
-        self._pw.setXRange(0, self._pid_logic.getBufferLength() * self._pid_logic.timestep)
+        self._pw.setXRange(0, self._pid_logic.getBufferLength() * self._pid_logic.get_timestep())
 
         #####################
         # Setting default parameters
@@ -174,9 +174,9 @@ class PIDGui(GUIBase):
         self._mw.restore_default_view_Action.triggered.connect(self.restore_default_view)
 
         #####################
-        # starting the physical measurement
-        self.sigStart.connect(self._pid_logic.startLoop)
-        self.sigStop.connect(self._pid_logic.stopLoop)
+        # TODO: Why should we able to stop or start this ??
+        # self.sigStart.connect(self._pid_logic.startLoop)
+        # self.sigStop.connect(self._pid_logic.stopLoop)
 
         self._pid_logic.sigUpdateDisplay.connect(self.updateData)
 
@@ -219,18 +219,18 @@ class PIDGui(GUIBase):
                 self._mw.labelkD.setText('{0:,.6f}'.format(extra['D']))
             self._curve1.setData(
                 y=self._pid_logic.history[0],
-                x=np.arange(0, self._pid_logic.getBufferLength()) * self._pid_logic.timestep
+                x=np.arange(0, self._pid_logic.getBufferLength()) * self._pid_logic.get_timestep()
                 )
             self._curve2.setData(
                 y=self._pid_logic.history[1],
-                x=np.arange(0, self._pid_logic.getBufferLength()) * self._pid_logic.timestep
+                x=np.arange(0, self._pid_logic.getBufferLength()) * self._pid_logic.get_timestep()
                 )
             self._curve3.setData(
                 y=self._pid_logic.history[2],
-                x=np.arange(0, self._pid_logic.getBufferLength()) * self._pid_logic.timestep
+                x=np.arange(0, self._pid_logic.getBufferLength()) * self._pid_logic.get_timestep()
                 )
 
-        if self._pid_logic.getSavingState():
+        if self._pid_logic.get_saving_state():
             self._mw.record_control_Action.setText('Save')
         else:
             self._mw.record_control_Action.setText('Start Saving Data')
@@ -262,7 +262,7 @@ class PIDGui(GUIBase):
     def save_clicked(self):
         """ Handling the save button to save the data into a file.
         """
-        if self._pid_logic.getSavingState():
+        if self._pid_logic.get_saving_state():
             self._mw.record_counts_Action.setText('Start Saving Data')
             self._pid_logic.saveData()
         else:
--- logic/pid_logic.py
+++ logic/pid_logic.py
@@ -38,22 +38,25 @@ class PIDLogic(GenericLogic):
     ## declare connectors
     controller = Connector(interface='PIDControllerInterface')
     savelogic = Connector(interface='SaveLogic')
-    _type = ConfigOption('type', 'PID')  # 'PID', 'PROCESS_CONTROL' or 'PROCESS'
-
+    _features = ConfigOption('features', ['PID_CONTROLLER'])
+    # can include 'PID_CONTROLLER', 'SETPOINT_CONTROLLER', 'SETPOINT', 'PROCESS_VARIABLE' or 'PROCESS_CONTROL'
+    # depending on what is available
 
     # status vars
     bufferLength = StatusVar('bufferlength', 1000)
-    timestep = StatusVar(default=100)
+    # TODO: Maybe the logic should keep everything (this should not be correlated with the GUI)
+    
+    _timestep = StatusVar(default=100)
+    _loop_enabled = False
 
     # signals
     sigUpdateDisplay = QtCore.Signal()
 
     def __init__(self, config, **kwargs):
         super().__init__(config=config, **kwargs)
-        self.log.debug('The following configuration was found.')
 
-        #number of lines in the matrix plot
-        self.NumberOfSecondsLog = 100
+        # number of lines in the matrix plot
+        self.NumberOfSecondsLog = 100  # This breaks logic/GUI compartmentalisation
         self.threadlock = Mutex()
 
     def on_activate(self):
@@ -64,65 +67,24 @@ class PIDLogic(GenericLogic):
 
         self.history = np.zeros([3, self.bufferLength])
         self.savingState = False
-        self.enabled = False
+
         self.timer = QtCore.QTimer()
         self.timer.setSingleShot(True)
-        self.timer.setInterval(self.timestep)
+        self.timer.setInterval(self._timestep)
         self.timer.timeout.connect(self.loop)
+
+        self.start_loop()
 
     def on_deactivate(self):
         """ Perform required deactivation. """
         pass
 
-    def getBufferLength(self):
+    def getBufferLength(self):  #TODO: This breaks logic/GUI compartmentalisation (and naming conventions)
         """ Get the current data buffer length.
         """
         return self.bufferLength
 
-    def startLoop(self):
-        """ Start the data recording loop.
-        """
-        self.enabled = True
-        self.timer.start(self.timestep)
-
-    def stopLoop(self):
-        """ Stop the data recording loop.
-        """
-        self.enabled = False
-
-    def loop(self):
-        """ Execute step in the data recording loop: save one of each control and process values
-        """
-        self.history = np.roll(self.history, -1, axis=1)
-        self.history[0, -1] = self._controller.get_process_value()
-        self.history[1, -1] = self._controller.get_control_value()
-        self.history[2, -1] = self._controller.get_setpoint()
-        self.sigUpdateDisplay.emit()
-        if self.enabled:
-            self.timer.start(self.timestep)
-
-    def getSavingState(self):
-        """ Return whether we are saving data
-
-            @return bool: whether we are saving data right now
-        """
-        return self.savingState
-
-    def startSaving(self):
-        """ Start saving data.
-
-            Function does nothing right now.
-        """
-        pass
-
-    def saveData(self):
-        """ Stop saving data and write data to file.
-
-            Function does nothing right now.
-        """
-        pass
-
-    def setBufferLength(self, newBufferLength):
+    def setBufferLength(self, newBufferLength): #TODO: This breaks logic/GUI compartmentalisation (and naming conventions)
         """ Change buffer length to new value.
 
             @param int newBufferLength: new buffer length
@@ -130,12 +92,78 @@ class PIDLogic(GenericLogic):
         self.bufferLength = newBufferLength
         self.history = np.zeros([3, self.bufferLength])
 
+    def start_loop(self):
+        """ Start the data acquisition loop.
+        """
+        self._loop_enabled = True
+        self.timer.start(self._timestep)
+
+    def stop_loop(self):
+        """ Stop the data acquisition loop.
+        """
+        self._loop_enabled = False
+
+    def loop(self):
+        """ Execute step in the data acquisition loop: save one of each control and process values
+        """
+        self.history = np.roll(self.history, -1, axis=1)  # TODO : What is the efficiency of "roll storing" method on large array ?
+
+        #TODO: only store activated info, hybrid for now
+        if set(['PID_CONTROLLER', 'SETPOINT_CONTROLLER', 'PROCESS_VARIABLE']).intersection(set(self._features)):
+            self.history[0, -1] = self._controller.get_process_value()
+        else:
+            self.history[0, -1] = 0
+
+        if set(['PID_CONTROLLER', 'PROCESS_VARIABLE']).intersection(set(self._features)):
+            self.history[1, -1] = self._controller.get_control_value()
+        else:
+            self.history[1, -1] = 0
+
+        if set(['PID_CONTROLLER', 'SETPOINT_CONTROLLER', 'SETPOINT']).intersection(set(self._features)):
+            self.history[2, -1] = self._controller.get_setpoint()
+        else:
+            self.history[2, -1] = 0
+
+        self.sigUpdateDisplay.emit()
+        if self._loop_enabled:
+            self.timer.start(self._timestep)
+
+    # TODO: to make the GUI happy for now, this could vary so this need to be redesigned
+    def get_timestep(self):
+        return self._timestep
+
+    def get_features(self):
+        return self._features
+
+    def get_saving_state(self):
+        """ Return whether we are saving data
+
+            @return bool: whether we are saving data right now
+        """
+        return self.savingState
+
+    def start_saving(self):
+        """ Start saving data.
+
+            Function does nothing right now.
+        """
+        pass
+
+    def save_sata(self):
+        """ Stop saving data and write data to file.
+
+            Function does nothing right now.
+        """
+        pass
+
+    # Beginning of features dependent methods :
+
     def get_kp(self):
         """ Return the proportional constant.
 
             @return float: proportional constant of PID controller
         """
-        if self._type == 'PID':
+        if 'PID_CONTROLLER' in self._features:
             return self._controller.get_kp()
         else:
             return 0
@@ -145,7 +173,7 @@ class PIDLogic(GenericLogic):
 
             @prarm float kp: proportional constant of PID controller
         """
-        if self._type == 'PID':
+        if 'PID_CONTROLLER' in self._features:
             return self._controller.set_kp(kp)
         else:
             return 0
@@ -155,7 +183,7 @@ class PIDLogic(GenericLogic):
 
             @return float: integration constant of the PID controller
         """
-        if self._type == 'PID':
+        if 'PID_CONTROLLER' in self._features:
             return self._controller.get_ki()
         else:
             return 0
@@ -165,7 +193,7 @@ class PIDLogic(GenericLogic):
 
             @param float ki: integration constant of the PID controller
         """
-        if self._type == 'PID':
+        if 'PID_CONTROLLER' in self._features:
             return self._controller.set_ki(ki)
         else:
             return 0
@@ -175,7 +203,7 @@ class PIDLogic(GenericLogic):
 
             @return float: the derivative constant of the PID controller
         """
-        if self._type == 'PID':
+        if 'PID_CONTROLLER' in self._features:
             return self._controller.get_kd()
         else:
             return 0
@@ -185,17 +213,17 @@ class PIDLogic(GenericLogic):
 
             @param float kd: the derivative constant of the PID controller
         """
-        if self._type == 'PID':
+        if 'PID_CONTROLLER' in self._features:
             return self._controller.set_kd(kd)
         else:
             return 0
 
     def get_setpoint(self):
-        """ Get the current setpoint of the PID controller.
+        """ Get the current setpoint of the controller.
 
-            @return float: current set point of the PID controller
+            @return float: current set point of the controller
         """
-        if self._type == 'PID' or self._type == 'PROCESS_CONTROL':
+        if set(['PID_CONTROLLER', 'SETPOINT_CONTROLLER', 'SETPOINT']).intersection(set(self._features)):
             return self.history[2, -1]
         else:
             return 0
@@ -203,68 +231,51 @@ class PIDLogic(GenericLogic):
     def set_setpoint(self, setpoint):
         """ Set the current setpoint of the PID controller.
 
-            @param float setpoint: new set point of the PID controller
+            @param float setpoint: new set point of the controller
         """
-        if self._type == 'PID' or self._type == 'PROCESS_CONTROL':
-            self._controller.set_setpoint(setpoint)
-
-    def get_manual_value(self):
-        """ Return the control value for manual mode.
-
-            @return float: control value for manual mode
-        """
-
-        if self._type == 'PID':
-            return self._controller.get_manual_value()
+        if set(['PID_CONTROLLER', 'SETPOINT_CONTROLLER', 'SETPOINT']).intersection(set(self._features)):
+            return self._controller.set_setpoint(setpoint)
         else:
             return 0
-
-    def set_manual_value(self, manualvalue):
-        """ Set the control value for manual mode.
-
-            @param float manualvalue: control value for manual mode of controller
-        """
-        if self._type == 'PID':
-            return self._controller.set_manual_value(manualvalue)
-        else:
-            return 0
-
 
     def get_enabled(self):
         """ See if the PID controller is controlling a process.
 
             @return bool: whether the PID controller is preparing to or conreolling a process
         """
-        return self.enabled
+        if set(['PID_CONTROLLER', 'SETPOINT_CONTROLLER']).intersection(set(self._features)):
+            return self._controller.get_enabled()
+        else:
+            return 0
 
     def set_enabled(self, enabled):
         """ Set the state of the PID controller.
 
             @param bool enabled: desired state of PID controller
         """
-        if enabled and not self.enabled:
-            self.startLoop()
-        if not enabled and self.enabled:
-            self.stopLoop()
+        if set(['PID_CONTROLLER', 'SETPOINT_CONTROLLER']).intersection(set(self._features)):
+            return self._controller.set_enabled(enabled)
+        else:
+            return 0
 
     def get_control_limits(self):
         """ Get the minimum and maximum value of the control actuator.
 
             @return list(float): (minimum, maximum) values of the control actuator
         """
-        if self._type == 'PID' or self._type == 'PROCESS_CONTROL':
+        if set(['PID_CONTROLLER', 'PROCESS_CONTROL']).intersection(set(self._features)):
             return self._controller.get_control_limits()
         else:
             return 0
 
-    def set_control_limits(self, limits):
+    def set_control_limits(self, limits):  # TODO: Should this be ok ?
         """ Set the minimum and maximum value of the control actuator.
 
             @param list(float) limits: (minimum, maximum) values of the control actuator
 
             This function does nothing, control limits are handled by the control module
         """
-        if self._type == 'PID':
+        if 'PID_CONTROLLER' in self._features:
             return self._controller.set_control_limits(limits)
         else:
             return 0
@@ -274,25 +285,46 @@ class PIDLogic(GenericLogic):
 
             @return float: current process input value
         """
-        if self._type == 'PID' or self._type == 'PROCESS_CONTROL':
+        if set(['PID_CONTROLLER', 'SETPOINT_CONTROLLER', 'PROCESS_VARIABLE']).intersection(set(self._features)):
             return self.history[0, -1]
         else:
             return 0
-
 
     def get_cv(self):
         """ Get current control output value.
 
             @return float: control output value
         """
-        if self._type == 'PID' or self._type == 'PROCESS_CONTROL':
+        if set(['PID_CONTROLLER', 'PROCESS_CONTROL']).intersection(set(self._features)):
             return self.history[1, -1]
         else:
             return 0
 
+    # TODO : What is that exactly ?
     def get_extra(self):
-        if self._type == 'PID':
+        if set(['PID_CONTROLLER']).intersection(set(self._features)):
             return self._controller.get_extra()
         else:
             return []
 
+    # TODO: How does manual fit into all this ?
+    def get_manual_value(self):
+        """ Return the control value for manual mode.
+
+            @return float: control value for manual mode
+        """
+
+        if 'PID_CONTROLLER' in self._features:
+            return self._controller.get_manual_value()
+        else:
+            return 0
+
+    def set_manual_value(self, manualvalue):
+        """ Set the control value for manual mode.
+
+            @param float manualvalue: control value for manual mode of controller
+        """
+        if self._type == 'PID_CONTROLLER':
+            return self._controller.set_manual_value(manualvalue)
+        else:
+            return 0
