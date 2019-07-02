a8a4373b772e19524e75e302e67e4c4f80d3938d
Alrik <alrik.durand@gmail.com>
2019-07-02 18:09:37 +0200

    updated joystick to scanner

--- logic/logic_connectors/joystick_to_confocal.py
+++ logic/logic_connectors/joystick_to_confocal.py
@@ -58,25 +58,6 @@ class JoystickToConfocal(GenericLogic):
         """
         self.joystick_logic().register(str(self), self.callback, trigger_keys={'right_up': True})
 
-        # # Left axis : move continuously
-        # event_left_axis = [[self._joystick_logic.signal_left_vertical, 0],
-        #               [self._joystick_logic.signal_left_horizontal, 1],
-        #               [self._joystick_logic.signal_left_trigger, 2],
-        #               [self._joystick_logic.signal_right_trigger, 2]
-        #               ]
-        # for event in event_left_axis:
-        #     event[0].connect(lambda e=event: self.do_move(e[1]))
-        #
-        # # Middle buttons : start
-        # self._joystick_logic.signal_middle_left_pushed.connect(
-        #     lambda: self._confocal.start_scanning(zscan=False, tag='joystick') if self._get_interlock() else False)
-        #
-        # self._joystick_logic.signal_middle_right_pushed.connect(
-        #     lambda: self._confocal.stop_scanning() if self._get_interlock() else False)
-        #
-
-
-            # image_x_range
     def on_deactivate(self):
         """ Perform required deactivation.
         """
@@ -99,7 +80,8 @@ class JoystickToConfocal(GenericLogic):
             self.do_move('x', state['axis']['left_horizontal'])
         if state['axis']['left_vertical'] != 0:
             self.do_move('y', state['axis']['left_vertical'])
-
+        if state['axis']['trigger'] != 0:
+            self.do_move('z', state['axis']['trigger'])
 
     def do_step(self, axis, direction=1):
         """ Function called when a button is pushed and should act on a position """
@@ -110,37 +92,13 @@ class JoystickToConfocal(GenericLogic):
 
     def do_move(self, axis, value):
         """ Function called when a joystick is moved to move the scanner position """
-        step = self.get_step_joystick(axis)
         position = self._confocal.get_position()
-        position[axis] += step
+        position[axis] += self.get_step(axis) / self.joystick_logic.fps() * value ** self._joystick_gamma_correction
         self._confocal.set_position('joystick_axis', *position)
 
     def get_step(self, axis, direction=1):
         """ Helper function do compute the step for a given axis and a given direction """
-        _range = getattr(self._confocal, '{}_range'.format(axis))
-        step = (_range[1] - _range[0]) * self._button_step_ratio
+        scanner_range = getattr(self._confocal, '{}_range'.format(axis))
+        step = (scanner_range[1] - scanner_range[0]) * self._button_step_ratio
         step = step * direction
         return step
-
-    def get_step_joystick(self, axis):
-        """ Helper function do compute the step for a given axis joystick """
-        _range = getattr(self._confocal, '{}_range'.format(self.axis_index_to_letter(axis)))
-        step = (_range[1] - _range[0]) * self._button_step_ratio
-        step = step * self.get_axis_joystick_position(axis)
-        return step
-
-    def get_axis_joystick_position(self, axis, which='left'):
-        """ Helper function to get a joystick axis corrected position """
-        position = 0
-        state = self._joystick_logic.get_last_state()
-        if axis == 0:
-            position = state['axis']['{}_horizontal'.format(which)]
-        if axis == 1:
-            position = state['axis']['{}_vertical'.format(which)]
-        if axis == 2:
-            position = - state['axis']['left_trigger'] + state['axis']['right_trigger']
-        if position == 0:
-            return 0
-        sign = position / abs(position)
-        position = abs(position)**self._joystick_gamma_correction*sign
-        return position
