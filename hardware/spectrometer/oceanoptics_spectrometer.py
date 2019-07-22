023ee9db9654f31f8977ca17878920c16c7f5da0
jochen-scheuer <scheuerjochen@gmail.com>
2019-07-22 16:58:36 +0200

    Fixing copy paste error.

--- hardware/spectrometer/oceanoptics_spectrometer.py
+++ hardware/spectrometer/oceanoptics_spectrometer.py
@@ -14,9 +14,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with Qudi. If not, see <http://www.gnu.org/licenses/>.
 
-Derived from the pyPL project (https://github.com/kaseyrussell/pyPL)
-Copyright 2010 Kasey Russell ( email: krussell _at_ post.harvard.edu )
-
 Copyright (c) the Qudi Developers. See the COPYRIGHT.txt file at the
 top-level directory of this distribution and at <https://github.com/Ulm-IQO/qudi/>
 
