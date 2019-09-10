8f2cdfcc06d8edef9101970c123dbe036af8c8ee
kay-jahnke <kay.jahnke@alumni.uni-ulm.de>
2019-09-10 12:08:52 +0200

    safer implementation of the ThreadFixer, but still does not solve the problem of notebook prints of threads being printed in the qudi console, if the hierarchical structure of the redirects is disturbed.

--- logic/jupyterkernel/helpers.py
+++ logic/jupyterkernel/helpers.py
@@ -29,11 +29,6 @@ distributed as part of this software.
 import sys
 import builtins
 from base64 import encodebytes
-import threading
-
-
-class ThreadFixer(threading.Thread):
-    notebook_thread = True
 
 
 class ExecutionResult:
