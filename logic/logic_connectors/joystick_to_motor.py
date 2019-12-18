a5be48cabf4e6d8e15a1db39eea2ed6f34c66b58
Alrik <alrik.durand@gmail.com>
2019-12-18 14:52:14 +0100

    modified joystick to motor

--- logic/logic_connectors/joystick_to_motor.py
+++ logic/logic_connectors/joystick_to_motor.py
@@ -26,24 +26,24 @@ import numpy as np
 import time
 
 
-class JoystickToSteppers(GenericLogic):
-    """ This logic module get callbacks from joystick logic and interact with confocal logic based on these events
+class JoystickToMotor(GenericLogic):
+    """ This logic module get callbacks from joystick logic and interact with motor hardware based on these events
 
     This module needs joystick_logic to function
 
     To prevent accidentally changing things, module only act if some 'trigger_keys' are pushed.
 
     Example configuration :
-        connector_joystick_confocal:
-        module.Class: 'logic_connectors.joystick_to_confocal.JoystickToConfocal'
+        connector_joystick_motor:
+        module.Class: 'logic_connectors.joystick_to_motor.JoystickToMotor'
         connect:
             joystick_logic: joystick_logic
-            confocal: scanner
+            motor: motor_logic
     """
 
     # declare connectors
     joystick_logic = Connector(interface='JoystickLogic')
-    hardware = Connector(interface='SteppersInterface')
+    motor_hardware = Connector(interface='MotorInterface')
 
     _button_interlock = ConfigOption('button_trigger', 'right_left')  # if this button is not pushed, do nothing
     # this must be a button from logic _button_list
@@ -51,7 +51,6 @@ class JoystickToSteppers(GenericLogic):
     # sensitivity of the axes
 
     _hardware_frequency = ConfigOption('hardware_frequency', 100)
-    _hardware_voltage = ConfigOption('hardware_voltage', 30)
     _axis = ConfigOption('axis', ('x', 'y', 'z'))
     _xy_max_velocity = ConfigOption('xy_max_velocity', 100)  # the maximum number of steps by second
     _z_max_velocity = ConfigOption('z_max_velocity', 20)  # the maximum number of steps by second
@@ -114,48 +113,8 @@ class JoystickToSteppers(GenericLogic):
             if axis:
                 steps = difference[int(index)]
                 if steps:
-                    self.hardware().steps(axis, steps)
+                    self.motor_logic.motor_hardware().move_rel(axis, steps)
                     changed = True
         self._hardware_position = after
         # if changed:
         #     self.log.debug('New position ({}, {}, {})'.format(*after))
-
-    def setup_axis(self):
-        """ Set axis as in config file"""
-        for axis in self._axis:
-            if axis:
-                self.hardware().frequency(axis, self._hardware_frequency)
-                self.hardware().voltage(axis, self._hardware_voltage)
-
-    def hello(self):
-        """ Greet humans properly """
-
-        axis = self._axis[0]
-        notes = {'c': 261, 'd': 294, 'e': 329, 'f': 349, 'g': 391, 'gS': 415, 'a': 440, 'aS': 455, 'b': 466, 'cH': 523,
-                 'cSH': 554, 'dH': 587, 'dSH': 622, 'eH': 659, 'fH': 698, 'fSH': 740, 'gH': 784, 'gSH': 830, 'aH': 880}
-
-        first_section = [('a', 500), ('a', 500), ('a', 500), ('f', 350), ('cH', 150), ('a', 500), ('f', 350),
-                         ('cH', 150), ('a', 650), ('', 500), ('eH', 500), ('eH', 500), ('eH', 500), ('fH', 350),
-                         ('cH', 150), ('gS', 500), ('f', 350), ('cH', 150), ('a', 650), ('', 500)]
-        second_section = [('aH', 500), ('a', 300), ('a', 150), ('aH', 500), ('gSH', 325), ('fSH', 125), ('fH', 125),
-                          ('fSH', 250), ('', 325), ('aS', 250), ('dSH', 500), ('dH', 325), ('cSH', 175), ('cH', 125),
-                          ('b', 125), ('cH', 250), ('', 350)]
-        variant_1 = [('f', 250), ('gS', 500), ('f', 350), ('a', 125), ('cH', 500), ('a', 375), ('cH', 125), ('eH', 650),
-                     ('', 500)]
-        variant_2 = [('f', 250), ('gS', 500), ('f', 375), ('cH', 125), ('a', 500), ('f', 375), ('cH', 125), ('a', 650),
-                     ('', 650)]
-        total = first_section + second_section + variant_1 + second_section + variant_2
-        count = 0
-        up = True
-        for note, duration in total:
-            if note != '':
-                frequency = notes[note]
-                steps = int(frequency * (float(duration)/1000.))
-                self.hardware().frequency(axis, frequency)
-                if not up:
-                    steps = -steps
-                count += steps
-                self.hardware().steps(axis, steps)
-            time.sleep((duration + 50)/1000)
-            up = not up
-        self.hardware().steps(axis, -count)  # Back to origin
