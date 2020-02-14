632e5b67cbe29e12146338c183768b8b9590552f
Neverhorst <tomek.qudi@gmail.com>
2020-02-14 14:59:28 +0100

    Code cleaning of remote.py. Functionality has not been touched so far.

--- core/remote.py
+++ core/remote.py
@@ -19,10 +19,7 @@ Copyright (c) the Qudi Developers. See the COPYRIGHT.txt file at the
 top-level directory of this distribution and at <https://github.com/Ulm-IQO/qudi/>
 """
 
-
 import logging
-logger = logging.getLogger(__name__)
-
 from qtpy.QtCore import QObject
 from urllib.parse import urlparse
 import ssl
@@ -30,6 +27,9 @@ from .util.models import DictTableModel, ListTableModel
 import rpyc
 from rpyc.utils.server import ThreadedServer
 from rpyc.utils.authenticators import SSLAuthenticator
+from .manager import Manager
+
+logger = logging.getLogger(__name__)
 rpyc.core.protocol.DEFAULT_CONFIG['allow_pickle'] = True
 
 
@@ -41,21 +41,24 @@ class RemoteObjectManager(QObject):
         """ Handle sharing and getting shared modules.
         """
         super().__init__(**kwargs)
-        self.tm = manager.thread_manager
-        self.manager = manager
-        self.remoteModules = ListTableModel()
-        self.remoteModules.headers[0] = 'Remote Modules'
-        self.sharedModules = DictTableModel()
-        self.sharedModules.headers[0] = 'Shared Modules'
+        if not isinstance(manager, Manager):
+            raise TypeError('manager argument must be Manager class instance.')
+        self.__manager = manager
+        self.remote_modules = ListTableModel()
+        self.remote_modules.headers[0] = 'Remote Modules'
+        self.shared_modules = DictTableModel()
+        self.shared_modules.headers[0] = 'Shared Modules'
+        self.server = None
 
-    def makeRemoteService(self):
-        """ A function that returns a class containing a module list hat can be manipulated from the host.
+    def make_remote_service(self):
+        """ A function that returns a class containing a module list hat can be manipulated from
+        the host.
         """
         class RemoteModuleService(rpyc.Service):
             """ An RPyC service that has a module list.
             """
-            modules = self.sharedModules
-            _manager = self.manager
+            modules = self.shared_modules
+            _manager = self.__manager
 
             @staticmethod
             def get_service_name():
@@ -76,118 +79,123 @@ class RemoteObjectManager(QObject):
             def exposed_getModule(self, name):
                 """ Return reference to a module in the shared module list.
 
-                  @param str name: unique module name
+                @param str name: unique module name
 
-                  @return object: reference to the module
+                @return object: reference to the module
                 """
                 name = str(name)
                 if name in self.modules.storage:
                     return self.modules.storage[name]
                 else:
-                    for base in ['hardware', 'logic', 'gui']:
-                        logger.info('remotesearch: {0}'.format(name))
-                        if name in self._manager.tree['defined'][base] and 'remoteaccess' in self._manager.tree['defined'][base][name]:
+                    logger.info('remotesearch: {0}'.format(name))
+                    if name in self._manager.managed_modules:
+                        module = self._manager.managed_modules[name]
+                        if module.allow_remote_access:
                             self._manager.activate_module(name)
-                            logger.info('remoteload: {0}{1}'.format(base, name))
+                            logger.info('remote load: {0}.{1}'.format(module.module_base, name))
                     if name in self.modules.storage:
                         return self.modules.storage[name]
                     else:
-                        logger.error('Client requested a module that is not '
-                                'shared.')
+                        logger.error('Client requested a module that is not shared.')
                         return None
         return RemoteModuleService
 
-    def createServer(self, hostname, port, certfile=None, keyfile=None):
+    def create_server(self, hostname, port, certfile=None, keyfile=None):
         """ Start the rpyc modules server on a given port.
 
-          @param int port: port where the server should be running
+        @param str hostname:
+        @param int port: port where the server should be running
+        @param str certfile:
+        @param str keyfile:
         """
-        thread = self.tm.get_new_thread('rpyc-server')
+        thread = self.__manager.thread_manager.get_new_thread('rpyc-server')
         if certfile is not None and keyfile is not None:
-            self.server = RPyCServer(
-                self.makeRemoteService(),
-                hostname,
-                port,
-                keyfile=keyfile,
-                certfile=certfile)
+            self.server = RPyCServer(self.make_remote_service(),
+                                     hostname,
+                                     port,
+                                     keyfile=keyfile,
+                                     certfile=certfile)
         else:
             if hostname != 'localhost':
                 logger.warning('Remote connection not secured! Use a certificate!')
-            self.server = RPyCServer(self.makeRemoteService(), hostname, port)
+            self.server = RPyCServer(self.make_remote_service(), hostname, port)
         self.server.moveToThread(thread)
         thread.started.connect(self.server.run)
         thread.start()
         logger.info('Started module server at {0} on port {1}'.format(hostname, port))
 
-    def stopServer(self):
+    def stop_server(self):
         """ Stop the remote module server.
         """
-        if hasattr(self, 'server'):
+        if self.server is not None:
             self.server.close()
 
-    def shareModule(self, name, obj):
+    def share_module(self, name, obj):
         """ Add a module to the list of modules that can be accessed remotely.
 
-          @param str name: unique name that is used to access the module
-          @param object obj: a reference to the module
+        @param str name: unique name that is used to access the module
+        @param object obj: a reference to the module
         """
-        if name in self.sharedModules.storage:
-            logger.warning('Module {0} already shared.'.format(name))
-        self.sharedModules.add(name, obj)
-        logger.info('Shared module {0}.'.format(name))
+        if name in self.shared_modules.storage:
+            logger.warning('Module "{0}" already shared.'.format(name))
+        self.shared_modules.add(name, obj)
+        logger.info('Shared module "{0}".'.format(name))
 
-    def unshareModule(self, name):
+    def remove_shared_module(self, name):
         """ Remove a module from the shared module list.
 
-          @param str name: unique name of the module that should not be accessible any more
+        @param str name: unique name of the module that should not be accessible any more
         """
-        if name in self.sharedModules.storage:
-            logger.error('Module {0} was not shared.'.format(name))
-        self.sharedModules.pop(name)
+        if name not in self.shared_modules.storage:
+            logger.error('Module "{0}" was not shared.'.format(name))
+            return
+        self.shared_modules.pop(name)
 
-    def getRemoteModuleUrl(self, url, certfile=None, keyfile=None):
+    def get_remote_module_from_url(self, url, certfile=None, keyfile=None):
         """ Get a remote module via its URL.
 
-          @param str url: URL pointing to a module hosted b a remote server
-          @param str certfile: filename of certificate or None if SSL is not used
-          @param str keyfile: filename of key or None if SSL is not used
+        @param str url: URL pointing to a module hosted b a remote server
+        @param str certfile: filename of certificate or None if SSL is not used
+        @param str keyfile: filename of key or None if SSL is not used
 
-          @return object: remote module
+        @return object: remote module
         """
+        # FIXME: This method gets mainly called by Manager but certfile and keyfile are not used.
         parsed = urlparse(url)
         name = parsed.path.replace('/', '')
-        return self.getRemoteModule(parsed.hostname, parsed.port, name)
+        return self.get_remote_module(parsed.hostname, parsed.port, name)
 
-    def getRemoteModule(self, host, port, name, certfile=None, keyfile=None):
+    def get_remote_module(self, host, port, name, certfile=None, keyfile=None):
         """ Get a remote module via its host, port and name.
 
-          @param str host: host that the remote module server is running on
-          @param int port: port that the remote module server is listening on
-          @param str name: unique name of the remote module
-          @param str certfile: filename of certificate or None if SSL is not used
-          @param str keyfile: filename of key or None if SSL is not used
+        @param str host: host that the remote module server is running on
+        @param int port: port that the remote module server is listening on
+        @param str name: unique name of the remote module
+        @param str certfile: filename of certificate or None if SSL is not used
+        @param str keyfile: filename of key or None if SSL is not used
 
-          @return object: remote module
+        @return object: remote module
         """
         module = RemoteModule(host, port, name, certfile=certfile, keyfile=keyfile)
-        self.remoteModules.append(module)
+        self.remote_modules.append(module)
         return module.module
 
 
 class RPyCServer(QObject):
     """ Contains a RPyC server that serves modules to remote computers. Runs in a QThread.
     """
-    def __init__(self, serviceClass, host, port, certfile=None, keyfile=None):
+    def __init__(self, service_class, host, port, certfile=None, keyfile=None):
         """
-          @param class serviceClass: class that represents an RPyC service
+          @param class service_class: class that represents an RPyC service
           @param int port: port that hte RPyC server should listen on
         """
         super().__init__()
-        self.serviceClass = serviceClass
+        self.service_class = service_class
         self.host = host
         self.port = port
         self.certfile = certfile
         self.keyfile = keyfile
+        self.server = None
 
     def run(self):
         """ Start the RPyC server
@@ -212,7 +220,8 @@ class RPyCServer(QObject):
         self.server.start()
 
     def close(self):
-        self.server.close()
+        if self.server is not None:
+            self.server.close()
 
 
 class RemoteModule:
@@ -220,13 +229,15 @@ class RemoteModule:
     """
     def __init__(self, host, port, name, certfile=None, keyfile=None):
         if certfile is not None and keyfile is not None:
-            self.connection = rpyc.ssl_connect(
-                host,
-                port=port,
-                config={'allow_all_attrs': True},
-                certfile=certfile,
-                keyfile=keyfile)
+            self.connection = rpyc.ssl_connect(host,
+                                               port=port,
+                                               config={'allow_all_attrs': True},
+                                               certfile=certfile,
+                                               keyfile=keyfile)
         else:
             self.connection = rpyc.connect(host, port, config={'allow_all_attrs': True})
-        self.module = self.connection.root.getModule(name)
         self.name = name
+
+    @property
+    def module(self):
+        return self.connection.root.getModule(self.name)
