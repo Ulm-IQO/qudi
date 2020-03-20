e9b9c414d54f285bb51928550be22d064dd3722d
Neverhorst <tomek.qudi@gmail.com>
2020-03-20 18:56:04 +0100

    Added command line prompt to quit/restart when in no-gui mode. Fixed a minor bug with not prompting to restart.

--- core/application.py
+++ core/application.py
@@ -29,7 +29,7 @@ from qtpy import QtCore, QtWidgets, API_NAME
 from .logger import init_rotating_file_handler, get_logger
 from .util.paths import get_main_dir, get_default_log_dir
 from .util.helpers import import_check
-from .util.mutex import RecursiveMutex
+from .util.mutex import RecursiveMutex, Mutex
 from .config import Configuration
 from .watchdog import AppWatchdog
 from .manager import ModuleManager
@@ -48,10 +48,11 @@ class Qudi(QtCore.QObject):
 
     """
     _instance = None
-    _lock = RecursiveMutex()
+    _run_lock = RecursiveMutex()
+    _quit_lock = Mutex()
 
     def __new__(cls, *args, **kwargs):
-        with cls._lock:
+        with cls._run_lock:
             if cls._instance is None or cls._instance() is None:
                 obj = super().__new__(cls, *args, **kwargs)
                 cls._instance = weakref.ref(obj)
@@ -74,28 +75,27 @@ class Qudi(QtCore.QObject):
 
         self._configured_extension_paths = list()
         self._is_running = False
+        self._shutting_down = False
 
     @classmethod
     def instance(cls):
-        with cls._lock:
-            if cls._instance is None:
-                return None
-            return cls._instance()
+        if cls._instance is None:
+            return None
+        return cls._instance()
 
     @property
     def is_running(self):
-        with self._lock:
-            return self._is_running
+        return self._is_running
 
     def _remove_extensions_from_path(self):
-        with self._lock:
+        with self._run_lock:
             # Clean up previously configured expansion paths
             for ext_path in self._configured_extension_paths:
                 if ext_path in sys.path:
                     sys.path.remove(ext_path)
 
     def _add_extensions_to_path(self):
-        with self._lock:
+        with self._run_lock:
             extensions = self.configuration.extension_paths
             # Add qudi extension paths to sys.path
             for ext_path in reversed(extensions):
@@ -106,7 +106,7 @@ class Qudi(QtCore.QObject):
     def _configure_qudi(self):
         """
         """
-        with self._lock:
+        with self._run_lock:
             print('\n================= Starting Qudi configuration from "{0}" ================='
                   '\n'.format(self.configuration.config_file))
             self.log.info(
@@ -148,138 +148,167 @@ class Qudi(QtCore.QObject):
 
         @return:
         """
-        if self._is_running:
-            raise Exception('Qudi is already running!')
+        with self._run_lock:
+            if self._is_running:
+                raise Exception('Qudi is already running!')
 
-        # add qudi to PATH
-        qudi_path = get_main_dir()
-        if qudi_path not in sys.path:
-            sys.path.insert(1, qudi_path)
+            # add qudi to PATH
+            qudi_path = get_main_dir()
+            if qudi_path not in sys.path:
+                sys.path.insert(1, qudi_path)
 
-        # Enable stack trace output for SIGSEGV, SIGFPE, SIGABRT, SIGBUS and SIGILL signals
-        # -> e.g. for segmentation faults
-        faulthandler.disable()
-        faulthandler.enable(all_threads=True)
+            # Enable stack trace output for SIGSEGV, SIGFPE, SIGABRT, SIGBUS and SIGILL signals
+            # -> e.g. for segmentation faults
+            faulthandler.disable()
+            faulthandler.enable(all_threads=True)
 
-        # install logging facility
-        init_rotating_file_handler(path=self.log_dir)
-        self.log.info('Loading Qudi...')
-        print('Loading Qudi...')
+            # install logging facility
+            init_rotating_file_handler(path=self.log_dir)
+            self.log.info('Loading Qudi...')
+            print('Loading Qudi...')
 
-        # Check Qt API
-        self.log.info('Used Qt API: {0}'.format(API_NAME))
-        print('Used Qt API: {0}'.format(API_NAME))
+            # Check Qt API
+            self.log.info('Used Qt API: {0}'.format(API_NAME))
+            print('Used Qt API: {0}'.format(API_NAME))
 
-        # Check vital packages for qudi, otherwise qudi will not even start.
-        err_code = import_check()
-        if err_code != 0:
-            sys.exit(err_code)
+            # Check vital packages for qudi, otherwise qudi will not even start.
+            err_code = import_check()
+            if err_code != 0:
+                sys.exit(err_code)
 
-        # Get QApplication instance
-        if self.no_gui:
-            app = QtCore.QCoreApplication.instance()
-        else:
-            app = QtWidgets.QApplication.instance()
-        if app is None:
+            # Get QApplication instance
             if self.no_gui:
-                app = QtCore.QCoreApplication(sys.argv)
+                app = QtCore.QCoreApplication.instance()
             else:
-                app = QtWidgets.QApplication(sys.argv)
+                app = QtWidgets.QApplication.instance()
+            if app is None:
+                if self.no_gui:
+                    app = QtCore.QCoreApplication(sys.argv)
+                else:
+                    app = QtWidgets.QApplication(sys.argv)
 
-        # configure qudi
-        self.configuration.load_config()
-        self._configure_qudi()
+            # configure qudi
+            self.configuration.load_config()
+            self._configure_qudi()
 
-        # Install the pyzmq ioloop.
-        # This has to be done before anything else from tornado is imported.
-        try:
-            ioloop.install()
-        except:
-            self.log.error('Preparing ZMQ failed, probably no IPython possible!')
+            # Install the pyzmq ioloop.
+            # This has to be done before anything else from tornado is imported.
+            try:
+                ioloop.install()
+            except:
+                self.log.error('Preparing ZMQ failed, probably no IPython possible!')
 
-        # manhole for debugging stuff inside the app from outside
-        # if args.manhole:
-        #     import manhole
-        #     manhole.install()
+            # manhole for debugging stuff inside the app from outside
+            # if args.manhole:
+            #     import manhole
+            #     manhole.install()
 
-        # first disable our pyqtgraph's cleanup function; won't be needing it.
-        # try:
-        #     import pyqtgraph
-        #     pyqtgraph.setConfigOptions(exitCleanup=False)
-        # except ImportError:
-        #     pass
+            # first disable our pyqtgraph's cleanup function; won't be needing it.
+            # try:
+            #     import pyqtgraph
+            #     pyqtgraph.setConfigOptions(exitCleanup=False)
+            # except ImportError:
+            #     pass
 
-        # Install app watchdog
-        self.watchdog = AppWatchdog()
+            # Install app watchdog
+            self.watchdog = AppWatchdog()
 
-        # Start GUI if needed
-        self._start_gui()
+            # Start GUI if needed
+            self._start_gui()
 
-        # Start Qt event loop unless running in interactive mode
-        self._is_running = True
-        exit_code = app.exec_()
+            # Start Qt event loop unless running in interactive mode
+            self._is_running = True
+            exit_code = app.exec_()
+            self._shutting_down = False
+            self._is_running = False
 
-        # ToDo: Is the following issue still a thing with qudi?
-        # in this subprocess we redefine the stdout, therefore on Unix systems we need to handle
-        # the opened file descriptors, see PEP 446: https://www.python.org/dev/peps/pep-0446/
-        if sys.platform in ('linux', 'darwin'):
-            fd_min, fd_max = 3, 4096
-            fd_set = set(range(fd_min, fd_max))
+            # ToDo: Is the following issue still a thing with qudi?
+            # in this subprocess we redefine the stdout, therefore on Unix systems we need to handle
+            # the opened file descriptors, see PEP 446: https://www.python.org/dev/peps/pep-0446/
+            if sys.platform in ('linux', 'darwin'):
+                fd_min, fd_max = 3, 4096
+                fd_set = set(range(fd_min, fd_max))
 
-            if sys.platform == 'darwin':
-                # trying to close 7 produces an illegal instruction on the Mac.
-                fd_set.remove(7)
+                if sys.platform == 'darwin':
+                    # trying to close 7 produces an illegal instruction on the Mac.
+                    fd_set.remove(7)
 
-            # remove specified file descriptor
-            for fd in fd_set:
-                try:
-                    os.close(fd)
-                except OSError:
-                    pass
+                # remove specified file descriptor
+                for fd in fd_set:
+                    try:
+                        os.close(fd)
+                    except OSError:
+                        pass
 
-        # Exit application
-        sys.exit(exit_code)
+            # Exit application
+            sys.exit(exit_code)
+
+    def exit(self, prompt=True, restart=False):
+        """ Shutdown Qudi. Nicely request that all modules shut down if prompt is True.
+        Signal restart to parent process (if present) via exitcode 42 if restart is True.
+        """
+        with self._quit_lock:
+            if not self.is_running or self._shutting_down:
+                return
+            self._shutting_down = True
+            if prompt:
+                locked_modules = False
+                broken_modules = False
+                for module in self.module_manager.values():
+                    if module.is_busy:
+                        locked_modules = True
+                    elif module.state == 'BROKEN':
+                        broken_modules = True
+                    if broken_modules and locked_modules:
+                        break
+
+                if self.no_gui:
+                    # command line prompt
+                    question = '\nSome modules are still locked. ' if locked_modules else '\n'
+                    if restart:
+                        question += 'Do you really want to restart Qudi (y/N)?: '
+                    else:
+                        question += 'Do you really want to quit Qudi (y/N)?: '
+                    while True:
+                        response = input(question).lower()
+                        if response in ('y', 'yes'):
+                            break
+                        elif response in ('', 'n', 'no'):
+                            return
+                else:
+                    # GUI prompt
+                    if restart:
+                        if not self.gui.prompt_restart(locked_modules):
+                            return
+                    else:
+                        if not self.gui.prompt_shutdown(locked_modules):
+                            return
+
+            remotemodules.stop_remote_server()
+            self.module_manager.clear()
+            if not self.no_gui:
+                self.log.info('Closing windows...')
+                print('Closing windows...')
+                self.gui.close_windows()
+                self.gui.close_system_tray_icon()
+            if restart:
+                QtCore.QCoreApplication.exit(42)
+            else:
+                QtCore.QCoreApplication.quit()
 
     @QtCore.Slot()
-    @QtCore.Slot(bool)
-    def quit(self, prompt=True, restart=False):
-        """ Shutdown Qudi. Nicely request that all modules shut down if prompt is True.
-        """
-        if not self.is_running:
-            return
-        if prompt:
-            locked_modules = False
-            broken_modules = False
-            for module in self.module_manager.values():
-                if module.is_busy:
-                    locked_modules = True
-                elif module.state == 'BROKEN':
-                    broken_modules = True
-                if broken_modules and locked_modules:
-                    break
+    def quit(self):
+        self.exit(prompt=False, restart=False)
 
-            if self.no_gui:
-                # FIXME: console prompt here
-                pass
-            else:
-                if restart:
-                    if not self.gui.prompt_restart(locked_modules):
-                        return
-                else:
-                    if not self.gui.prompt_shutdown(locked_modules):
-                        return
+    @QtCore.Slot()
+    def prompt_quit(self):
+        self.exit(prompt=True, restart=False)
 
-        remotemodules.stop_remote_server()
-        self.module_manager.clear()
-        if not self.no_gui:
-            self.log.info('Closing windows...')
-            print('Closing windows...')
-            self.gui.close_windows()
-            self.gui.close_system_tray_icon()
-        if restart:
-            QtCore.QCoreApplication.exit(42)
-        else:
-            QtCore.QCoreApplication.quit()
+    @QtCore.Slot()
+    def restart(self):
+        self.exit(prompt=False, restart=True)
 
-    def restart(self, prompt=True):
-        self.quit(prompt, restart=True)
+    @QtCore.Slot()
+    def prompt_restart(self):
+        self.exit(prompt=True, restart=True)
+
