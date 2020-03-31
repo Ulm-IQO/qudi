ccf4f6b9e55e16b89c48ec3838b8a15b59f370d1
Neverhorst <tomek.qudi@gmail.com>
2020-03-31 14:00:43 +0200

    Proper model/view handling of qudi module lists in Main GUI.

--- qudi/core/gui/main_gui/modulewidget.py
+++ qudi/core/gui/main_gui/modulewidget.py
@@ -22,19 +22,20 @@ top-level directory of this distribution and at <https://github.com/Ulm-IQO/qudi
 import os
 from qtpy import QtCore, QtGui, QtWidgets
 from qudi.core.util.paths import get_main_dir
+from qudi.core.util.mutex import Mutex
 
 
 class ModuleFrameWidget(QtWidgets.QFrame):
     """
     Custom module QFrame widget for the Qudi manager GUI
     """
-    sigLoadClicked = QtCore.Signal(str)
+    sigActivateClicked = QtCore.Signal(str)
     sigDeactivateClicked = QtCore.Signal(str)
     sigReloadClicked = QtCore.Signal(str)
     sigCleanupClicked = QtCore.Signal(str)
 
-    def __init__(self, parent=None, module_name=None, **kwargs):
-        super().__init__(parent, **kwargs)
+    def __init__(self, *args, module_name=None, **kwargs):
+        super().__init__(*args, **kwargs)
 
         # Create QToolButtons
         self.cleanup_button = QtWidgets.QToolButton()
@@ -51,12 +52,12 @@ class ModuleFrameWidget(QtWidgets.QFrame):
         self.reload_button.setIcon(QtGui.QIcon(os.path.join(icon_path, 'view-refresh.png')))
 
         # Create activation pushbutton
-        self.load_button = QtWidgets.QPushButton('load/activate <module_name>')
-        self.load_button.setObjectName('loadButton')
-        # self.load_button.setCheckable(True)
-        self.load_button.setMinimumWidth(200)
-        self.load_button.setSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding,
-                                       QtWidgets.QSizePolicy.Fixed)
+        self.activate_button = QtWidgets.QPushButton('load/activate <module_name>')
+        self.activate_button.setObjectName('loadButton')
+        # self.activate_button.setCheckable(True)
+        self.activate_button.setMinimumWidth(200)
+        self.activate_button.setSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding,
+                                           QtWidgets.QSizePolicy.Fixed)
 
         # Create status label
         self.status_label = QtWidgets.QLabel('Module status goes here...')
@@ -66,12 +67,12 @@ class ModuleFrameWidget(QtWidgets.QFrame):
         self.cleanup_button.setToolTip('Clean up module status file')
         self.deactivate_button.setToolTip('Deactivate module')
         self.reload_button.setToolTip('Reload module')
-        self.load_button.setToolTip('Load this module and all its dependencies')
+        self.activate_button.setToolTip('Load this module and all its dependencies')
         self.status_label.setToolTip('Displays module status information')
 
         # Combine all widgets in a layout and set as main layout
         layout = QtWidgets.QGridLayout()
-        layout.addWidget(self.load_button, 0, 0)
+        layout.addWidget(self.activate_button, 0, 0)
         layout.addWidget(self.reload_button, 0, 1)
         layout.addWidget(self.deactivate_button, 0, 2)
         layout.addWidget(self.cleanup_button, 0, 3)
@@ -82,30 +83,30 @@ class ModuleFrameWidget(QtWidgets.QFrame):
         if module_name:
             self.set_module_name(module_name)
 
-        self.load_button.clicked.connect(self.load_clicked)
-        self.reload_button.clicked.connect(self.reload_clicked)
+        self.activate_button.clicked.connect(self.activate_clicked)
         self.deactivate_button.clicked.connect(self.deactivate_clicked)
+        self.reload_button.clicked.connect(self.reload_clicked)
         self.cleanup_button.clicked.connect(self.cleanup_clicked)
         return
 
     def set_module_name(self, name):
         if name:
-            self.load_button.setText('Load {0}'.format(name))
+            self.activate_button.setText('Load {0}'.format(name))
             self._module_name = name
 
     def set_module_state(self, state):
         if state == 'not loaded':
-            self.load_button.setText('Load {0}'.format(self._module_name))
+            self.activate_button.setText('Load {0}'.format(self._module_name))
             self.cleanup_button.setEnabled(True)
             self.deactivate_button.setEnabled(False)
             self.reload_button.setEnabled(False)
         elif state == 'deactivated':
-            self.load_button.setText('Activate {0}'.format(self._module_name))
+            self.activate_button.setText('Activate {0}'.format(self._module_name))
             self.cleanup_button.setEnabled(True)
             self.deactivate_button.setEnabled(False)
             self.reload_button.setEnabled(False)
         else:
-            self.load_button.setText(self._module_name)
+            self.activate_button.setText(self._module_name)
             self.cleanup_button.setEnabled(False)
             self.deactivate_button.setEnabled(True)
             self.reload_button.setEnabled(True)
@@ -113,8 +114,8 @@ class ModuleFrameWidget(QtWidgets.QFrame):
         return
 
     @QtCore.Slot()
-    def load_clicked(self):
-        self.sigLoadClicked.emit(self._module_name)
+    def activate_clicked(self):
+        self.sigActivateClicked.emit(self._module_name)
 
     @QtCore.Slot()
     def deactivate_clicked(self):
@@ -129,87 +130,147 @@ class ModuleFrameWidget(QtWidgets.QFrame):
         self.sigReloadClicked.emit(self._module_name)
 
 
-class ModuleScrollWidget(QtWidgets.QScrollArea):
+class ModuleListModel(QtCore.QAbstractListModel):
     """
-
     """
-    sigActivateModule = QtCore.Signal(str)
-    sigDeactivateModule = QtCore.Signal(str)
-    sigCleanupModule = QtCore.Signal(str)
-    sigReloadModule = QtCore.Signal(str)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._lock = Mutex()
+        self._module_states = dict()
+        self._module_names = list()
 
-    def __init__(self, parent=None, module_names=None, **kwargs):
-        super().__init__(parent, **kwargs)
-        self.setLayout(QtWidgets.QVBoxLayout())
+    def rowCount(self, parent):
+        return len(self._module_names)
 
-        self._frames = dict()
-        if module_names:
-            self.create_module_frames(module_names)
-        return
+    def data(self, index, role):
+        if not index.isValid():
+            return
+        row = index.row()
+        if row >= len(self._module_names):
+            return
+        name = self._module_names[row]
+        state = self._module_states[name]
+        if role == QtCore.Qt.DisplayRole:
+            return name, state
 
-    def __contains__(self, item):
-        return self._frames.__contains__(item)
+    def flags(self, index):
+        return QtCore.Qt.ItemNeverHasChildren | QtCore.Qt.ItemIsEnabled# | QtCore.Qt.ItemIsEditable
 
-    def __len__(self):
-        return self._frames.__len__()
+    def append_module(self, name, state):
+        with self._lock:
+            if name in self._module_states:
+                raise Exception(
+                    'Module with name "{0}" already present in ModuleListModel.'.format(name))
+            self.beginInsertRows(len(self._module_names))
+            self._module_names.append(name)
+            self._module_states[name] = state
+            self.endInsertRows()
 
-    def clear(self):
-        # Remove old connections and module frame widgets
-        for frame in self._frames.values():
-            frame.sigLoadClicked.disconnect()
-            frame.sigReloadClicked.disconnect()
-            frame.sigDeactivateClicked.disconnect()
-            frame.sigCleanupClicked.disconnect()
-            frame.setParent(None)
-        self._frames = dict()
+    def remove_module(self, name):
+        with self._lock:
+            if name not in self._module_states:
+                return
+            row = self._module_names.index(name)
+            self.beginRemoveRows(row, row + 1)
+            del self._module_names[row]
+            del self._module_states[name]
+            self.endRemoveRows()
 
-    @property
-    def module_names(self):
-        return tuple(self._frames)
+    def reset_modules(self, state_dict):
+        with self._lock:
+            self.beginResetModel()
+            self._module_states = state_dict.copy()
+            self._module_names = list(state_dict)
+            self.endResetModel()
 
-    @property
-    def module_frames(self):
-        return self._frames.copy()
+    def change_module_state(self, name, state):
+        with self._lock:
+            if name not in self._module_states:
+                raise Exception('Can not change module state in ModuleListModel. No module by the '
+                                'name "{0}" found.'.format(name))
+            self._module_states[name] = state
+            row = self._module_names.index(name)
+            self.dataChanged.emit(self.createIndex(row, 0),
+                                  self.createIndex(row + 1, 0),
+                                  (QtCore.Qt.DisplayRole,))
 
-    @QtCore.Slot(dict)
-    def create_module_frames(self, modules):
-        # Create new frame widgets and connect them
-        frames = dict()
-        for name, mod in modules.items():
-            if name in frames:
-                raise NameError('Module with name "{0}" occurs twice in module list.')
-            frames[name] = ModuleFrameWidget(parent=self, module_name=name)
-            frames[name].sigLoadClicked.connect(self.sigActivateModule)
-            frames[name].sigReloadClicked.connect(self.sigReloadModule)
-            frames[name].sigDeactivateClicked.connect(self.sigDeactivateModule)
-            frames[name].sigCleanupClicked.connect(self.sigCleanupModule)
-            frames[name].set_module_state(mod.state)
 
-        # delete old frame widgets and disconnect them
-        self.clear()
+class ModuleListItemDelegate(QtWidgets.QStyledItemDelegate):
+    """
+    """
+    sigActivateClicked = QtCore.Signal(str)
+    sigDeactivateClicked = QtCore.Signal(str)
+    sigReloadClicked = QtCore.Signal(str)
+    sigCleanupClicked = QtCore.Signal(str)
 
-        self.setLayout(QtWidgets.QVBoxLayout())
-        self._frames = frames
-        for frame in frames.values():
-            self.layout().addWidget(frame)
-        return
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.render_widget = ModuleFrameWidget()
+        self.__origin = QtCore.QPoint()
 
-    @QtCore.Slot(dict)
-    def set_module_states(self, module_states):
-        for mod_name, frame in self._frames.items():
-            state = module_states.get(mod_name, 'BROKEN')
-            frame.set_module_state(state)
-        return
+    def createEditor(self, parent, option, index):
+        widget = ModuleFrameWidget(parent=parent)
+        widget.sigActivateClicked.connect(self.sigActivateClicked)
+        widget.sigDeactivateClicked.connect(self.sigDeactivateClicked)
+        widget.sigReloadClicked.connect(self.sigReloadClicked)
+        widget.sigCleanupClicked.connect(self.sigCleanupClicked)
+        return widget
 
-    @QtCore.Slot(str, str)
-    def set_module_state(self, name, state):
-        if name in self._frames:
-            self._frames[name].set_module_state(state)
+    def setEditorData(self, editor, index):
+        data = index.data()
+        if data:
+            editor.set_module_name(data[0])
+            editor.set_module_state(data[1])
+
+    def setModelData(self, editor, model, index):
+        pass
+
+    def sizeHint(self, option=None, index=None):
+        return self.render_widget.sizeHint()
+
+    def paint(self, painter, option, index):
+        """
+        """
+        name, state = index.data()
+        self.render_widget.set_module_name(name)
+        self.render_widget.set_module_state(state)
+        self.render_widget.setGeometry(option.rect)
+        painter.save()
+        painter.translate(option.rect.topLeft())
+        self.render_widget.render(painter, self.__origin)
+        painter.restore()
+
+
+class ModuleListView(QtWidgets.QListView):
+    """
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setMouseTracking(True)
+        delegate = ModuleListItemDelegate()
+        self.setItemDelegate(delegate)
+        self.setMinimumWidth(delegate.sizeHint().width())
+        self.setUniformItemSizes(True)
+        self.setSpacing(1)
+        self.previous_index = QtCore.QModelIndex()
+
+    def mouseMoveEvent(self, event):
+        index = self.indexAt(event.pos())
+        if index != self.previous_index:
+            if self.previous_index.isValid():
+                self.closePersistentEditor(self.previous_index)
+            if index.isValid():
+                self.openPersistentEditor(index)
+            self.previous_index = index
+
+    def leaveEvent(self, event):
+        if self.previous_index.isValid():
+            self.closePersistentEditor(self.previous_index)
+        self.previous_index = QtCore.QModelIndex()
 
 
 class ModuleWidget(QtWidgets.QTabWidget):
     """
-
     """
     sigActivateModule = QtCore.Signal(str)
     sigDeactivateModule = QtCore.Signal(str)
@@ -219,35 +280,30 @@ class ModuleWidget(QtWidgets.QTabWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
-        self.module_scroll_widgets = {'gui': ModuleScrollWidget(),
-                                      'logic': ModuleScrollWidget(),
-                                      'hardware': ModuleScrollWidget()}
-        self.addTab(self.module_scroll_widgets['gui'], 'GUI')
-        self.addTab(self.module_scroll_widgets['logic'], 'Logic')
-        self.addTab(self.module_scroll_widgets['hardware'], 'Hardware')
-        self.module_scroll_widgets['gui'].sigActivateModule.connect(self.sigActivateModule)
-        self.module_scroll_widgets['logic'].sigActivateModule.connect(self.sigActivateModule)
-        self.module_scroll_widgets['hardware'].sigActivateModule.connect(self.sigActivateModule)
-        self.module_scroll_widgets['gui'].sigDeactivateModule.connect(self.sigDeactivateModule)
-        self.module_scroll_widgets['logic'].sigDeactivateModule.connect(self.sigDeactivateModule)
-        self.module_scroll_widgets['hardware'].sigDeactivateModule.connect(self.sigDeactivateModule)
-        self.module_scroll_widgets['gui'].sigCleanupModule.connect(self.sigCleanupModule)
-        self.module_scroll_widgets['logic'].sigCleanupModule.connect(self.sigCleanupModule)
-        self.module_scroll_widgets['hardware'].sigCleanupModule.connect(self.sigCleanupModule)
-        self.module_scroll_widgets['gui'].sigReloadModule.connect(self.sigReloadModule)
-        self.module_scroll_widgets['logic'].sigReloadModule.connect(self.sigReloadModule)
-        self.module_scroll_widgets['hardware'].sigReloadModule.connect(self.sigReloadModule)
+        self.list_models = {'gui'     : ModuleListModel(),
+                            'logic'   : ModuleListModel(),
+                            'hardware': ModuleListModel()}
+        self.list_views = {'gui'     : ModuleListView(),
+                           'logic'   : ModuleListView(),
+                           'hardware': ModuleListView()}
+        self.addTab(self.list_views['gui'], 'GUI')
+        self.addTab(self.list_views['logic'], 'Logic')
+        self.addTab(self.list_views['hardware'], 'Hardware')
+        for base, view in self.list_views.items():
+            view.setModel(self.list_models[base])
+            delegate = view.itemDelegate()
+            delegate.sigActivateClicked.connect(self.sigActivateModule)
+            delegate.sigDeactivateClicked.connect(self.sigDeactivateModule)
+            delegate.sigReloadClicked.connect(self.sigReloadModule)
+            delegate.sigCleanupClicked.connect(self.sigCleanupModule)
 
     @QtCore.Slot(dict)
     def update_modules(self, modules_dict):
-        self.module_scroll_widgets['gui'].create_module_frames(
-            {name: mod for name, mod in modules_dict.items() if mod.module_base == 'gui'})
-        self.module_scroll_widgets['logic'].create_module_frames(
-            {name: mod for name, mod in modules_dict.items() if mod.module_base == 'logic'})
-        self.module_scroll_widgets['hardware'].create_module_frames(
-            {name: mod for name, mod in modules_dict.items() if mod.module_base == 'hardware'})
+        for base, model in self.list_models.items():
+            model.reset_modules(
+                {name: mod.state for name, mod in modules_dict.items() if mod.module_base == base})
         return
 
     @QtCore.Slot(str, str, str)
     def update_module_state(self, base, name, state):
-        self.module_scroll_widgets[base].set_module_state(name, state)
+        self.list_models[base].change_module_state(name, state)
