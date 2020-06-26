72358a610ad643879b2d16b37ba21c5a1e80e2e6
Timo Joas <timo.joas@gmx.de>
2020-06-26 17:33:49 +0200

    some performance optimizations for loading big waveforms - save ~20%. - note: binary.read() in for loop not slower than list comprehension of cached array

--- hardware/awg/keysight_plot_waveform.py
+++ hardware/awg/keysight_plot_waveform.py
@@ -3,7 +3,7 @@
 # *********************************************************
 # Script to decode and plot an M8190A/M8195A waveform binary
 # Usage: deliver path to channel file as argument to this
-# program (eg. in Pycharm: Run->Edit Conf->Parameters
+# program (eg. in Pycharm: Run->Edit Conf->Parameters)
 # *********************************************************
 
 # =========================================================
@@ -22,23 +22,6 @@ matplotlib.use('Qt5Agg')  # allows interactive mode in Pycharm
 import matplotlib.pyplot as plt
 
 
-# =========================================================
-# Main Program
-# =========================================================
-
-if len(sys.argv) != 2:
-    sys.stderr.write("Usage: python %s <binary_data_file>\n" % sys.argv[0])
-    sys.exit()
-
-# ---------------------------------------------------------
-# Open message output file.
-# ---------------------------------------------------------
-msg_output_file = re.sub("\.bin", "_info.txt", sys.argv[1])
-msg = open(msg_output_file, "w")
-
-# ---------------------------------------------------------
-# Open binary file.
-# ---------------------------------------------------------
 
 def sign_extend(value, bits):
     sign_bit = 1 << (bits - 1)
@@ -47,10 +30,12 @@ def sign_extend(value, bits):
 def decode_m8190_val(val_int):
     shiftbits = 16 - dac_resolution  # 2 for marker, dac: 12 -> 2, dac: 14 -> 4
 
-    bit_marker = bool(0x1 & np.asarray(val_int).astype('uint16'))
-    bit_sync = bool((0x2 & np.asarray(val_int).astype('uint16')) >> 1)
+    val_int = np.asarray(val_int).astype('uint16')
 
-    analog_binary = 0xFFFC & np.asarray(val_int).astype('uint16') >> shiftbits
+    bit_marker = bool(0x1 & val_int)
+    bit_sync = bool((0x2 & val_int) >> 1)
+
+    analog_binary = 0xFFFC & val_int >> shiftbits
     #print("Debug: Analog bit {:#016b}".format(analog_binary))
     # sign extend to correctly fit 14 bit of 2s complement into 16 bit int
     int_analog = np.int16(sign_extend(analog_binary, 14))
@@ -113,7 +98,7 @@ def strip_ch_str(path):
 
     return path
 
-def load_waveform_files(fname, mode='bin'):
+def get_waveform_files(fname, mode='bin'):
     if mode == 'bin':
         # eg. AWG8190
         path_ch1 = strip_ch_str(path) + "_ch1" + ".bin"
@@ -125,6 +110,12 @@ def load_waveform_files(fname, mode='bin'):
     else:
         raise ValueError("Unknown loading mode: {}".format(mode))
 
+    print("Loading waveforms file1: {}, file2: {}. Mode: {}".format(path_ch1, path_ch2, mode))
+
+    return path_ch1, path_ch2
+
+def load_waveform_files(fname, mode='bin'):
+    path_ch1, path_ch2 = get_waveform_files(fname, mode)
     try:
         bin_ch1 = open(path_ch1, "rb")
     except:
@@ -134,9 +125,47 @@ def load_waveform_files(fname, mode='bin'):
     except:
         bin_ch2 = None
 
-    print("Loading waveforms file1: {}, file2: {}. Mode: {}".format(path_ch1, path_ch2, mode))
-
     return bin_ch1, bin_ch2
+
+
+def _decode_from_int16(int_array, mode="bin", channel_config=None):
+
+    n_data = len(int_array)
+    print("Debug: Starting decoding {} int16 values.".format(n_data))
+
+    t_us = 1e6 * np.asarray(range(n_data)) / sampling_rate
+
+    res = [_decode_two_bytes(val16, mode, channel_config) for val16 in int_array]
+
+    wave_analog_int = [el[0] for el in res]
+    wave_analog = [analog_int_to_normalized_float(el) for el in wave_analog_int]
+    wave_sample_marker = [el[1] for el in res]
+    wave_sync_marker = [el[2] for el in res]
+
+    print("Debug: Decode done.")
+
+    return t_us, wave_analog, wave_analog_int, wave_sample_marker, wave_sync_marker
+
+
+def _decode_two_bytes(val_int16, mode='bin', channel_config=None):
+    # typtical file has a lot of 0s, break here for performance
+    if val_int16 == 0:
+        return 0, 0, 0
+
+    if mode == 'bin':
+        analog, marker, sync = decode_m8190_val(val_int16)
+    elif mode == 'bin8':
+        if channel_config == 'marker':
+            analog = decode_m8195_val(val_int16, 'a_ch1')
+            # marker sync only exist for M8190A
+            marker = decode_m8195_val(val_int16, 'd_ch1')
+            sync = decode_m8195_val(val_int16, 'd_ch2')
+        else:
+            raise NotImplementedError
+    else:
+        raise ValueError("Unknown decoding mode: {}".format(mode))
+
+    return analog, marker, sync
 
 def _decode(bin_input, mode="bin", channel_config=None):
 
@@ -152,23 +181,12 @@ def _decode(bin_input, mode="bin", channel_config=None):
     wave_sample_marker = np.zeros((n_data), dtype=bool)
     wave_sync_marker = np.zeros((n_data), dtype=bool)
 
+
     # Todo: Speed up by reading into buffer, instead of looping
     for i in range(n_data):
         try:
-            if mode == 'bin':
-                (val_int,) = struct.unpack('H', bin_input.read(n_bytes))
-                analog, marker, sync = decode_m8190_val(val_int)
-            elif mode == 'bin8':
-                (val_int,) = struct.unpack('H', bin_input.read(n_bytes))
-                if channel_config == 'marker':
-                    analog = decode_m8195_val(val_int, 'a_ch1')
-                    # marker sync only exist for M8190A
-                    marker = decode_m8195_val(val_int, 'd_ch1')
-                    sync = decode_m8195_val(val_int, 'd_ch2')
-                else:
-                    raise NotImplementedError
-            else:
-                raise ValueError("Unknown decoding mode: {}".format(mode))
+            (val_int,) = struct.unpack('H', bin_input.read(n_bytes))
+            analog, marker, sync = _decode_two_bytes(val_int, mode, channel_config=channel_config)
             wave_analog[i] = analog_int_to_normalized_float(analog)
             wave_analog_int[i] = analog
             wave_sample_marker[i] = marker
@@ -182,6 +200,16 @@ def _decode(bin_input, mode="bin", channel_config=None):
     return t_us, wave_analog, wave_analog_int, wave_sample_marker, wave_sync_marker
 
 
+def decode_int16_to_wave(int_array, mode='bin', channel_config=None):
+    if mode == 'bin':
+        if channel_config is not None:
+            print("Warning: ignoring specified channel {}".format(channel_config))
+        return _decode_from_int16(int_array, mode=mode)
+    elif mode == 'bin8':
+        return _decode_from_int16(int_array, mode=mode, channel_config='marker')
+    else:
+        raise ValueError("Unknown decoding mode: {}".format(mode))
+
 def decode_bin_to_wave(bin_input, mode='bin', channel_config=None):
     if mode == 'bin':
         if channel_config is not None:
@@ -193,8 +221,18 @@ def decode_bin_to_wave(bin_input, mode='bin', channel_config=None):
         raise ValueError("Unknown decoding mode: {}".format(mode))
 
 
+
+
 dac_resolution = 14
 sampling_rate = 8e9
+
+# =========================================================
+# Main Program
+# =========================================================
+
+if len(sys.argv) != 2:
+    sys.stderr.write("Usage: python %s <binary_data_file>\n" % sys.argv[0])
+    sys.exit()
 
 print("Settings: Sampling Rate: {}; DAC res (M8190A only): {} bit".format(sampling_rate,
                                                                           dac_resolution))
@@ -202,8 +240,27 @@ print("Settings: Sampling Rate: {}; DAC res (M8190A only): {} bit".format(sampli
 path = get_filename_no_extension(sys.argv[1])
 ext = get_extension(sys.argv[1])[1:]
 
-bin_in_ch1, bin_in_ch2 = load_waveform_files(path, mode=ext)
+import datetime
+start_fast = datetime.datetime.now()
 
+path_ch1, path_ch2 = get_waveform_files(path, mode=ext)
+data_ch1 = np.fromfile(path_ch1, dtype=np.uint16, count=-1)
+data_ch2 = np.fromfile(path_ch2, dtype=np.uint16, count=-1)
+if ext == 'bin':
+    t_us_ch1, w_analog_ch1, w_analog_int_ch1, w_sample_marker_ch1, w_sync_marker_ch1 = decode_int16_to_wave(data_ch1, mode=ext)
+    t_us_ch2, w_analog_ch2, w_analog_int_ch2, w_sample_marker_ch2, w_sync_marker_ch2 = decode_int16_to_wave(data_ch2, mode=ext)
+elif ext == 'bin8':
+    t_us_ch1, w_analog_ch1, _, w_sample_marker_ch1, w_sync_marker_ch1 = decode_int16_to_wave(data_ch1,
+                                                                                             mode=ext, channel_config='marker')
+else:
+    raise NotImplementedError
+
+
+"""
+stop_fast = datetime.datetime.now()
+start_slow = datetime.datetime.now()
+
+bin_in_ch1, bin_in_ch2 = load_waveform_files(path, mode=ext)
 if ext == 'bin':
     t_us_ch1, w_analog_ch1, w_analog_int_ch1, w_sample_marker_ch1, w_sync_marker_ch1 = decode_bin_to_wave(bin_in_ch1, mode=ext)
     t_us_ch2, w_analog_ch2, w_analog_int_ch2, w_sample_marker_ch2, w_sync_marker_ch2 = decode_bin_to_wave(bin_in_ch2, mode=ext)
@@ -212,10 +269,14 @@ elif ext == 'bin8':
                                                                                            mode=ext, channel_config='marker')
 else:
     raise NotImplementedError
+stop_slow = datetime.datetime.now()
+
+print("Debug profiling: Fast: {}, Slow: {}".format(stop_fast-start_fast, stop_slow-start_slow))
+"""
+
 
 # Plotting waveforms
 plt.ion()
-
 
 plt.plot(t_us_ch1, w_analog_ch1, label="Ch1: analog", color="blue")
 plt.plot(t_us_ch1, w_sample_marker_ch1 - 2, label="M1: sample", color="orange")
@@ -243,21 +304,9 @@ while keyboardClick != True:
     keyboardClick=plt.waitforbuttonpress()
 
 
-
-
-# ---------------------------------------------------------
-# Close binary file.
-# ---------------------------------------------------------
+"""
 bin_in_ch1.close()
 bin_in_ch2.close()
-
-# ---------------------------------------------------------
-# Close message output file.
-# ---------------------------------------------------------
-msg.close()
-
-# ---------------------------------------------------------
-# Exit program.
-# ---------------------------------------------------------
+"""
 sys.exit()
 
