a4d3e483b7cb300c66184c2ca5ef33fc2c47d3a1
Neverhorst <tomek.qudi@gmail.com>
2020-10-30 01:47:05 +0100

    Removed obsolete imports

--- qudi/core/gui/qtwidgets/scan_widget.py
+++ qudi/core/gui/qtwidgets/scan_widget.py
@@ -20,15 +20,12 @@ Copyright (c) the Qudi Developers. See the COPYRIGHT.txt file at the
 top-level directory of this distribution and at <https://github.com/Ulm-IQO/qudi/>
 """
 
-import os
 import numpy as np
-from PySide2 import QtCore, QtWidgets, QtGui
+from PySide2 import QtCore, QtWidgets
 from pyqtgraph import PlotWidget, ImageItem, ViewBox, InfiniteLine, ROI
 
 from .colorbar import ColorBarWidget, ColorBarMode
 from ..colordefs import ColorScaleInferno
-from ...util.paths import get_artwork_dir
-from ...util.filters import scan_blink_correction
 
 __all__ = ('ScanImageItem', 'ScanPlotWidget', 'ScanViewBox', 'ScanWidget')
 
