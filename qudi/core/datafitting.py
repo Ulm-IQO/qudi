8b9e8fb3080c721a465286da4ccd170f030484e8
Neverhorst <tomek.qudi@gmail.com>
2021-01-29 17:22:39 +0100

    Implemented proper Qt model approach for FitConfigurations. Besides enabling scalable item views for fit configurations, this also allows for multiple FitContainer instances to use the same set of fit configurations.

--- qudi/core/datafitting.py
+++ qudi/core/datafitting.py
@@ -22,7 +22,7 @@ top-level directory of this distribution and at <https://github.com/Ulm-IQO/qudi
 
 import inspect
 import lmfit
-from PySide2 import QtCore
+from PySide2 import QtCore, QtWidgets
 from qudi.core.util.mutex import Mutex
 from qudi.core.util.units import create_formatted_output
 from qudi.core import qudi_slot
@@ -38,14 +38,16 @@ def __is_fit_model(cls):
 _fit_models = {name: cls for name, cls in inspect.getmembers(__models, __is_fit_model)}
 
 
-class FitConfiguration:
+class FitConfiguration(QtCore.QObject):
     """
     """
+    sigConfigurationChanged = QtCore.Signal(object)
 
-    def __init__(self, name, model, estimator=None, parameters=None, parameter_units=None):
+    def __init__(self, name, model, estimator=None, parameters=None, parameter_units=None, **kargs):
         assert isinstance(name, str), 'FitConfiguration name must be str type.'
         assert name, 'FitConfiguration name must be non-empty string.'
         assert model in _fit_models, f'Invalid fit model name encountered: "{model}".'
+        super().__init__(**kargs)
 
         self._access_lock = Mutex()
         self._name = name
@@ -76,6 +78,7 @@ class FitConfiguration:
                 self._model].estimators, f'Invalid fit model estimator encountered: "{value}"'
         with self._access_lock:
             self._estimator = value
+            self.sigConfigurationChanged.emit(self)
 
     @property
     def parameters(self):
@@ -92,6 +95,7 @@ class FitConfiguration:
                        value.values()), 'Fit parameters must be of type <lmfit.Parameter>.'
         with self._access_lock:
             self._parameters = value.copy() if value is not None else None
+            self.sigConfigurationChanged.emit(self)
 
     @property
     def parameter_units(self):
@@ -108,6 +112,7 @@ class FitConfiguration:
                 'Fit parameter units must be of str type.'
         with self._access_lock:
             self._parameters_units = value.copy() if value is not None else None
+            self.sigConfigurationChanged.emit(self)
 
     def formatted_result(self, fit_result):
         assert self._model == fit_result.name.split('(', 1)[1].rsplit(')', 1)[0], \
@@ -125,18 +130,116 @@ class FitConfiguration:
         return create_formatted_output(parameters_to_format)
 
 
+class FitConfigurationsModel(QtCore.QAbstractListModel):
+    """
+    """
+    sigFitConfigurationsChanged = QtCore.Signal(tuple)
+
+    def __init__(self, *args, configurations=None, **kwargs):
+        assert all(isinstance(c, FitConfiguration) for c in configurations) or (
+                    configurations is None)
+        super().__init__(*args, **kwargs)
+        self._fit_configurations = list() if configurations is None else list(configurations)
+        for config in self._fit_configurations:
+            config.sigConfigurationChanged.connect(
+                self._configuration_data_changed, QtCore.Qt.QueuedConnection
+            )
+
+    @property
+    def configuration_names(self):
+        return tuple(fc.name for fc in self._fit_configurations)
+
+    @property
+    def configurations(self):
+        return self._fit_configurations.copy()
+
+    @qudi_slot(str, str)
+    def add_configuration(self, name, model):
+        assert name not in self.configuration_names, f'Fit config "{name}" already defined.'
+        config = FitConfiguration(name, model, parent=self)
+        new_row = len(self._fit_configurations)
+        self.beginInsertRows(self.createIndex(new_row, 0), new_row, new_row)
+        self._fit_configurations.append(config)
+        config.sigConfigurationChanged.connect(
+            self._configuration_data_changed, QtCore.Qt.QueuedConnection
+        )
+        self.endInsertRows()
+        self.sigFitConfigurationsChanged.emit(self.configuration_names)
+
+    @qudi_slot(str)
+    def remove_configuration(self, name):
+        try:
+            row_index = self.configuration_names.index(name)
+        except ValueError:
+            return
+        self.beginRemoveRows(self.createIndex(row_index, 0), row_index, row_index)
+        config = self._fit_configurations.pop(row_index)
+        config.sigConfigurationChanged.disconnect()
+        config.setParent(None)
+        self.endRemoveRows()
+        self.sigFitConfigurationsChanged.emit(self.configuration_names)
+
+    def get_configuration_by_name(self, name):
+        try:
+            row_index = self.configuration_names.index(name)
+        except ValueError:
+            raise ValueError(f'No fit configuration found with name "{name}".')
+        return self._fit_configurations[row_index]
+
+    def flags(self, index):
+        if index.isValid():
+            return QtCore.Qt.ItemIsEditable | QtCore.Qt.ItemIsEnabled
+
+    def rowCount(self, parent=QtCore.QModelIndex()):
+        return len(self._fit_configurations)
+
+    def headerData(self, section, orientation, role=QtCore.Qt.DisplayRole):
+        if role == QtCore.Qt.DisplayRole:
+            if (orientation == QtCore.Qt.Horizontal) and (section == 0):
+                return 'Fit Configurations'
+            elif orientation == QtCore.Qt.Vertical:
+                try:
+                    return self.configuration_names[section]
+                except IndexError:
+                    pass
+        return None
+
+    def data(self, index=QtCore.QModelIndex(), role=QtCore.Qt.DisplayRole):
+        if (role == QtCore.Qt.DisplayRole) and (index.isValid()):
+            row = index.row()
+            # ToDo: Return the entire object to an item delegate instead of the name
+            return self._fit_configurations[row].name
+        return None
+
+    def setData(self, index, value, role=QtCore.Qt.EditRole):
+        return False
+
+    @qudi_slot(object)
+    def _configuration_data_changed(self, config):
+        try:
+            row = self._fit_configurations.index(config)
+        except ValueError:
+            return
+        self.dataChanged.emit(self.createIndex(row, 0), self.createIndex(row, 0))
+
+
 class FitContainer(QtCore.QObject):
     """
     """
-    sigFitConfigurationsChanged = QtCore.Signal(dict)  # {config_name: fit_config}
+    sigFitConfigurationsChanged = QtCore.Signal(tuple)  # config_names
     sigLastFitResultChanged = QtCore.Signal(object, object)  # (fit_config, lmfit.ModelResult)
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, config_model, **kwargs):
+        assert isinstance(config_model, FitConfigurationsModel)
         super().__init__(*args, **kwargs)
         self._access_lock = Mutex()
-        self._configured_fits = dict()
+        self._configuration_model = config_model
         self._last_fit_result = None
         self._last_fit_config = None
+
+        self._configuration_model.sigFitConfigurationsChanged.connect(
+            self.sigFitConfigurationsChanged
+        )
 
     @property
     def model_names(self):
@@ -152,43 +255,21 @@ class FitContainer(QtCore.QObject):
 
     @property
     def fit_configurations(self):
-        with self._access_lock:
-            return self._configured_fits.copy()
+        return self._configuration_model.configurations
 
     @property
     def fit_configuration_names(self):
-        with self._access_lock:
-            return tuple(self._configured_fits)
+        return self._configuration_model.configuration_names
 
     @property
     def last_fit(self):
         with self._access_lock:
-            return self._last_fit_config_name, self._last_fit_result
-
-    @qudi_slot(str, str)
-    def add_fit_configuration(self, fit_name, model):
-        with self._access_lock:
-            assert fit_name not in self._configured_fits, \
-                f'Fit configuration "{fit_name}" already present.'
-            self._configured_fits[fit_name] = FitConfiguration(fit_name, model)
-            self.sigFitConfigurationsChanged.emit(self.fit_configurations)
-
-    @qudi_slot(str)
-    def remove_fit_configuration(self, fit_name):
-        with self._access_lock:
-            removed = self._configured_fits.pop(fit_name, None)
-            if removed is not None:
-                self.sigFitConfigurationsChanged.emit(self.fit_configurations)
-
-    def get_fit_configuration(self, fit_name):
-        with self._access_lock:
-            return self._configured_fits.get(fit_name, None)
+            return self._last_fit_config, self._last_fit_result
 
     @qudi_slot(str, object, object)
     def fit_data(self, fit_config, x, data):
         with self._access_lock:
-            config = self._configured_fits.get(fit_config, None)
-            assert config is not None, f'No fit configuration found with name "{fit_config}".'
+            config = self._configuration_model.get_configuration_by_name(fit_config)
             model = _fit_models[config.model]
             estimator = config.estimator
             add_parameters = config.parameters
