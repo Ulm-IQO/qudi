edf753bd645ba9844e13ae71b10da0bff537195b
Neverhorst <tomek.qudi@gmail.com>
2021-02-01 22:16:28 +0100

    Created prototypes of finite sampling input and output hardware module dummys to serve as template for corresponding future interfaces.

--- qudi/hardware/dummy/finite_sampling_input_dummy.py
+++ qudi/hardware/dummy/finite_sampling_input_dummy.py
@@ -23,35 +23,40 @@ top-level directory of this distribution and at <https://github.com/Ulm-IQO/qudi
 
 import time
 import numpy as np
-
+from enum import Enum
 from qudi.core.module import Base
 from qudi.core.util.mutex import RecursiveMutex
-from qudi.core.statusvariable import StatusVar
 from qudi.core.configoption import ConfigOption
-from qudi.core.util.helpers import natural_sort
 
 
-class FiniteSamplingDummy(Base):
+class SimulationMode(Enum):
+    RANDOM = 0
+    ODMR = 1
+
+
+class FiniteSamplingInputDummy(Base):
     """
     ToDo: Document
     """
 
     _sample_rate_limits = ConfigOption(name='sample_rate_limits', default=(1, 1e6))
-    _acquisition_limits = ConfigOption(name='acquisition_limits', default=(1, 1e9))
+    _frame_size_limits = ConfigOption(name='frame_size_limits', default=(1, 1e9))
     _channel_units = ConfigOption(name='channel_units',
                                   default={'APD counts': 'c/s', 'Photodiode': 'V'})
+    _simulation_mode = ConfigOption(name='simulation_mode', default='ODMR')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self._thread_lock = RecursiveMutex()
         self._sample_rate = -1
-        self._number_of_samples = 0
+        self._frame_size = -1
         self._active_channels = frozenset()
 
         self.__start_time = 0.0
         self.__returned_samples = 0
         self.__simulated_samples = None
+        self.__constraints = dict()
 
     def on_activate(self):
         # Check and refine ConfigOptions
@@ -61,18 +66,22 @@ class FiniteSamplingDummy(Base):
         assert all(isinstance(unit, str) for unit in self._channel_units.values()), \
             'Channel units must be strings'
         assert len(self._sample_rate_limits) == 2, 'Sample rate limits must be iterable of length 2'
-        assert len(self._acquisition_limits) == 2, 'Acquisition limits must be iterable of length 2'
+        assert len(self._frame_size_limits) == 2, 'Frame size limits must be iterable of length 2'
         assert all(lim > 0 for lim in self._sample_rate_limits), 'Sample rate limits must be > 0'
-        assert all(lim > 0 for lim in self._acquisition_limits), 'Acquisition limits must be > 0'
+        assert all(lim > 0 for lim in self._frame_size_limits), 'Frame size limits must be > 0'
+        self._simulation_mode = SimulationMode[self._simulation_mode.upper()]
         self._sample_rate_limits = (float(min(self._sample_rate_limits)),
                                     float(max(self._sample_rate_limits)))
-        self._acquisition_limits = (int(round(min(self._acquisition_limits))),
-                                    int(round(max(self._acquisition_limits))))
+        self._frame_size_limits = (int(round(min(self._frame_size_limits))),
+                                    int(round(max(self._frame_size_limits))))
 
         # initialize default settings
         self._sample_rate = self._sample_rate_limits[1]
-        self._acquisition_length = self._acquisition_limits[1]
+        self._frame_size = self._frame_size_limits[1]
         self._active_channels = frozenset(self._channel_units)
+        self.__constraints = {'sample_rate_limits': self._sample_rate_limits,
+                              'frame_size_limits': self._frame_size_limits,
+                              'channel_units': self._channel_units.copy()}
 
         # process parameters
         self.__start_time = 0.0
@@ -84,37 +93,29 @@ class FiniteSamplingDummy(Base):
 
     @property
     def constraints(self):
-        return {
-            'sample_rate_limits': self._sample_rate_limits,
-            'acquisition_length_limits': self._acquisition_limits,
-        }
+        return self.__constraints.copy()
 
     @property
     def active_channels(self):
         return self._active_channels
 
     @property
-    def channel_units(self):
-        return self._channel_units.copy()
-
-    @property
     def sample_rate(self):
         return self._sample_rate
 
     @property
-    def acquisition_length(self):
-        return self._acquisition_length
+    def frame_size(self):
+        return self._frame_size
 
     @property
     def samples_in_buffer(self):
         with self._thread_lock:
             if self.module_state() == 'locked':
                 elapsed_time = time.time() - self.__start_time
-                acquired_samples = min(self._acquisition_length,
+                acquired_samples = min(self._frame_size,
                                        int(elapsed_time * self._sample_rate))
                 return max(0, acquired_samples - self.__returned_samples)
-            else:
-                return 0
+            return 0
 
     def set_sample_rate(self, rate):
         sample_rate = float(rate)
@@ -126,31 +127,35 @@ class FiniteSamplingDummy(Base):
             self._sample_rate = sample_rate
 
     def set_active_channels(self, channels):
-        channel_set = frozenset(channels)
-        assert channel_set.issubset(self._channel_units), \
+        chnl_set = frozenset(channels)
+        assert chnl_set.issubset(self._channel_units), \
             'Invalid channels encountered to set active'
         with self._thread_lock:
             assert self.module_state() == 'idle', \
                 'Unable to set active channels. Data acquisition in progress.'
-            self._active_channels = channel_set
+            self._active_channels = chnl_set
 
-    def set_acquisition_length(self, length):
+    def set_frame_size(self, length):
         samples = int(round(length))
-        assert self._acquisition_limits[0] <= samples <= self._acquisition_limits[1], \
-            f'acquisition length "{samples}" to set is out of bounds {self._acquisition_limits}'
+        assert self._frame_size_limits[0] <= samples <= self._frame_size_limits[1], \
+            f'frame size "{samples}" to set is out of bounds {self._frame_size_limits}'
         with self._thread_lock:
             assert self.module_state() == 'idle', \
-                'Unable to set acquisition length. Data acquisition in progress.'
-            self._acquisition_length = samples
+                'Unable to set frame size. Data acquisition in progress.'
+            self._frame_size = samples
 
     def start_buffered_acquisition(self):
         with self._thread_lock:
             assert self.module_state() == 'idle', \
                 'Unable to start data acquisition. Data acquisition already in progress.'
+            assert isinstance(self._simulation_mode, SimulationMode), 'Invalid simulation mode'
             self.module_state.lock()
 
             # ToDo: discriminate between different types of data
-            self.__simulate_odmr(self._acquisition_length)
+            if self._simulation_mode is SimulationMode.ODMR:
+                self.__simulate_odmr(self._frame_size)
+            elif self._simulation_mode is SimulationMode.RANDOM:
+                self.__simulate_random(self._frame_size)
 
             self.__returned_samples = 0
             self.__start_time = time.time()
@@ -158,10 +163,12 @@ class FiniteSamplingDummy(Base):
     def stop_buffered_acquisition(self):
         with self._thread_lock:
             if self.module_state() == 'locked':
-                unread_samples = self.__returned_samples < self._acquisition_length
-                if unread_samples > 0:
-                    self.log.warning(f'Buffered sample acquisition stopped before all samples have '
-                                     f'been read. {unread_samples} remaining samples will be lost.')
+                remaining_samples = self._frame_size - self.__returned_samples
+                if remaining_samples > 0:
+                    self.log.warning(
+                        f'Buffered sample acquisition stopped before all samples have '
+                        f'been read. {remaining_samples} remaining samples will be lost.'
+                    )
                 self.module_state.unlock()
 
     def get_buffered_samples(self, number_of_samples=None):
@@ -170,10 +177,10 @@ class FiniteSamplingDummy(Base):
             if number_of_samples is None:
                 number_of_samples = available_samples
             else:
-                remaining_samples = self._acquisition_length - self.__returned_samples
+                remaining_samples = self._frame_size - self.__returned_samples
                 assert number_of_samples <= remaining_samples, \
                     f'Number of samples to read ({number_of_samples}) exceeds remaining samples ' \
-                    f'in this acquisition ({remaining_samples})'
+                    f'in this frame ({remaining_samples})'
 
             # Return early if no samples are requested
             if number_of_samples < 1:
@@ -189,20 +196,20 @@ class FiniteSamplingDummy(Base):
             self.__returned_samples += number_of_samples
             return data
 
-    def acquire_samples(self, number_of_samples=None):
+    def acquire_frame(self, frame_size=None):
         with self._thread_lock:
-            if number_of_samples is None:
-                buffered_acquisition_length = None
+            if frame_size is None:
+                buffered_frame_size = None
             else:
-                buffered_acquisition_length = self._acquisition_length
-                self.set_acquisition_length(number_of_samples)
+                buffered_frame_size = self._frame_size
+                self.set_frame_size(frame_size)
 
             self.start_buffered_acquisition()
             data = self.get_buffered_samples()
             self.stop_buffered_acquisition()
 
-            if buffered_acquisition_length is not None:
-                self._acquisition_length = buffered_acquisition_length
+            if buffered_frame_size is not None:
+                self._frame_size = buffered_frame_size
             return data
 
     def __simulate_random(self, length):
@@ -221,8 +228,8 @@ class FiniteSamplingDummy(Base):
             if ch in self._active_channels:
                 pos = length / 2 + (np.random.rand() - 0.5) * length / 3
                 offset = np.random.rand() * 1000
-                noise = np.sqrt(amp)
                 amp = offset / 30
+                noise = np.sqrt(amp)
                 data[ch] = amp + (np.random.rand() - 0.5) * noise + amp * gamma ** 2 / (
                             (x - pos) ** 2 + gamma ** 2)
         self.__simulated_samples = data
---
+++ qudi/hardware/dummy/finite_sampling_output_dummy.py
@@ -0,0 +1,222 @@
+# -*- coding: utf-8 -*-
+
+"""
+ToDo: Document
+This file contains a dummy hardware module for the
+
+Qudi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Qudi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Qudi. If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (c) the Qudi Developers. See the COPYRIGHT.txt file at the
+top-level directory of this distribution and at <https://github.com/Ulm-IQO/qudi/>
+"""
+
+import time
+import numpy as np
+from enum import Enum
+from PySide2.QtCore import QTimer
+from qudi.core.module import Base
+from qudi.core.util.mutex import RecursiveMutex
+from qudi.core.configoption import ConfigOption
+
+
+class SamplingOutputMode(Enum):
+    JUMP_LIST = 0
+    EQUIDISTANT_SWEEP = 1
+
+
+class FiniteSamplingOutputDummy(Base):
+    """
+    ToDo: Document
+    """
+
+    _sample_rate_limits = ConfigOption(name='sample_rate_limits', default=(1, 1e6))
+    _frame_size_limits = ConfigOption(name='frame_size_limits', default=(1, 1e9))
+    _channel_units = ConfigOption(name='channel_units',
+                                  default={'Frequency': 'Hz', 'Voltage': 'V'})
+    _output_mode = ConfigOption(name='output_mode',
+                                default='JUMP_LIST',
+                                constructor=lambda x: SamplingOutputMode[x])
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self._thread_lock = RecursiveMutex()
+        self._sample_rate = -1
+        self._frame_size = -1
+        self._active_channels = frozenset()
+
+        self.__start_time = 0.0
+        self.__emitted_samples = 0
+        self.__frame_buffer = None
+        self.__constraints = dict()
+
+    def on_activate(self):
+        # Check and refine ConfigOptions
+        assert len(self._channel_units) > 0, 'Specify at least one channel with unit in config'
+        assert all(isinstance(name, str) and name for name in self._channel_units), \
+            'Channel names must be non-empty strings'
+        assert all(isinstance(unit, str) for unit in self._channel_units.values()), \
+            'Channel units must be strings'
+        assert len(self._sample_rate_limits) == 2, 'Sample rate limits must be iterable of length 2'
+        assert len(self._frame_size_limits) == 2, 'Acquisition limits must be iterable of length 2'
+        assert all(lim > 0 for lim in self._sample_rate_limits), 'Sample rate limits must be > 0'
+        assert all(lim > 0 for lim in self._frame_size_limits), 'Acquisition limits must be > 0'
+        self._sample_rate_limits = (float(min(self._sample_rate_limits)),
+                                    float(max(self._sample_rate_limits)))
+        self._frame_size_limits = (int(round(min(self._frame_size_limits))),
+                                   int(round(max(self._frame_size_limits))))
+
+        # initialize default settings
+        self._sample_rate = self._sample_rate_limits[1]
+        self._frame_size = self._frame_size_limits[1]
+        self._active_channels = frozenset(self._channel_units)
+        self.__constraints = {
+            'sample_rate_limits': self._sample_rate_limits,
+            'frame_size_limits': self._frame_size_limits,
+            'channel_units': self._channel_units.copy(),
+            'output_modes': frozenset(SamplingOutputMode)
+        }
+
+        # process parameters
+        self.__start_time = 0.0
+        self.__emitted_samples = 0
+        self.__frame_buffer = None
+
+    def on_deactivate(self):
+        self.__frame_buffer = None
+
+    @property
+    def constraints(self):
+        return self.__constraints.copy()
+
+    @property
+    def active_channels(self):
+        with self._thread_lock:
+            return self._active_channels
+
+    @property
+    def sample_rate(self):
+        return self._sample_rate
+
+    @property
+    def frame_size(self):
+        self._frame_size
+
+    @property
+    def output_mode(self):
+        return self._output_mode
+
+    @property
+    def samples_in_buffer(self):
+        with self._thread_lock:
+            if self.module_state() == 'locked':
+                elapsed_time = time.time() - self.__start_time
+                emitted_samples = min(self._frame_size, int(elapsed_time * self._sample_rate))
+                return max(0, emitted_samples - self.__emitted_samples)
+            return 0
+
+    def set_sample_rate(self, rate):
+        sample_rate = float(rate)
+        assert self._sample_rate_limits[0] <= sample_rate <= self._sample_rate_limits[1], \
+            f'Sample rate "{sample_rate}Hz" to set is out of bounds {self._sample_rate_limits}'
+        with self._thread_lock:
+            assert self.module_state() == 'idle', \
+                'Unable to set sample rate. Sampling output in progress.'
+            self._sample_rate = sample_rate
+
+    def set_active_channels(self, channels):
+        chnl_set = frozenset(channels)
+        assert chnl_set.issubset(self._channel_units), \
+            'Invalid channels encountered to set active'
+        with self._thread_lock:
+            assert self.module_state() == 'idle', \
+                'Unable to set active channels. Sampling output in progress.'
+            if (self.__frame_buffer is not None) and (self._active_channels != chnl_set):
+                self.__frame_buffer = None
+            self._active_channels = chnl_set
+
+    def set_frame_data(self, data):
+        assert isinstance(data, dict) or (data is None)
+        if data is not None:
+            assert frozenset(data) == self._active_channels, \
+                f'Must provide data for all active channels: {self._active_channels}'
+            if self._output_mode == SamplingOutputMode.JUMP_LIST:
+                frame_size = len(next(iter(data.values())))
+                assert all(len(d) == frame_size for d in data.values()), \
+                    'Frame data arrays for all channels must be of equal length.'
+            elif self._output_mode == SamplingOutputMode.EQUIDISTANT_SWEEP:
+                assert all(len(d) == 3 for d in data.values()), \
+                    'EQUIDISTANT_SWEEP output mode requires value tuples of length 3 for each ' \
+                    'active channel.'
+                frame_size = next(iter(data.values()))[-1]
+                assert all(d[-1] == frame_size for d in data.values()), \
+                    'Frame data arrays for all channels must be of equal length.'
+            assert self._frame_size_limits[0] <= frame_size <= self._frame_size_limits[1], \
+                f'Frame size "{frame_size}" to set is out of bounds {self._frame_size_limits}'
+        with self._thread_lock:
+            assert self.module_state() == 'idle', \
+                'Unable to set frame data. Sampling output in progress.'
+            if data is None:
+                self._frame_size = 0
+                self.__frame_buffer = None
+            elif self._output_mode == SamplingOutputMode.JUMP_LIST:
+                self._frame_size = frame_size
+                self.__frame_buffer = data.copy()
+            elif self._output_mode == SamplingOutputMode.EQUIDISTANT_SWEEP:
+                self._frame_size = frame_size
+                self.__frame_buffer = {ch: np.linspace(*d) for ch, d in data.items()}
+
+    def set_output_mode(self, mode):
+        assert mode in self.__constraints['output_modes'], f'Invalid output mode "{mode}"'
+        with self._thread_lock:
+            assert self.module_state() == 'idle', \
+                'Unable to set output mode. Sampling output in progress.'
+            self.__frame_buffer = None
+            self._output_mode = mode
+
+    def start_buffered_output(self):
+        with self._thread_lock:
+            assert self.module_state() == 'idle', \
+                'Unable to start sampling output. Sampling output already in progress.'
+            assert self.__frame_buffer is not None, \
+                'Unable to start sampling output. No frame data set.'
+            self.module_state.lock()
+
+            self.__emitted_samples = 0
+            total_time = 1000 * (self._frame_size / self._sample_rate)
+            self.__start_time = time.time()
+            QTimer.singleShot(total_time, self.stop_buffered_output)
+
+    def stop_buffered_output(self):
+        with self._thread_lock:
+            if self.module_state() == 'locked':
+                remaining_samples = self._frame_size - self.__emitted_samples
+                if remaining_samples > 0:
+                    self.log.warning(
+                        f'Buffered sampling output stopped before all samples have '
+                        f'been emitted. {remaining_samples} remaining samples will be discarded.'
+                    )
+                self.__frame_buffer = None
+                self.module_state.unlock()
+
+    def emit_samples(self, data):
+        with self._thread_lock:
+            buffered_frame_size = self._frame_size
+            self.set_frame_data(data)
+
+            self.start_buffered_acquisition()
+            time.sleep(self._frame_size / self._sample_rate)
+            self.stop_buffered_acquisition()
+
+            self._frame_size = buffered_frame_size
