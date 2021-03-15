7bc485850a1bf2150b1dd4d3e501fdead22db183
Neverhorst <tomek.qudi@gmail.com>
2021-03-15 10:30:58 +0100

    added logging module to module_finder.py

--- qudi/tools/config_editor/module_finder.py
+++ qudi/tools/config_editor/module_finder.py
@@ -7,10 +7,14 @@ import os
 import sys
 import importlib
 import inspect
+import logging
 from qudi.core.paths import get_main_dir
 from qudi.core.module import Base, LogicBase, GuiBase
 
 __all__ = ('ModuleFinder', 'QudiModules')
+
+
+log = logging.getLogger(__package__)
 
 
 class ModuleFinder:
