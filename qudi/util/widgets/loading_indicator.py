8e14758f42630b339988b414474979b09828775f
Timo <timo.joas@uni-ulm.de>
2021-09-11 17:32:54 +0200

    Rolling back unnecessary code from chery pick prior to merging

--- qudi/util/widgets/loading_indicator.py
+++ qudi/util/widgets/loading_indicator.py
@@ -20,7 +20,7 @@ Copyright (c) the Qudi Developers. See the COPYRIGHT.txt file at the
 top-level directory of this distribution and at <https://github.com/Ulm-IQO/qudi/>
 """
 
-from qtpy import QtWidgets, QtCore, QtGui
+from PySide2 import QtWidgets, QtCore, QtGui
 
 
 class CircleLoadingIndicator(QtWidgets.QWidget):
