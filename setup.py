0af3e09dfe8d7830b513b50883491f5341656182
Neverhorst <tomek.qudi@gmail.com>
2021-09-23 12:25:58 +0200

    Higher Python minor version requirement (3.7 -> 3.8) due to "typing" standard library module.

--- setup.py
+++ setup.py
@@ -120,7 +120,7 @@ setup(name='qudi',
                 ],
       license='GPLv3',
       install_requires=windows_dep if sys.platform == 'win32' else unix_dep,
-      python_requires='~=3.7',
+      python_requires='~=3.8',
       cmdclass={'develop': PrePostDevelopCommands, 'install': PrePostInstallCommands},
       entry_points={
           'console_scripts': ['qudi=qudi.runnable:main',
