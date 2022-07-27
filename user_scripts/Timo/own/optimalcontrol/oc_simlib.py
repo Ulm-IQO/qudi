e77b01dbfb1a96ecc3a3e9c0d0db63700c1587ff
Timo <timo.joas@uni-ulm.de>
2022-07-27 16:17:29 +0200

    update lib

--- user_scripts/Timo/own/optimalcontrol/oc_simlib.py
+++ user_scripts/Timo/own/optimalcontrol/oc_simlib.py
@@ -1,4 +1,5 @@
 import os
+import copy as cp
 import numpy as np
 from scipy import interpolate
 
@@ -74,36 +75,214 @@ class SimParameters():
     rho_x = qutip.tensor(rho_x_nv, rho_14n)
     rho_y = qutip.tensor(rho_y_nv, rho_14n)
 
-class TimeDependentSimulation():
+
+class ArbPulse():
+
+    __allowed_si_units = ['s', 'V', 'Hz']
+    __unit_prefix_dict = {
+        'y': 1e-24,
+        'z': 1e-21,
+        'a': 1e-18,
+        'f': 1e-15,
+        'p': 1e-12,
+        'n': 1e-9,
+        'µ': 1e-6,
+        'm': 1e-3,
+        '': 1,
+        'k': 1e3,
+        'M': 1e6,
+        'G': 1e9,
+        'T': 1e12,
+        'P': 1e15,
+        'E': 1e18,
+        'Z': 1e21,
+        'Y': 1e24
+    }
+
+    def __init__(self):
+        self.name = None
+        self._folder = None
+        self._file = None
+        self._file_ampl = None
+        self._file_phase = None
+        self.timegrid_unit = 'us'
+        self.data_unit = 'MHz'
+
+        self._data_ampl = None
+        self._data_phase = None
+        self._timegrid_ampl = None
+        self._timegrid_phase = None
+
+        self.length_pix = np.nan
+
+        self._func_p_v_2_omega_mhz = None
+
+    @property
+    def file(self):
+        if self._file and not self._file_ampl and not self._file_phase:
+            return [self._file]
+        elif not self._file and self._file_ampl:
+            return [self._file_ampl, self._file_phase]
+        elif not self._file and not self._file_ampl:
+            return []
+        else:
+            raise RuntimeError("Should either have a single file or at least an ampl file.")
+
+    def get_timegrid(self, unit=None):
+        if unit is None:
+            unit = self.timegrid_unit
+        t_a, t_ph = self.convert_unit_time(unit)
+        assert np.array_equal(t_a, t_ph)
+        return t_a
+    # @property, acts like decorator
+    timegrid = property(get_timegrid)
+
+    def get_data_ampl(self, unit=None):
+        if unit is None:
+            unit = self.data_unit
+        return self.convert_unit_data(unit)[0]
+    # @property, acts like decorator
+    data_ampl = property(get_data_ampl)
+
+    def get_data_phase(self, unit='Hz'):
+        return self.convert_unit_data(unit)[1]
+    data_phase = property(get_data_phase)
+
+    def as_dict(self):
+        out_dict = {'name': self.name,
+                    'folder': self._folder,
+                    'file': self.file}
+
+        out_dict['timegrid_unit'] = self.timegrid_unit
+        out_dict['data_unit'] = self.data_unit
+        out_dict['data_ampl'] = self._data_ampl
+        out_dict['data_phase'] = self._data_phase
+        out_dict['timegrid_ampl'] = self._timegrid_ampl
+        out_dict['timegrid_phase'] = self._timegrid_phase
+
+        return out_dict
+
+    def _split_unit_prefix(self, unit_str):
+
+        full_str = cp.copy(unit_str)
+        # Attentions: requires that entries of __allowed_si_units don't overlap with __unit_prefix_dict
+        for si in self.__allowed_si_units:
+            unit_str = unit_str.replace(si, '')
+
+        prefix = unit_str
+        unit_str = full_str.replace(unit_str, '')
+
+        return prefix, unit_str
+
+    def _get_unit_type_data(self, unit=None):
+        if unit is None:
+            unit = self.data_unit
+        if 'Hz' in unit:
+            return 'rabi_freq'
+        elif 'V' in unit:
+            return 'voltage'
+        else:
+            raise ValueError
+
+    def _check_unit_sanity(self, unit):
+        sane = True
+        try:
+            prefix, unit = self._split_unit_prefix(unit)
+            if not(prefix in self.__unit_prefix_dict.keys() and unit in self.__allowed_si_units):
+                sane = False
+        except:
+            sane = False
+        return sane
+
+    def convert_p_2_rabi(self, unit='Hz'):
+        type_old = self._get_unit_type_data()
+        type_new = self._get_unit_type_data(unit)
+        prefix_old, unit_si_old = self._split_unit_prefix(self.data_unit)
+
+        if type_old == 'voltage' and type_new == 'rabi_freq':
+            if not self._func_p_v_2_omega_mhz:
+                raise ValueError("No available scaling function for voltage->rabi conversion.")
+                # to Rabi freq units
+            data_ampl = self._func_p_v_2_omega_mhz(self._data_ampl)
+            data_phase = self._func_p_v_2_omega_mhz(self._data_phase)
+
+            scale_old = self.__unit_prefix_dict[prefix_old]
+            inter_scale = 1e6*scale_old
+            # guaranteed to be exactly once in dict
+            inter_prefix = [k for k,v in self.__unit_prefix_dict.items() if v == inter_scale][0]
+            data_unit = f'{inter_prefix}Hz'
+
+            return data_ampl, data_phase, data_unit
+        else:
+            raise ValueError
+
+    def convert_unit_data(self, unit='Hz'):
+        prefix_old, unit_si_old = self._split_unit_prefix(self.data_unit)
+        prefix_new, unit_si_new = self._split_unit_prefix(unit)
+        type_old = self._get_unit_type_data()
+        type_new = self._get_unit_type_data(unit)
+        conv_factor = self.__unit_prefix_dict[prefix_old]/self.__unit_prefix_dict[prefix_new]
+
+        if type_old == 'voltage' and type_new == 'rabi_freq':
+            self._data_ampl, self._data_phase, self.data_unit = self.convert_p_2_rabi(unit=unit)
+            return self.convert_unit_data(unit=unit)
+
+        if type_old != type_new or unit_si_old != unit_si_new:
+            raise ValueError(f"Can't automatically convert units {unit_si_old}->{unit_si_new} "
+                             f"of type ({type_old}) to ({type_new})")
+
+        return conv_factor*self._data_ampl, conv_factor*self._data_phase
+
+    def convert_unit_time(self, unit='s'):
+        prefix_old, unit_si_old = self._split_unit_prefix(self.timegrid_unit)
+        prefix_new, unit_si_new = self._split_unit_prefix(unit)
+
+        if unit_si_old != unit_si_new:
+            raise ValueError(f"Can't automatically convert units {unit_si_old}->{unit_si_new}")
+        conv_factor = self.__unit_prefix_dict[prefix_old]/self.__unit_prefix_dict[prefix_new]
+
+        return conv_factor*self._timegrid_ampl, conv_factor*self._timegrid_phase
+
+    def set_unit_data(self, unit='Hz'):
+        self._data_ampl, self._data_phase = self.convert_unit_data(unit)
+        self.data_unit = unit
+
+    def set_unit_time(self, unit='s'):
+        self._timegrid_ampl, self._timegrid_phase = self.convert_unit_time(unit)
+        self.timegrid_unit = unit
 
     @staticmethod
     def get_pulse_filename(path, name="", name_ampl="amplitude.txt", name_phase="phase.txt"):
         return os.path.abspath(path + "/" + name + name_ampl), os.path.abspath(path + "/" + name + name_phase)
 
     @staticmethod
-    def load_pulse(folder, name, extension='txt', func_volt_2_rabi=None, func_t_2_us=None):
-        # expect experimental data in units ampl (V), time (s)
-        # convert to Rabi drive in (MHz), (us)
+    def load_pulse(folder, name, extension='txt', unit_t='s', unit_data='V', func_p_v_2_omega_mhz=None):
+        pulse = ArbPulse()
+        pulse.load(folder, name, extension, unit_t, unit_data, func_p_v_2_omega_mhz)
 
-        out_dict = {'name': name,
-                    'folder': folder}
+        return pulse
 
-        if func_volt_2_rabi is None:
-            func_volt_2_rabi = lambda x: x
-        if func_t_2_us is None:
-            func_t_2_us = lambda t:t
+    def load(self, folder, name, extension='', unit_t='s', unit_data='V', func_p_v_2_omega_mhz=None):
+        self.name = name
+        self._folder = folder
+        self._func_p_v_2_omega_mhz = func_p_v_2_omega_mhz
 
         if extension=='txt':
-            f_ampl, f_ph = TimeDependentSimulation.get_pulse_filename(os.path.abspath(folder), name=name)
+            f_ampl, f_ph = ArbPulse.get_pulse_filename(os.path.abspath(folder), name=name)
 
             data_ampl = np.loadtxt(f_ampl)[:,1]
             t_ampl = np.loadtxt(f_ampl)[:,0]
-            data_ph = np.loadtxt(f_ph)[:,1]
-            t_ph = np.loadtxt(f_ph)[:,0]
+            try:
+                data_ph = np.loadtxt(f_ph)[:,1]
+                t_ph = np.loadtxt(f_ph)[:,0]
+            except OSError:
+                t_ph = t_ampl
+                data_ph = np.zeros((len(data_ampl)))
 
-            out_dict.update({'file_ampl': f_ampl,
-                             'file_phase': f_ph})
-        elif extension =='npz':
+            self._file_ampl  = f_ampl
+            self._file_phase = f_ph
+
+        elif extension == 'npz':
             file = os.path.abspath(folder) + "/" + name + ".npz"
             data = np.load(file)
             t_ampl = data['time_grid1']
@@ -111,29 +290,28 @@ class TimeDependentSimulation():
             t_ph = data['time_grid2']
             data_ph = data['pulse2']
 
-            out_dict.update({'file':file})
-
+            self._file = file
         else:
             raise ValueError
 
         if not np.all(t_ph == t_ampl):
             raise ValueError("Timegrids differ for quadratures")
 
-        out_dict.update({
-            'timegrid_ampl': t_ampl,
-            'data_ampl': data_ampl,
-            'timegrid_phase': t_ph,
-            'data_phase': data_ph})
+        self._timegrid_ampl = t_ampl
+        self._timegrid_phase = t_ph
+        self._data_ampl = data_ampl
+        self._data_phase = data_ph
 
-        # convert units
-        out_dict['timegrid_unit'] = 'us'
-        out_dict['data_unit'] = 'MHz'
-        out_dict['data_ampl'] = func_volt_2_rabi(out_dict['data_ampl'])
-        out_dict['data_phase'] = func_volt_2_rabi(out_dict['data_phase'])
-        out_dict['timegrid_ampl'] = func_t_2_us(out_dict['timegrid_ampl'])
-        out_dict['timegrid_phase'] = func_t_2_us(out_dict['timegrid_phase'])
+        if self._check_unit_sanity(unit_t) and self._check_unit_sanity(unit_data):
+            # convert units
+            self.timegrid_unit = unit_t
+            self.data_unit = unit_data
+        else:
+            raise ValueError(f"Don't understand units (t/data): {unit_t}, {unit_data}")
 
-        return out_dict
+
+
+class TimeDependentSimulation():
 
     @staticmethod
     def oc_element(t, pulse_timegrid, data_ampl, data_ph, frequency, B_gauss, amplitude_scaling, sim_params):
@@ -194,6 +372,14 @@ class TimeDependentSimulation():
 
 
     def run_sim_fsweep(self, freq_array, pulse, B_gauss, sim_params, n_timebins=500, t_idle_extension=-1e-9):
+
+        # for compability reason, accept pulse as dict or ArbPulse object
+        # if supplying a dict, you are responsible for correct units!
+        if type(pulse) == ArbPulse:
+            pulse.set_unit_time('µs')
+            pulse.set_unit_data('MHz')
+            pulse = pulse.as_dict()
+
         B = B_gauss
         simp = sim_params
         oc_length = pulse['timegrid_ampl'][-1] + t_idle_extension*1e6
@@ -201,13 +387,40 @@ class TimeDependentSimulation():
         t = np.linspace(0, oc_length, n_timebins)
         options=qutip.Options(atol=1e-15, rtol=1e-15, nsteps=1e8, store_final_state=True)
 
-        freq_init_state = simp.rho_ms0
+        init_state = simp.rho_ms0
 
         # perform the measurement
         data_freq_detuning = np.zeros(len(freq_array))
         for idx,freq in enumerate(freq_array):
             oc_el = TimeDependentSimulation.oc_element(t, pulse['timegrid_ampl'], pulse['data_ampl'], pulse['data_phase'], freq, B, 1, simp)
-            results_measurement = qutip.mesolve(oc_el, freq_init_state, t, [], [simp.P_nv], options=options, progress_bar=None)
+            results_measurement = qutip.mesolve(oc_el, init_state, t, [], [simp.P_nv], options=options, progress_bar=None)
             data_freq_detuning[idx] = results_measurement.expect[0][-1]
 
         return data_freq_detuning
+
+    def run_sim_ampsweep(self, amp_array, pulse, B_gauss, sim_params, n_timebins=500, t_idle_extension=-1e-9):
+
+        # for compability reason, accept pulse as dict or ArbPulse object
+        # if supplying a dict, you are responsible for correct units!
+        if type(pulse) == ArbPulse:
+            pulse.set_unit_time('µs')
+            pulse.set_unit_data('MHz')
+            pulse = pulse.as_dict()
+
+        B = B_gauss
+        simp = sim_params
+        oc_length = pulse['timegrid_ampl'][-1] + t_idle_extension*1e6
+
+        t = np.linspace(0, oc_length, n_timebins)
+        options=qutip.Options(atol=1e-15, rtol=1e-15, nsteps=1e8, store_final_state=True)
+
+        init_state = simp.rho_ms0
+        freq = simp.D - simp.gamma_nv * B
+        # perform the measurement
+        data_amp_detuning = np.zeros(len(amp_array))
+        for idx,amp in enumerate(amp_array):
+            oc_el = TimeDependentSimulation.oc_element(t, pulse['timegrid_ampl'], pulse['data_ampl'], pulse['data_phase'], freq, B, 1+amp, simp)
+            results_measurement = qutip.mesolve(oc_el, init_state, t, [], [simp.P_nv], options=options, progress_bar=None)
+            data_amp_detuning[idx] = results_measurement.expect[0][-1]
+
+        return data_amp_detuning
